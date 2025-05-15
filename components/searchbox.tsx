fb1a529d27218fa3d9342cec935944a04891b22b
Alimnfll <alim@systatum.com>
2025-05-15 11:22:26 +0700

    Add search box and tab

---
+++ components/searchbox.tsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+import { Search, X } from "lucide-react";
+import { ChangeEvent, InputHTMLAttributes } from "react";
+
+interface BaseSearchBoxProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  value: string;
+  className?: string;
+  onChange: (data: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Searchbox({
+  name,
+  value,
+  className,
+  onChange,
+  ...props
+}: BaseSearchBoxProps) {
+  const inputId = `textbox-${name}`;
+
+  const inputClass = clsx(
+    "rounded-3xl text-black px-9 bg-white text-xs w-full py-3 outline-none",
+    "border border-gray-300 border-2 focus:ring-[#61A9F9] focus:border-[#61A9F9]",
+    className,
+  );
+
+  return (
+    <div className="relative w-full ring-0">
+      <Search size={14} className="absolute top-1/2 left-3 -translate-y-1/2" />
+
+      <input
+        id={inputId}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={inputClass}
+        {...(props as InputHTMLAttributes<HTMLInputElement>)}
+      />
+
+      <X
+        onClick={() => {
+          const event = {
+            target: {
+              name,
+              value: "",
+            },
+          } as ChangeEvent<HTMLInputElement>;
+          onChange(event);
+        }}
+        size={14}
+        className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
+      />
+    </div>
+  );
+}
---
+++ components/tab.tsx
@@ -0,0 +1,140 @@
+import  { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { TabFieldsProps } from "../type/Tab";
+
+interface TabProps {
+  view: string | null;
+  fields: TabFieldsProps[];
+  setView: (data: string | null) => void;
+}
+
+export default function Tab({ fields, view, setView }: TabProps) {
+  const [hovered, setHovered] = useState<string | null>(null);
+
+  const activeId = hovered || view;
+  const activeIndex = fields.findIndex((item) => item.id === activeId);
+  const tabRefs = useRef<Array<HTMLDivElement | null>>([]);
+  const [tabSizes, setTabSizes] = useState<{ width: number; left: number }[]>(
+    [],
+  );
+  const [isInitialized, setIsInitialized] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    tabRefs.current = tabRefs.current.slice(0, fields.length);
+
+    while (tabRefs.current.length < fields.length) {
+      tabRefs.current.push(null);
+    }
+  }, [fields]);
+
+  useEffect(() => {
+    if (!view && fields.length > 0) {
+      setView(fields[0].id);
+    }
+  }, [view, fields, setView]);
+
+  useEffect(() => {
+    const calculateTabSizes = () => {
+      if (
+        !tabRefs.current.length ||
+        !tabRefs.current[0] ||
+        !containerRef.current
+      )
+        return;
+
+      const parentRect = containerRef.current.getBoundingClientRect();
+
+      const sizes = tabRefs.current.map((tabRef) => {
+        if (!tabRef) return { width: 0, left: 0 };
+
+        const rect = tabRef.getBoundingClientRect();
+        return {
+          width: rect.width,
+          left: rect.left - parentRect.left,
+        };
+      });
+
+      setTabSizes(sizes);
+      setIsInitialized(true);
+    };
+
+    calculateTabSizes();
+    const timeoutId = setTimeout(calculateTabSizes, 50);
+
+    window.addEventListener("resize", calculateTabSizes);
+    return () => {
+      window.removeEventListener("resize", calculateTabSizes);
+      clearTimeout(timeoutId);
+    };
+  }, [fields.length]);
+
+  const setTabRef = (index: number) => (element: HTMLDivElement | null) => {
+    tabRefs.current[index] = element;
+  };
+
+  const getInitialPosition = () => {
+    if (!isInitialized && fields.length > 0) {
+      if (activeIndex === 0) {
+        return { left: 4, width: 60 };
+      }
+    }
+
+    if (isInitialized && activeIndex !== -1 && tabSizes[activeIndex]) {
+      return {
+        left: tabSizes[activeIndex].left,
+        width: tabSizes[activeIndex].width - 3,
+      };
+    }
+
+    return { left: 4, width: 60 };
+  };
+
+  const initialPosition = getInitialPosition();
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative flex w-fit flex-row items-center justify-center overflow-hidden rounded-xl border border-gray-100 px-1 shadow-sm"
+    >
+      <motion.div
+        layout
+        className="absolute top-1 bottom-1 z-0 h-[25px] rounded-xl bg-blue-600"
+        initial={{
+          left: initialPosition.left,
+          width: initialPosition.width,
+        }}
+        animate={{
+          left: initialPosition.left,
+          width: initialPosition.width,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 250,
+          damping: 30,
+        }}
+      />
+
+      {fields.map((data, index) => {
+        const isHovered = hovered === data.id;
+        const isActive = view === data.id;
+
+        const textColor =
+          isHovered || (!hovered && isActive) ? "text-white" : "text-gray-900";
+
+        return (
+          <div
+            key={index}
+            ref={setTabRef(index)}
+            className={`relative z-10 cursor-pointer px-4 py-1 text-center font-medium transition-colors duration-200 ${textColor}`}
+            onMouseEnter={() => setHovered(data.id)}
+            onMouseLeave={() => setHovered(null)}
+            onClick={() => setView(data.id)}
+          >
+            {data.title}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
