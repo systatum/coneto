fe7aafa70ef882da601a2355e51490f2ae73dc38
Alimnfll <alim@systatum.com>
2025-05-19 13:02:52 +0700

    Update for phone box component

--- components/phonebox.tsx
+++ components/phonebox.tsx
@@ -5,42 +5,6 @@ import { CountryCodeProps } from "../type/phonebox";
 import { COUNTRY_CODES } from "../constants/countries";
 import { AsYouType, CountryCode } from "libphonenumber-js/max";
 
-function trimPhone(input: string): string {
-  const onlyNumber = input.replace(/[^0-9]/g, "");
-  return onlyNumber.startsWith("0") ? onlyNumber.slice(1) : onlyNumber;
-}
-
-function formatWithSimplePattern(phone: string): string {
-  const digits = phone.replace(/\D/g, "");
-  if (digits.length <= 3) return digits;
-  if (digits.length <= 7) return `${digits.slice(0, 3)}-${digits.slice(3)}`;
-  return `${digits.slice(0, 3)}-${digits.slice(3, 7)}-${digits.slice(7)}`;
-}
-
-function formatPhoneNumber(
-  phone: string,
-  countryCode: CountryCode = "ID"
-): string {
-  const cleaned = phone.replace(/[^\d+]/g, "");
-
-  const formatter = new AsYouType(countryCode);
-  formatter.input(cleaned);
-
-  const formattedInternational = formatter.getNumber()?.formatInternational();
-
-  if (!formattedInternational) {
-    return formatWithSimplePattern(cleaned);
-  }
-
-  const parts = formattedInternational.split(" ");
-  if (parts.length < 2) return formattedInternational;
-
-  const rest = parts.slice(1).join(" ");
-  const restWithDash = rest.replace(/ /g, "-");
-
-  return `${restWithDash}`;
-}
-
 interface PhoneboxProps {
   label: string;
   name?: string;
@@ -235,4 +199,40 @@ export default function Phonebox({
       )}
     </div>
   );
+}
+
+function trimPhone(input: string): string {
+  const onlyNumber = input.replace(/[^0-9]/g, "");
+  return onlyNumber.startsWith("0") ? onlyNumber.slice(1) : onlyNumber;
+}
+
+function formatWithSimplePattern(phone: string): string {
+  const digits = phone.replace(/\D/g, "");
+  if (digits.length <= 3) return digits;
+  if (digits.length <= 7) return `${digits.slice(0, 3)}-${digits.slice(3)}`;
+  return `${digits.slice(0, 3)}-${digits.slice(3, 7)}-${digits.slice(7)}`;
+}
+
+function formatPhoneNumber(
+  phone: string,
+  countryCode: CountryCode = "ID"
+): string {
+  const cleaned = phone.replace(/[^\d+]/g, "");
+
+  const formatter = new AsYouType(countryCode);
+  formatter.input(cleaned);
+
+  const formattedInternational = formatter.getNumber()?.formatInternational();
+
+  if (!formattedInternational) {
+    return formatWithSimplePattern(cleaned);
+  }
+
+  const parts = formattedInternational.split(" ");
+  if (parts.length < 2) return formattedInternational;
+
+  const rest = parts.slice(1).join(" ");
+  const restWithDash = rest.replace(/ /g, "-");
+
+  return `${restWithDash}`;
 }
