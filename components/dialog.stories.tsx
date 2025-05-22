5b9710e9890736aee07897ef026a829547d59378
Alimnfll <alim@systatum.com>
2025-05-22 14:41:34 +0700

    Update for dialog deny on storybook (padding, margin, etc).

--- components/dialog.stories.tsx
+++ components/dialog.stories.tsx
@@ -94,16 +94,24 @@ export const DialogWithButton: Story = {
         <DialogTrigger asChild>
           <Button>Dialog With Button</Button>
         </DialogTrigger>
-        <DialogContent>
-          <div className="flex flex-col w-full items-start">
+        <DialogContent className="p-3">
+          <div className="flex flex-col w-full gap-2 items-start">
             <div className="flex flex-col gap-1">
               <DialogTitle>Dialog With Button</DialogTitle>
               <DialogDescription>
                 Dialog with actions in the footer.
               </DialogDescription>
             </div>
-            <p className="text-xs py-4">You can put any content here.</p>
-            <div className="flex flex-row justify-end w-full gap-2">
+            <p className="text-xs py-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="flex flex-row justify-end w-full mt-4 gap-2">
               <DialogClose asChild>
                 <Button>Cancel</Button>
               </DialogClose>
