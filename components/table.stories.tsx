6b2bbb5ddb7308febee3837f542fd4e0733c54d7
Alimnfll <alim@systatum.com>
2025-05-26 22:38:33 +0700

    Update for table stories w/ sortable content.

--- components/table.stories.tsx
+++ components/table.stories.tsx
@@ -1,5 +1,8 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { RowData, Table } from "./table";
+import { Toolbar } from "./toolbar";
+import { ArrowDown, ArrowUp, RefreshCw } from "lucide-react";
+import { useState } from "react";
 
 const meta: Meta<typeof Table> = {
   title: "Content/Table",
@@ -20,6 +23,11 @@ const sampleRows = Array.from({ length: 20 }, (_, i) => {
   const type = TYPES_DATA[i % TYPES_DATA.length];
   return <Table.Row key={i} content={[`Load Balancer ${i + 1}`, type]} />;
 });
+
+const rawRows = Array.from({ length: 20 }, (_, i) => ({
+  name: `Load Balancer ${i + 1}`,
+  type: TYPES_DATA[i % TYPES_DATA.length],
+}));
 
 export const TableDefault: Story = {
   render: () => {
@@ -45,6 +53,94 @@ export const TableSelectable: Story = {
       <Table selectable columns={columns} onItemsSelected={handleItemsSelected}>
         {sampleRows}
       </Table>
+    );
+  },
+};
+
+export const TableSelectableWithSorting: Story = {
+  render: () => {
+    const [rows, setRows] = useState(rawRows);
+    const [isOpen, setIsOpen] = useState<boolean | null>(false);
+
+    const handleSortAsc = () => {
+      const sorted = [...rows].sort((a, b) => a.type.localeCompare(b.type));
+      setRows(sorted);
+    };
+
+    const handleSortDesc = () => {
+      const sorted = [...rows].sort((a, b) => b.type.localeCompare(a.type));
+      setRows(sorted);
+    };
+
+    const originalRows = [...rawRows];
+
+    const handleResetSort = () => {
+      setRows(originalRows);
+      setIsOpen(null);
+    };
+
+    const columns = ["Name", "Type"];
+    const TIP_MENU_ACTION = [
+      {
+        caption: "Sort Ascending",
+        icon: ArrowDown,
+        iconColor: "gray",
+        onClick: () => {
+          handleSortAsc();
+          setIsOpen(null);
+        },
+      },
+      {
+        caption: "Sort Descending",
+        icon: ArrowUp,
+        iconColor: "gray",
+        onClick: () => {
+          handleSortDesc();
+          setIsOpen(null);
+        },
+      },
+      {
+        caption: "Reset Sorting",
+        icon: RefreshCw,
+        iconColor: "gray",
+        onClick: handleResetSort,
+      },
+    ];
+
+    const handleItemsSelected = (data: RowData[]) => {
+      console.log("Selected rows:", data);
+    };
+
+    return (
+      <div className="flex flex-col gap-4">
+        <h3 className="font-semibold text-xl font-mono">
+          Data Load Balancer 2025
+        </h3>
+        <Toolbar className="w-fit">
+          <Toolbar.Menu
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            className="min-w-[235px]"
+            onClick={() => {
+              console.log("good for click");
+              alert("Your button clickked!");
+            }}
+            caption="Sorted by"
+            iconColor="red"
+            subMenuList={TIP_MENU_ACTION}
+          />
+        </Toolbar>
+
+        <Table
+          selectable
+          columns={columns}
+          onItemsSelected={handleItemsSelected}
+        >
+          {rows?.map((data, index) => (
+            <Table.Row key={index} content={[data.name, data.type]} />
+          ))}
+        </Table>
+      </div>
     );
   },
 };
