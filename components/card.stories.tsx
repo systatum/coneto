d702979d52cf039c9db380586e9bb85e0374a7ec
Alimnfll <alim@systatum.com>
2025-05-26 13:48:43 +0700

    Update for card and loading spinner.

--- components/card.stories.tsx
+++ components/card.stories.tsx
@@ -1,7 +1,19 @@
-import React from "react";
 import Card from "./card";
 import { Meta, StoryObj } from "@storybook/react";
 import { expect, within } from "@storybook/test";
+import {
+  Archive,
+  Ban,
+  CheckCircle,
+  Download,
+  Link,
+  MessageCircleCode,
+  OctagonMinus,
+  Pencil,
+  Send,
+  ShieldOff,
+} from "lucide-react";
+import { Toolbar } from "./toolbar";
 
 const meta: Meta<typeof Card> = {
   title: "Content/Card",
@@ -59,17 +71,96 @@ const meta: Meta<typeof Card> = {
 
 export default meta;
 
-type Story = StoryObj<typeof Card>;
+const TIP_MENU_ITEMS = [
+  {
+    caption: "Report Phishing",
+    icon: Ban,
+    iconColor: "blue",
+    onClick: () => console.log("Junk reported"),
+  },
+  {
+    caption: "Report Junk",
+    icon: OctagonMinus,
+    iconColor: "red",
+    onClick: () => console.log("Junk reported"),
+  },
+  {
+    caption: "Block Sender",
+    icon: ShieldOff,
+    iconColor: "orange",
+    isDangerous: true,
+    onClick: () => console.log("Sender blocked"),
+  },
+  {
+    caption: "Mark as Read",
+    icon: CheckCircle,
+    iconColor: "green",
+    onClick: () => console.log("Marked as read"),
+  },
+  {
+    caption: "Move to Spam",
+    icon: Archive,
+    iconColor: "purple",
+    onClick: () => console.log("Moved to spam"),
+  },
+  {
+    caption: "Download Attachment",
+    icon: Download,
+    iconColor: "teal",
+    onClick: () => console.log("Downloading"),
+  },
+  {
+    caption: "Copy Link",
+    icon: Link,
+    iconColor: "gray",
+    onClick: () => console.log("Link copied"),
+  },
+  {
+    caption: "Share",
+    icon: Send,
+    iconColor: "indigo",
+    isDangerous: true,
+    onClick: () => console.log("Shared"),
+  },
+  {
+    caption: "Edit",
+    icon: Pencil,
+    iconColor: "yellow",
+    onClick: () => console.log("Edit mode"),
+  },
+];
 
-const renderCard = (args) => <Card {...args}>{args.children}</Card>;
+type Story = StoryObj<typeof Card>;
 
 export const Default: Story = {
   args: {
     shadow: "sm",
     padding: "sm",
-    children: "Card with full rounded corners",
   },
-  render: renderCard,
+  render: () => {
+    return (
+      <Card>
+        <Toolbar>
+          <Toolbar.Menu
+            className="min-w-[210px]"
+            openOn="hover"
+            caption="Toolbar Hover Mode"
+            icon={MessageCircleCode}
+            iconColor="red"
+            subMenuList={TIP_MENU_ITEMS}
+          />
+          <Toolbar.Menu
+            className="min-w-[210px]"
+            caption="Toolbar Click Mode"
+            icon={MessageCircleCode}
+            iconColor="white"
+            variant="primary"
+            subMenuList={TIP_MENU_ITEMS}
+          />
+        </Toolbar>
+      </Card>
+    );
+  },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const cardContent = await canvas.getByText(
