337dbb3a3831d39e77cc1e0e940f7690435ab49a
Alimnfll <alim@systatum.com>
2025-06-13 18:50:22 +0700

    Update for string to color extracted.

--- lib/code-color.ts
+++ lib/code-color.ts
@@ -1,12 +1,14 @@
-export function getCode(fullname: string): number {
+export function getCode(str: string): number {
   let code = 0;
-  for (const char of fullname) {
+  for (const char of str) {
     code += char.codePointAt(0) ?? 0;
   }
   return code;
 }
 
-export function getBackground(code: number, backgroundColor: string[]): string {
-  const pos = code % backgroundColor.length;
-  return backgroundColor[pos];
+export function strToColor(str: string, colorSpace: string[]): string {
+  const code = getCode(str);
+
+  const pos = code % colorSpace.length;
+  return colorSpace[pos];
 }
