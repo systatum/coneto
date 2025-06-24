3892bee8b912410651a79c39de6bec2a9b02fe80
Alimnfll <alim@systatum.com>
2025-06-24 18:00:53 +0700

    Add Messagebox component and stories.

---
+++ components/messagebox.stories.tsx
@@ -0,0 +1,121 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Messagebox from "./messagebox";
+import * as RemixIcons from "@remixicon/react";
+
+const meta: Meta<typeof Messagebox> = {
+  title: "Content/Messagebox",
+  component: Messagebox,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["primary", "success", "danger", "warning"],
+    },
+    title: {
+      control: "text",
+    },
+    children: {
+      control: "text",
+    },
+    icon: {
+      control: {
+        type: "select",
+      },
+      options: Object.keys(RemixIcons),
+      mapping: RemixIcons,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Messagebox>;
+
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+    title: "Information",
+    children: "This is a primary message box.",
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const Success: Story = {
+  args: {
+    variant: "success",
+    title: "Success",
+    children: "This is a success message.",
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const Danger: Story = {
+  args: {
+    variant: "danger",
+    title: "Error",
+    children: "This is a danger message.",
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const Warning: Story = {
+  args: {
+    variant: "warning",
+    title: "Warning",
+    children: "This is a warning message.",
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const WithCustomIcon: Story = {
+  args: {
+    variant: "primary",
+    title: "Custom Icon",
+    children: "This message uses a custom RemixIcon.",
+    icon: RemixIcons.RiAlarmWarningFill,
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const WithActions: Story = {
+  args: {
+    variant: "success",
+    title: "Success with Actions",
+    children: "This success message includes buttons and links.",
+    actionLinks: [
+      {
+        caption: "Retry",
+        type: "button",
+        onClick: () => alert("Test click"),
+      },
+      {
+        caption: "View Website",
+        type: "link",
+        href: "https://systatum.com",
+      },
+    ],
+  },
+  render: (args) => <Messagebox {...args} />,
+};
+
+export const WithActionsAndClosable: Story = {
+  args: {
+    variant: "success",
+    title: "Success with Actions",
+    children: "This success message includes buttons and links.",
+    actionLinks: [
+      {
+        caption: "Retry",
+        type: "button",
+        onClick: () => alert("Retry Succeed"),
+      },
+      {
+        caption: "View Website",
+        type: "link",
+        href: "https://systatum.com",
+      },
+    ],
+    closable: true,
+    onClosableRequest: () => alert("Close Request Succeed"),
+  },
+  render: (args) => <Messagebox {...args} />,
+};
---
+++ components/messagebox.tsx
@@ -0,0 +1,132 @@
+import { cn } from "./../lib/utils";
+import {
+  RemixiconComponentType,
+  RiCloseLine,
+  RiInformation2Fill,
+} from "@remixicon/react";
+import { ReactNode } from "react";
+
+interface MessageboxProps {
+  variant?: "primary" | "success" | "danger" | "warning";
+  title: string;
+  icon?: RemixiconComponentType;
+  children: ReactNode;
+  actionLinks?: ActionLinkProps[];
+  closable?: boolean;
+  onClosableRequest?: () => void;
+}
+
+interface ActionLinkProps {
+  caption: string;
+  onClick?: () => void;
+  href?: string;
+  type: "button" | "link";
+}
+
+const VARIANT_MESSAGEBOX_STYLES = {
+  primary: {
+    container: "bg-[rgb(231,242,252)] text-[rgb(42,99,180)]",
+    border: "border-[rgb(42,99,180)]",
+    icon: "text-[rgb(42,99,180)]",
+  },
+  success: {
+    container: "bg-[rgb(233,243,232)] text-[rgb(67,132,61)]",
+    border: "border-[rgb(67,132,61)]",
+    icon: "text-[rgb(67,132,61)]",
+  },
+  danger: {
+    container: "bg-[rgb(246,231,231)] text-[rgb(185,44,37)]",
+    border: "border-[rgb(185,44,37)]",
+    icon: "text-[rgb(185,44,37)]",
+  },
+  warning: {
+    container: "bg-[rgb(251,240,228)] text-[rgb(158,91,32)]",
+    border: "border-[rgb(158,91,32)]",
+    icon: "text-[rgb(158,91,32)]",
+  },
+};
+
+export default function Messagebox({
+  variant = "primary",
+  title,
+  icon: Icon = RiInformation2Fill,
+  children,
+  actionLinks,
+  onClosableRequest,
+  closable = false,
+}: MessageboxProps) {
+  const messageBoxVariant = VARIANT_MESSAGEBOX_STYLES[variant];
+
+  return (
+    <div
+      className={cn(
+        "flex flex-row p-3 py-5 w-full gap-3 relative border-t-2 rounded-xs overflow-hidden h-full",
+        messageBoxVariant.container
+      )}
+    >
+      <div className={cn("absolute", messageBoxVariant.border)} />
+      {Icon && (
+        <Icon className={cn("mt-1", messageBoxVariant.icon)} size={16} />
+      )}
+      <div className="flex flex-col gap-1">
+        <h2 className="font-semibold">{title}</h2>
+        <span className="text-sm">{children}</span>
+        {actionLinks && (
+          <div className="flex flex-row gap-2">
+            {actionLinks?.map((action, index) => {
+              const sharedClasses = cn(
+                "text-sm cursor-pointer font-medium duration-300 transition-all w-fit",
+                messageBoxVariant.icon,
+                {
+                  primary: "hover:underline hover:decoration-[rgb(42,99,180)]",
+                  success: "hover:underline hover:decoration-[rgb(67,132,61)]",
+                  danger: "hover:underline hover:decoration-[rgb(185,44,37)]",
+                  warning: "hover:underline hover:decoration-[rgb(158,91,32)]",
+                }[variant]
+              );
+
+              if (action.type === "button") {
+                return (
+                  <button
+                    key={index}
+                    onClick={action.onClick}
+                    className={sharedClasses}
+                  >
+                    {action.caption}
+                  </button>
+                );
+              }
+
+              return (
+                <a
+                  key={index}
+                  href={action.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={sharedClasses}
+                >
+                  {action.caption}
+                </a>
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      {closable && (
+        <RiCloseLine
+          role="button"
+          aria-label="Closable request"
+          onClick={(e) => {
+            e.stopPropagation();
+            onClosableRequest();
+          }}
+          size={18}
+          className={cn(
+            "absolute top-4 right-3 duration-300 transition-all hover:bg-gray-300 cursor-pointer"
+          )}
+        />
+      )}
+    </div>
+  );
+}
