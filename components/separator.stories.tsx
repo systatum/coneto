c8e7e8728131c8d81b0c618a2e02236c8ac26c80
Alimnfl <mnaufallim234@gmail.com>
2025-07-03 17:45:53 +0700

    Update separator mechanism and stories

--- components/separator.stories.tsx
+++ components/separator.stories.tsx
@@ -5,6 +5,12 @@ const meta: Meta<typeof Separator> = {
   title: "Stage/Separator",
   component: Separator,
   tags: ["autodocs"],
+  argTypes: {
+    depth: {
+      control: "text",
+      description: "How far indented is the text from the base",
+    },
+  },
 };
 
 export default meta;
@@ -12,7 +18,28 @@ export default meta;
 type Story = StoryObj<typeof Separator>;
 
 export const Default: Story = {
-  render: () => {
-    return <Separator title="Test" />;
+  args: {},
+  render: (args) => {
+    return <Separator {...args} />;
+  },
+};
+
+export const WithLeftTitle: Story = {
+  args: {
+    textFloat: "left",
+    title: "systatum.com",
+  },
+  render: (args) => {
+    return <Separator {...args} />;
+  },
+};
+
+export const WithRightTitle: Story = {
+  args: {
+    textFloat: "right",
+    title: "systatum.com",
+  },
+  render: (args) => {
+    return <Separator {...args} />;
   },
 };
--- components/separator.tsx
+++ components/separator.tsx
@@ -3,15 +3,35 @@ import { cn } from "./../lib/utils";
 interface SeparatorProps {
   title?: string;
   className?: string;
+  textFloat?: "left" | "right";
+  depth?: string;
 }
 
-export default function Separator({ title, className }: SeparatorProps) {
+export default function Separator({
+  title,
+  className,
+  textFloat = "left",
+  depth = "20px",
+}: SeparatorProps) {
   return (
-    <div className={cn("flex w-full relative", className)}>
-      <span className="h-[2px] absolute w-full top-1 rounded-sm bg-gray-900 shadow-[inset_0_2px_2px_#ffffff,inset_0_-1px_1px_#7a7a7a]"></span>
-      <h2 className="font-medium text-gray-500 absolute -top-2 bg-white left-3 px-2">
+    <div className={cn("flex text-gray-500 w-full relative", className)}>
+      <span className="h-[2px] absolute w-full rounded-sm bg-gray-900 shadow-[inset_0_2px_2px_#ffffff,inset_0_-1px_1px_#7a7a7a]"></span>
+      <span
+        className={cn(
+          "font-medium absolute -top-1/2 -translate-y-1/2 bg-white px-2"
+        )}
+        style={
+          textFloat === "left"
+            ? {
+                left: depth,
+              }
+            : {
+                right: depth,
+              }
+        }
+      >
         {title}
-      </h2>
+      </span>
     </div>
   );
 }
