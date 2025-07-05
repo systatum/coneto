033c73e8d9da31faa06dbf744104ff9376947cd3
Alimnfl <mnaufallim234@gmail.com>
2025-07-05 13:18:07 +0700

    Update error slate stories with args (feature of storybook).

--- components/error-slate.stories.tsx
+++ components/error-slate.stories.tsx
@@ -12,62 +12,43 @@ export default meta;
 type Story = StoryObj<typeof ErrorSlate>;
 
 export const Default: Story = {
-  render: () => {
-    return (
-      <ErrorSlate code="404" title="PageNotFound">
-        <div className="max-w-[400px] text-center">
-          We couldn't find the page you were looking for. Try searching or&nbsp;
-          <a href="/" className="underline">
-            return to the homepage
-          </a>
-        </div>
-      </ErrorSlate>
-    );
+  args: {
+    code: "404",
+    title: "PageNotFound",
+    children: (
+      <div className="max-w-[400px] text-center">
+        We couldn't find the page you were looking for. Try searching or&nbsp;
+        <a href="/" className="underline">
+          return to the homepage
+        </a>
+      </div>
+    ),
+  },
+  render: (args) => {
+    return <ErrorSlate {...args} />;
   },
 };
 
-export const Forbidden: Story = {
-  render: () => {
-    return (
-      <ErrorSlate
-        code="403"
-        title="AccessDenied"
-        cubeFaceStyle={{
-          background: "gold",
-          borderColor: "black",
-          color: "black",
-        }}
-      >
-        <div className="max-w-[400px] text-center">
-          You don't have permission to view this page. Please contact the
-          administrator or&nbsp;
-          <a href="/" className="underline">
-            return to the homepage
-          </a>
-        </div>
-      </ErrorSlate>
-    );
+export const CustomColor: Story = {
+  args: {
+    code: "403",
+    title: "AccessDenied",
+    cubeFaceStyle: {
+      background: "gold",
+      borderColor: "black",
+      color: "black",
+    },
+    children: (
+      <div className="max-w-[400px] text-center">
+        You don't have permission to view this page. Please contact the
+        administrator or&nbsp;
+        <a href="/" className="underline">
+          return to the homepage
+        </a>
+      </div>
+    ),
   },
-};
-
-export const ServerError: Story = {
-  render: () => {
-    return (
-      <ErrorSlate
-        code="500"
-        title="SomethingWrong"
-        cubeFaceStyle={{
-          background: "blue",
-          borderColor: "black",
-        }}
-      >
-        <div className="max-w-[400px] text-center">
-          Oops! Something went wrong on our end. Please try again later or&nbsp;
-          <a href="/" className="underline">
-            go back to the homepage
-          </a>
-        </div>
-      </ErrorSlate>
-    );
+  render: (args) => {
+    return <ErrorSlate {...args} />;
   },
 };
