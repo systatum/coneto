00d0c5c6a7a1398a6dfa3bc7b6e9e2e47a8f1543
Alimnfl <mnaufallim234@gmail.com>
2025-07-09 18:31:06 +0700

    Update for treelist action, make on stories and compoonent

--- components/treelist.stories.tsx
+++ components/treelist.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { TreeList } from "./treelist";
+import { TreeList, TreeListActionsProps } from "./treelist";
+import { RiAtLine, RiSearchLine } from "@remixicon/react";
 
 const meta: Meta<typeof TreeList> = {
   title: "Content/TreeList",
@@ -13,24 +14,24 @@ type Story = StoryObj<typeof TreeList>;
 
 export const Default: Story = {
   render: () => {
-    const setPerson = (item: { id: number; label: string }) => {
-      console.log("Clicked person:", item.label);
+    const setPerson = (item: { id: number; title: string }) => {
+      console.log("Clicked person:", item.title);
     };
 
     const TREE_LIST_DATA = [
       {
-        label: "Member of Technical Staff",
+        title: "Member of Technical Staff",
         items: [
-          { id: 1, label: "Adam Noto Hakarsa", onClick: setPerson },
-          { id: 2, label: "Mohamad Naufal Alim", onClick: setPerson },
+          { id: 1, title: "Adam Noto Hakarsa", onClick: setPerson },
+          { id: 2, title: "Mohamad Naufal Alim", onClick: setPerson },
         ],
       },
       {
-        label: "Product Management Team",
+        title: "Product Management Team",
         items: [
-          { id: 1, label: "Samantha Lee", onClick: setPerson },
-          { id: 2, label: "Jason Kim", onClick: setPerson },
-          { id: 3, label: "Rina Patel", onClick: setPerson },
+          { id: 1, title: "Samantha Lee", onClick: setPerson },
+          { id: 2, title: "Jason Kim", onClick: setPerson },
+          { id: 3, title: "Rina Patel", onClick: setPerson },
         ],
       },
     ];
@@ -39,6 +40,63 @@ export const Default: Story = {
       <div className="flex flex-col gap-1 max-w-[250px]">
         <TreeList
           content={TREE_LIST_DATA}
+          emptySlate={
+            <div className="text-sm font-semibold w-full items-center flex">
+              Not found.
+            </div>
+          }
+        />
+      </div>
+    );
+  },
+};
+
+export const WithActions: Story = {
+  render: () => {
+    const setPerson = (item: { id: number; title: string }) => {
+      console.log("Clicked person:", item.title);
+    };
+
+    const TREE_LIST_DATA = [
+      {
+        title: "Member of Technical Staff",
+        items: [
+          { id: 1, title: "Adam Noto Hakarsa", onClick: setPerson },
+          { id: 2, title: "Mohamad Naufal Alim", onClick: setPerson },
+        ],
+      },
+      {
+        title: "Product Management Team",
+        items: [
+          { id: 1, title: "Samantha Lee", onClick: setPerson },
+          { id: 2, title: "Jason Kim", onClick: setPerson },
+          { id: 3, title: "Rina Patel", onClick: setPerson },
+        ],
+      },
+    ];
+
+    const TREE_LIST_ACTIONS: TreeListActionsProps[] = [
+      {
+        title: "Discover",
+        onClick: () => {
+          alert("Discover clicked");
+        },
+        icon: RiSearchLine,
+      },
+      {
+        title: "Mention",
+        onClick: () => {
+          alert("Mention clicked");
+        },
+        icon: RiAtLine,
+      },
+    ];
+
+    return (
+      <div className="flex flex-col gap-1 max-w-[250px]">
+        <TreeList
+          content={TREE_LIST_DATA}
+          actions={TREE_LIST_ACTIONS}
           emptySlate={
             <div className="text-sm font-semibold w-full items-center flex">
               Not found.
--- components/treelist.tsx
+++ components/treelist.tsx
@@ -1,20 +1,29 @@
 import { ReactNode, useState } from "react";
 import { cn } from "./../lib/utils";
+import { RemixiconComponentType } from "@remixicon/react";
 
 interface TreeListProps<T extends TreeListItemsProps> {
   content: Array<{
-    label: string;
+    title?: string;
     items: T[];
   }>;
   containerClassName?: string;
   children?: ReactNode;
   emptySlate?: ReactNode;
   searchTerm?: string;
+  actions?: TreeListActionsProps[];
+}
+
+export interface TreeListActionsProps {
+  title?: string;
+  onClick?: () => void;
+  icon?: RemixiconComponentType;
+  className?: string;
 }
 
 interface TreeListItemsProps {
   id: number;
-  label: string;
+  title: string;
   onClick: (item?: TreeListItemsProps) => void;
   setIsSelected?: (item?: string) => void;
   isSelected?: string;
@@ -26,6 +35,7 @@ function TreeList<T extends TreeListItemsProps>({
   children,
   emptySlate,
   searchTerm = "",
+  actions,
 }: TreeListProps<T>) {
   const [isSelected, setIsSelected] = useState("");
 
@@ -33,10 +43,44 @@ function TreeList<T extends TreeListItemsProps>({
 
   return (
     <div className={treeListClass}>
+      {actions && (
+        <div className="flex flex-col w-full gap-1">
+          {actions.map((data, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                if (data.onClick) {
+                  data.onClick();
+                }
+              }}
+              className={cn(
+                "flex flex-row relative hover:bg-gray-100 w-full items-center cursor-pointer px-3 py-1 gap-2",
+                data.className
+              )}
+            >
+              {data.icon && (
+                <span>
+                  <data.icon size={16} />
+                </span>
+              )}
+              <div>{data.title}</div>
+            </div>
+          ))}
+        </div>
+      )}
+      {content && actions && (
+        <div
+          aria-label="divider"
+          className="w-full h-px border-b border-gray-300"
+        />
+      )}
+
       {content.length > 0 ? (
         content.map((data, index) => (
           <div key={index} className="flex flex-col gap-1">
-            <span className={"font-medium px-4 py-[2px]"}>{data.label}</span>
+            {data.title && (
+              <span className={"font-medium px-4 py-[2px]"}>{data.title}</span>
+            )}
             <div className="flex flex-col">
               {data.items.map((val) => (
                 <TreeListItem
@@ -51,6 +95,7 @@ function TreeList<T extends TreeListItemsProps>({
       ) : (
         <div>{emptySlate}</div>
       )}
+
       {children}
     </div>
   );
@@ -64,11 +109,11 @@ function TreeListItem<T extends TreeListItemsProps>({
   searchTerm?: string;
 }) {
   const regex = new RegExp(`(${searchTerm})`, "i");
-  const parts = item.label.split(regex);
+  const parts = item.title.split(regex);
 
   const treeListItemClass = cn(
     "cursor-pointer border-l-3 border-transparent hover:bg-gray-100 py-1 px-4",
-    item.isSelected === item.label
+    item.isSelected === item.title
       ? "bg-gray-100 border-blue-500"
       : "border-transparent"
   );
@@ -78,7 +123,7 @@ function TreeListItem<T extends TreeListItemsProps>({
       className={treeListItemClass}
       onClick={() => {
         item.onClick(item);
-        item.setIsSelected(item.label);
+        item.setIsSelected(item.title);
       }}
     >
       {parts.map((part, index) =>
