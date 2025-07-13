0875d5232fc5b33405f6487e58528e6ad7fef10f
Alimnfl <mnaufallim234@gmail.com>
2025-07-13 18:53:14 +0700

    Update for title and favicon.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,9 +2,9 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
 
 addons.setConfig({
-  title: "Storybook",
   theme: create({
     base: "dark",
     brandImage: "/systatum.png",
+    brandTitle: "Coneto Systatum",
   }),
 });
