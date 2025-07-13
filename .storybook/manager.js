1cb4c4a89b18b806fe04f9c886b09a8a920c7f51
Alimnfl <mnaufallim234@gmail.com>
2025-07-13 19:16:18 +0700

    Update for manager head html

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,9 +2,12 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
 
 addons.setConfig({
+  title: "Coneto Systatum",
   theme: create({
     base: "dark",
     brandImage: "/systatum.png",
     brandTitle: "Coneto Systatum",
   }),
 });
+
+document.title = "Coneto Systatum";
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -15,4 +15,18 @@ const preview: Preview = {
   },
 };
 
+if (typeof window !== "undefined") {
+  window.addEventListener("DOMContentLoaded", () => {
+    const hash = window.location.hash.split("/")[1] ?? "";
+    const storyId = hash.split("--")[0] ?? "Component";
+
+    const formattedTitle = storyId
+      .split("-")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+
+    document.title = `Coneto Systatum – ${formattedTitle}`;
+  });
+}
+
 export default preview;
