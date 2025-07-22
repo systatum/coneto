c51ab25f3f027bcad5d293ea8bebce7426759225
Alimnfl <mnaufallim234@gmail.com>
2025-07-22 20:08:05 +0700

    Add stories dormant with moneybox, datebox, colorbox.

--- components/dormant-text.stories.tsx
+++ components/dormant-text.stories.tsx
@@ -6,6 +6,9 @@ import { Textbox } from "./textbox";
 import { OptionsProps } from "./selectbox";
 import { Combobox } from "./combobox";
 import { expect, userEvent, within } from "@storybook/test";
+import { Datebox } from "./datebox";
+import { Colorbox, ColorPickProps } from "./colorbox";
+import { formatNumber, Moneybox } from "./moneybox";
 
 const meta: Meta<typeof DormantText> = {
   title: "Stage/DormantText",
@@ -29,6 +32,7 @@ export const Default: Story = {
     const [value, setValue] = useState({
       normal: "Hello there, this is dormanted text",
       full: "Hello there, this is dormanted text with full width",
+      keydown: "Hello there, this is dormanted text with enable keydown",
     });
 
     const handleChange = (
@@ -67,6 +71,22 @@ export const Default: Story = {
             <Textbox
               value={value.full}
               onChange={(e) => handleChange(e, "full")}
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Enable Enter KeyDown</span>
+          <DormantText
+            fullWidth
+            enableKeyDown
+            content={value.keydown}
+            onActionClick={() => {
+              console.log(`The value is : ${value.keydown}`);
+            }}
+          >
+            <Textbox
+              value={value.keydown}
+              onChange={(e) => handleChange(e, "keydown")}
             />
           </DormantText>
         </div>
@@ -115,6 +135,7 @@ export const WithCombobox: Story = {
     const [value, setValue] = useState({
       normal: { text: "Apple", value: 1 },
       full: { text: "Banana", value: 2 },
+      keydown: { text: "Orange", value: 3 },
     });
 
     const FRUIT_OPTIONS = [
@@ -169,6 +190,28 @@ export const WithCombobox: Story = {
             />
           </DormantText>
         </div>
+        <div className="flex flex-col">
+          <span className="font-medium">
+            Enable Enter KeyDown & Enable Click
+          </span>
+          <DormantText
+            fullWidth
+            enableKeyDown
+            enableClick
+            content={value.keydown.text}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.keydown.value}`);
+            }}
+          >
+            <Combobox
+              placeholder="Select a fruit keydown..."
+              strict
+              inputValue={value.keydown}
+              options={FRUIT_OPTIONS}
+              setInputValue={(e) => handleChange(e, "keydown")}
+            />
+          </DormantText>
+        </div>
       </div>
     );
   },
@@ -202,5 +245,283 @@ export const WithCombobox: Story = {
 
     const checkButton2 = await canvas.findByRole("button");
     await userEvent.click(checkButton2);
+  },
+};
+
+export const WithDatebox: Story = {
+  parameters: {
+    layout: "padded",
+  },
+
+  render: () => {
+    const [value, setValue] = useState({
+      normal: { text: "07/25/2025", value: "07/25/2025" },
+      full: { text: "07/25/2025", value: "07/25/2025" },
+      keydown: { text: "07/25/2025", value: "07/25/2025" },
+    });
+
+    const DAY_NAMES = [
+      { text: "Su", value: 1 },
+      { text: "Mo", value: 2 },
+      { text: "Tu", value: 3 },
+      { text: "We", value: 4 },
+      { text: "Th", value: 5 },
+      { text: "Fr", value: 6 },
+      { text: "Sa", value: 7 },
+    ];
+
+    const MONTH_NAMES = [
+      { text: "JAN", value: 1 },
+      { text: "FEB", value: 2 },
+      { text: "MAR", value: 3 },
+      { text: "APR", value: 4 },
+      { text: "MAY", value: 5 },
+      { text: "JUN", value: 6 },
+      { text: "JUL", value: 7 },
+      { text: "AUG", value: 8 },
+      { text: "SEP", value: 9 },
+      { text: "OCT", value: 10 },
+      { text: "NOV", value: 11 },
+      { text: "DEC", value: 12 },
+    ];
+
+    const handleChange = (e: OptionsProps, name: string) => {
+      const newValue = e;
+      setValue((prev) => ({ ...prev, [name]: newValue }));
+    };
+
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col">
+          <span className="font-medium">Normal Width</span>
+          <DormantText
+            content={value.normal.text}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.normal.value}`);
+            }}
+          >
+            <Datebox
+              inputValue={value.normal}
+              dayNames={DAY_NAMES}
+              monthNames={MONTH_NAMES}
+              setInputValue={(e) => handleChange(e, "normal")}
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Full Width</span>
+          <DormantText
+            fullWidth
+            content={value.full.text}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.full.value}`);
+            }}
+          >
+            <Datebox
+              inputValue={value.full}
+              dayNames={DAY_NAMES}
+              monthNames={MONTH_NAMES}
+              setInputValue={(e) => handleChange(e, "full")}
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Enable Clicked</span>
+          <DormantText
+            fullWidth
+            enableClick
+            content={value.keydown.text}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.keydown.value}`);
+            }}
+          >
+            <Datebox
+              inputValue={value.keydown}
+              dayNames={DAY_NAMES}
+              monthNames={MONTH_NAMES}
+              setInputValue={(e) => handleChange(e, "keydown")}
+            />
+          </DormantText>
+        </div>
+      </div>
+    );
+  },
+};
+
+export const WithColorbox: Story = {
+  parameters: {
+    layout: "padded",
+  },
+
+  render: () => {
+    const [value, setValue] = useState({
+      normal: "#zzzzzz",
+      full: "#zzzzzz",
+      keydown: "#zzzzzz",
+    });
+
+    const onChangeValue = (
+      e: ChangeEvent<HTMLInputElement>,
+      data: ColorPickProps
+    ) => {
+      const { name, value } = e.target;
+
+      function isValidHexColor(value: string): boolean {
+        const hexRegex = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+        return hexRegex.test(value);
+      }
+
+      if (data === "color-picker") {
+        const isValidHex = isValidHexColor(value);
+        setValue((prev) => ({
+          ...prev,
+          [name]: value,
+          showError: !isValidHex,
+          errorMessage: isValidHex ? "" : "Invalid color value.",
+        }));
+      } else if (data === "color-text") {
+        let val = value;
+        if (!val.startsWith("#")) {
+          val = "#" + val;
+        }
+        const isValidHex = isValidHexColor(val);
+        setValue((prev) => ({
+          ...prev,
+          [name]: val,
+          showError: !isValidHex,
+          errorMessage: isValidHex ? "" : "Invalid color value.",
+        }));
+      }
+    };
+
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col">
+          <span className="font-medium">Normal Width</span>
+          <DormantText
+            content={value.normal}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.normal}`);
+            }}
+          >
+            <Colorbox
+              value={value.normal}
+              name="Full"
+              onChange={onChangeValue}
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Full Width</span>
+          <DormantText
+            fullWidth
+            content={value.full}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.full}`);
+            }}
+          >
+            <Colorbox value={value.full} name="full" onChange={onChangeValue} />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Enable Click & Enter Keydown</span>
+          <DormantText
+            fullWidth
+            enableClick
+            enableKeyDown
+            content={value.keydown}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.keydown}`);
+            }}
+          >
+            <Colorbox
+              value={value.keydown}
+              name="keydown"
+              onChange={onChangeValue}
+            />
+          </DormantText>
+        </div>
+      </div>
+    );
+  },
+};
+
+export const WithMoneybox: Story = {
+  parameters: {
+    layout: "padded",
+  },
+
+  render: () => {
+    const [value, setValue] = useState({
+      normal: "100000",
+      full: "100000",
+      keydown: "100000",
+    });
+
+    const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setValue((prev) => ({ ...prev, [name]: value }));
+    };
+
+    console.log(value);
+
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col">
+          <span className="font-medium">Normal Width</span>
+          <DormantText
+            content={`$ ${formatNumber(value.normal, "comma")}`}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.normal}`);
+            }}
+          >
+            <Moneybox
+              name="normal"
+              currency="$"
+              value={value.normal}
+              onChange={onChangeValue}
+              separator="comma"
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Full Width</span>
+          <DormantText
+            fullWidth
+            content={`$ ${formatNumber(value.full, "comma")}`}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.full}`);
+            }}
+          >
+            <Moneybox
+              name="full"
+              currency="$"
+              value={value.full}
+              onChange={onChangeValue}
+              separator="comma"
+            />
+          </DormantText>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-medium">Enable Enter Keydown</span>
+          <DormantText
+            fullWidth
+            enableKeyDown
+            content={`$ ${formatNumber(value.keydown || "0", "comma")}`}
+            onActionClick={() => {
+              console.log(`Selected value: ${value.keydown}`);
+            }}
+          >
+            <Moneybox
+              name="keydown"
+              currency="$"
+              value={value.keydown}
+              onChange={onChangeValue}
+              separator="comma"
+            />
+          </DormantText>
+        </div>
+      </div>
+    );
   },
 };
