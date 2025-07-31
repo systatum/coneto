68e73c70fd943807d877014f01e1f7b227bc0331
Alimnfl <mnaufallim234@gmail.com>
2025-07-31 17:08:15 +0700

    Add tooltip customize content.

--- components/tooltip.stories.tsx
+++ components/tooltip.stories.tsx
@@ -1,5 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Tooltip } from "./tooltip";
+import { Button } from "./button";
+import { FormFieldProps, StatefulForm } from "./stateful-form";
+import { ChangeEvent, useState } from "react";
+import { OptionsProps } from "./selectbox";
+import { CountryCodeProps } from "./phonebox";
+import z from "zod";
+import { RiAddBoxLine } from "@remixicon/react";
 
 const meta: Meta<typeof Tooltip> = {
   title: "Content/Tooltip",
@@ -13,13 +20,17 @@ const meta: Meta<typeof Tooltip> = {
       control: "text",
       description: "Value on your Tooltip",
     },
-    className: {
+    drawerClassName: {
       control: "text",
-      description: "Styling on your Tooltip",
+      description: "Styling on your drawer content of Tooltip",
     },
     containerClassName: {
       control: "text",
       description: "Styling on your Parent Tooltip",
+    },
+    arrowClassName: {
+      control: "text",
+      description: "Styling on your arrow Tooltip",
     },
     openOn: {
       control: "text",
@@ -54,7 +65,8 @@ export const Click: Story = {
     openOn: "click",
     children: "This tooltip appears on click",
     containerClassName: "text-blue-600 text-sm cursor-pointer",
-    className: "bg-blue-600 text-white",
+    drawerClassName: "bg-blue-600 text-white",
+    arrowClassName: "bg-blue-600 text-white",
   },
   render: (args) => {
     return <Tooltip {...args} />;
@@ -67,7 +79,10 @@ export const StyledTooltip: Story = {
     openOn: "hover",
     children: "Tooltip with custom styling",
     containerClassName: "text-green-600 text-sm underline decoration-wavy",
-    className: "bg-green-700 text-white shadow-lg rounded px-3 py-1 text-sm",
+    drawerClassName:
+      "bg-green-700 text-white shadow-lg rounded px-3 py-1 text-sm",
+    arrowClassName:
+      "bg-green-700 text-white shadow-lg rounded px-3 py-1 text-sm",
   },
   render: (args) => {
     return <Tooltip {...args} />;
@@ -81,7 +96,8 @@ export const DottedUnderlineTooltip: Story = {
     children: "Tooltip with dotted underline trigger",
     underline: "underline-dot",
     containerClassName: "text-sm",
-    className: "bg-gray-800 text-white",
+    drawerClassName: "bg-gray-800 text-white",
+    arrowClassName: "bg-gray-800 text-white",
   },
   render: (args) => {
     return <Tooltip {...args} />;
@@ -95,7 +111,8 @@ export const NoUnderlineTooltip: Story = {
     children: "Trigger text without underline",
     underline: "transparent",
     containerClassName: "font-semibold text-sm text-red-500",
-    className: "bg-red-600 text-white",
+    drawerClassName: "bg-red-600 text-white",
+    arrowClassName: "bg-red-600 text-white",
   },
   render: (args) => {
     return <Tooltip {...args} />;
@@ -109,9 +126,86 @@ export const BlueUnderlineTooltip: Story = {
     underline: "blue",
     containerClassName: "text-sm",
     children: "Clicked tooltip with blue underline",
-    className: "bg-blue-700 text-white",
+    drawerClassName: "bg-blue-700 text-white",
+    arrowClassName: "bg-blue-700 text-white",
   },
   render: (args) => {
     return <Tooltip {...args} />;
+  },
+};
+
+export const Custom: Story = {
+  render: () => {
+    const [value, setValue] = useState({
+      division_name: "",
+    });
+    const onChangeDivisionEmployeeForm = (
+      e?:
+        | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        | FileList
+        | { target: { name: string; value: CountryCodeProps } }
+        | OptionsProps
+    ) => {
+      if (!e || typeof e !== "object") return;
+
+      if ("target" in e && typeof e.target?.name === "string") {
+        const target = e.target;
+        const { name, value } = target as
+          | HTMLInputElement
+          | HTMLTextAreaElement;
+
+        let updatedValue: string | boolean = value;
+
+        if (target instanceof HTMLInputElement && target.type === "checkbox") {
+          updatedValue = target.checked;
+        }
+
+        setValue((prev) => ({ ...prev, [name]: updatedValue }));
+      }
+    };
+
+    const DIVISION_EMPLOYEE_FIELDS: FormFieldProps[] = [
+      {
+        name: "division_name",
+        title: "Division Name",
+        type: "text",
+        required: true,
+        onChange: onChangeDivisionEmployeeForm,
+      },
+    ];
+
+    const divisionEmployeeSchema = z.object({
+      division_name: z
+        .string()
+        .min(2, "Division name must be at least 2 characters long"),
+    });
+
+    return (
+      <Tooltip
+        containerClassName="w-fit px-3"
+        arrowClassName="bg-black border"
+        drawerClassName="w-fit left-4 border border-gray-200 bg-white text-black"
+        text={
+          <div className="flex flex-row">
+            <RiAddBoxLine />
+            <span>New Division</span>
+          </div>
+        }
+        openOn="click"
+        underline="transparent"
+      >
+        <div className="min-w-[300px] px-2 pt-2 pb-1">
+          <StatefulForm
+            fields={DIVISION_EMPLOYEE_FIELDS}
+            formValues={value}
+            validationSchema={divisionEmployeeSchema}
+            mode="onChange"
+          />
+          <div className="mt-1 flex justify-end">
+            <Button className="text-xs">Save</Button>
+          </div>
+        </div>
+      </Tooltip>
+    );
   },
 };
