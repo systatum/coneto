dad30a509c149f962f012269ae16a0db783b5741
Alimnfl <mnaufallim234@gmail.com>
2025-07-31 16:32:22 +0700

    Update for text use ReactNode, and make arrow possibility for style manually.

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -4,10 +4,11 @@ import clsx from "clsx";
 
 export type TooltipProps = {
   children: ReactNode;
-  text: string;
+  text: ReactNode;
   openOn?: "hover" | "click";
   className?: string;
   containerClassName?: string;
+  arrowClassName?: string;
   underline?: "underline" | "underline-dot" | "transparent" | "blue" | "gray";
 };
 
@@ -15,8 +16,9 @@ function Tooltip({
   children,
   text,
   openOn = "hover",
-  className = "",
+  className,
   containerClassName,
+  arrowClassName,
   underline = "underline",
 }: TooltipProps) {
   const [isOpen, setIsOpen] = useState(false);
@@ -67,28 +69,35 @@ function Tooltip({
 
   return (
     <span ref={tooltipRef} className="relative inline-flex items-center">
-      <span
+      <div
         {...triggerProps}
-        className={cn(tooltipContainerClass, containerClassName)}
+        className={cn(
+          "relative w-fit",
+          tooltipContainerClass,
+          containerClassName
+        )}
       >
         {text}
-      </span>
-      {isOpen && (
-        <div
-          className={cn(
-            "absolute left-[20%] -translate-x-[20%] top-full z-50 mt-2 w-max rounded-xs bg-gray-600 px-2 py-1 text-xs text-white shadow-lg",
-            className
-          )}
-        >
-          <div
-            className={cn(
-              "absolute z-10 -top-1 left-[25%] -translate-x-[25%] h-2 w-2 rotate-45 bg-gray-600",
-              className
-            )}
-          />
-          {children}
-        </div>
-      )}
+        {isOpen && (
+          <>
+            <div
+              aria-label="tooltip-arrow"
+              className={cn(
+                "absolute z-10 top-full mt-1 bg-gray-600 left-[25%] -translate-x-[25%] h-2 w-2 rotate-45",
+                arrowClassName ? arrowClassName : className
+              )}
+            />
+            <div
+              className={cn(
+                "absolute left-0 -translate-x-[20%] top-full z-50 mt-2 w-max rounded-xs bg-gray-600 px-2 py-1 text-xs text-white shadow-lg",
+                className
+              )}
+            >
+              {children}
+            </div>
+          </>
+        )}
+      </div>
     </span>
   );
 }
