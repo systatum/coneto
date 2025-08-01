3c7364b72950f076dc0fecb336541c85ed951c55
Alimnfl <mnaufallim234@gmail.com>
2025-08-01 13:26:47 +0700

    Update typo on launchpad story.

--- components/launchpad.stories.tsx
+++ components/launchpad.stories.tsx
@@ -113,26 +113,19 @@ export const Default: Story = {
     ];
 
     return (
-      <div className="flex flex-row">
-        <Launchpad className="max-w-[520px] w-full">
-          {LAUNCHPAD_SECTIONS.map((data, index) => (
-            <Launchpad.Section
-              gridPreset="1-to-3"
-              title={data.title}
-              key={index}
-            >
-              {data.items.map((item, itemIndex) => (
-                <Launchpad.Section.Item
-                  className="sm:flex-col flex-row items-center sm:px-0 px-4"
-                  key={itemIndex}
-                  {...item}
-                />
-              ))}
-            </Launchpad.Section>
-          ))}
-        </Launchpad>
-        qweqwe
-      </div>
+      <Launchpad className="max-w-[520px] w-full">
+        {LAUNCHPAD_SECTIONS.map((data, index) => (
+          <Launchpad.Section gridPreset="1-to-3" title={data.title} key={index}>
+            {data.items.map((item, itemIndex) => (
+              <Launchpad.Section.Item
+                className="sm:flex-col flex-row items-center sm:px-0 px-4"
+                key={itemIndex}
+                {...item}
+              />
+            ))}
+          </Launchpad.Section>
+        ))}
+      </Launchpad>
     );
   },
 };
