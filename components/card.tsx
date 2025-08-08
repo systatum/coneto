bc535bbe1bbe0a9f889040af4fa9acfb54c8cd92
Alimnfl <mnaufallim234@gmail.com>
2025-08-08 16:41:54 +0700

    Make constantize for global style.

--- components/card.tsx
+++ components/card.tsx
@@ -1,3 +1,8 @@
+import {
+  PADDING_MAP,
+  RADIUS_MAP,
+  SHADOW_MAP,
+} from "./../constants/global-style";
 import { RiCloseLine } from "@remixicon/react";
 import { ReactNode } from "react";
 import styled, { css, CSSProp } from "styled-components";
@@ -31,44 +36,6 @@ export interface CardProps {
   closable?: boolean;
   onCloseRequest?: () => void;
 }
-
-const SHADOW_MAP = {
-  none: "none",
-  sm: "0 1px 2px rgba(0, 0, 0, 0.05)",
-  md: "0 4px 6px rgba(0, 0, 0, 0.1)",
-  lg: "0 10px 15px rgba(0, 0, 0, 0.1)",
-  xl: "0 20px 25px rgba(0, 0, 0, 0.1)",
-  "2xl": "0 25px 50px rgba(0, 0, 0, 0.25)",
-};
-
-const RADIUS_MAP = {
-  none: "0px",
-  xs: "0.125rem",
-  sm: "0.25rem",
-  md: "0.375rem",
-  lg: "0.5rem",
-  xl: "0.75rem",
-  "2xl": "1rem",
-  "3xl": "1.5rem",
-  full: "9999px",
-};
-
-const PADDING_MAP = {
-  none: "0rem",
-  sm: "0.25rem",
-  md: "1rem",
-  lg: "1.5rem",
-  xl: "2rem",
-  "2xl": "2.5rem",
-  "3xl": "3rem",
-  "4": "1rem",
-  "5": "1.25rem",
-  "6": "1.5rem",
-  "7": "1.75rem",
-  "8": "2rem",
-  "9": "2.25rem",
-  "10": "2.5rem",
-};
 
 function Card({
   children,
