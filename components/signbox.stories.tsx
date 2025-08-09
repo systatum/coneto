1aff518ba881fd99f637286072c5ef0f7315646f
Alimnfl <mnaufallim234@gmail.com>
2025-08-09 17:28:36 +0700

    Add component signbox and stories.

---
+++ components/signbox.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Sidebar } from "./sidebar";
+import { Signbox } from "./signbox";
+import { Button } from "./button";
+import { useArgs } from "@storybook/preview-api";
+import { StatefulOnChangeType } from "./stateful-form";
+
+const meta: Meta<typeof Signbox> = {
+  title: "Input Elements/Signbox",
+  component: Sidebar,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Signbox>;
+
+export const Default: Story = {
+  render: (args) => {
+    const [, setArgs] = useArgs();
+
+    const onChangeForm = (e: StatefulOnChangeType) => {
+      if (e && "target" in e) {
+        const { name, value } = e.target;
+
+        setArgs({ ...args, [name]: value });
+      }
+    };
+
+    return (
+      <div
+        style={{
+          padding: 40,
+          display: "flex",
+          flexDirection: "column",
+          gap: 10,
+          maxWidth: 500,
+        }}
+      >
+        <Signbox
+          clearable
+          value={args.value}
+          label="Signature"
+          name={"signature"}
+          onChange={onChangeForm}
+        />
+      </div>
+    );
+  },
+};
---
+++ components/signbox.tsx
@@ -0,0 +1,263 @@
+import { RiCloseLine } from "@remixicon/react";
+import { useRef, useEffect, ChangeEvent, ReactElement, useState } from "react";
+import styled, { CSSProp } from "styled-components";
+
+interface SignboxProps {
+  name?: string;
+  value?: string;
+  clearable?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  label?: string;
+  showError?: boolean;
+  errorMessage?: string;
+  containerStyle?: CSSProp;
+  canvasStyle?: CSSProp;
+}
+
+function Signbox({
+  name = "signature",
+  onChange,
+  clearable,
+  disabled,
+  value,
+  label,
+  showError,
+  errorMessage,
+  containerStyle,
+  canvasStyle,
+}: SignboxProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const isDrawing = useRef(false);
+  const lastPoint = useRef<{ x: number; y: number } | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const resizeCanvas = () => {
+      const rect = canvas.getBoundingClientRect();
+
+      const scale = window.devicePixelRatio || 1;
+
+      canvas.width = rect.width * scale;
+      canvas.height = rect.height * scale;
+      ctx.scale(scale, scale);
+
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.lineCap = "round";
+    };
+
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
+    return () => window.removeEventListener("resize", resizeCanvas);
+  }, []);
+
+  useEffect(() => {
+    if (!value) return;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const drawValue = () => {
+      const rect = canvas.getBoundingClientRect();
+      const scale = window.devicePixelRatio || 1;
+
+      canvas.width = rect.width * scale;
+      canvas.height = rect.height * scale;
+      ctx.scale(scale, scale);
+
+      const img = new Image();
+      img.onload = () => {
+        ctx.clearRect(0, 0, rect.width, rect.height);
+
+        const ratio = Math.min(
+          rect.width / img.width,
+          rect.height / img.height
+        );
+        const x = (rect.width - img.width * ratio) / 2;
+        const y = (rect.height - img.height * ratio) / 2;
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          img.width,
+          img.height,
+          x,
+          y,
+          img.width * ratio,
+          img.height * ratio
+        );
+      };
+      img.src = value;
+    };
+
+    drawValue();
+  }, []);
+
+  const getCoordinates = (e: MouseEvent | TouchEvent) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return { x: 0, y: 0 };
+
+    const rect = canvas.getBoundingClientRect();
+
+    if (e instanceof TouchEvent) {
+      return {
+        x: e.touches[0].clientX - rect.left,
+        y: e.touches[0].clientY - rect.top,
+      };
+    } else {
+      return {
+        x: (e as MouseEvent).clientX - rect.left,
+        y: (e as MouseEvent).clientY - rect.top,
+      };
+    }
+  };
+
+  const startDrawing = (e: MouseEvent | TouchEvent) => {
+    isDrawing.current = true;
+    lastPoint.current = getCoordinates(e);
+  };
+
+  const draw = (e: MouseEvent | TouchEvent) => {
+    if (!isDrawing.current) return;
+
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    const { x, y } = getCoordinates(e);
+    const last = lastPoint.current;
+    if (last) {
+      ctx.beginPath();
+      ctx.moveTo(last.x, last.y);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+    lastPoint.current = { x, y };
+
+    if (canvasRef.current) {
+      const dataURL = canvasRef.current.toDataURL("image/png");
+      if (onChange) {
+        onChange({
+          target: { name: name ?? "signature", value: dataURL },
+        } as ChangeEvent<HTMLInputElement>);
+      }
+    }
+  };
+
+  const stopDrawing = () => {
+    isDrawing.current = false;
+    lastPoint.current = null;
+  };
+
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (canvas && ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    if (onChange) {
+      onChange({
+        target: { name: name ?? "signature", value: "" },
+      } as ChangeEvent<HTMLInputElement>);
+    }
+  };
+
+  const inputId = `signbox`;
+
+  const inputElement: ReactElement = (
+    <SignatureWrapper>
+      <SignatureCanvas
+        $canvasStyle={canvasStyle}
+        ref={canvasRef}
+        width={400}
+        height={200}
+        onMouseDown={(e) => startDrawing(e.nativeEvent)}
+        onMouseMove={(e) => draw(e.nativeEvent)}
+        onMouseUp={stopDrawing}
+        onMouseLeave={stopDrawing}
+        onTouchStart={(e) => startDrawing(e.nativeEvent)}
+        onTouchMove={(e) => draw(e.nativeEvent)}
+        onTouchEnd={stopDrawing}
+      />
+      {clearable && (
+        <SignatureClearable
+          aria-label="signbox-clearable"
+          onClick={() => clearCanvas()}
+          size={18}
+        />
+      )}
+    </SignatureWrapper>
+  );
+
+  return (
+    <InputWrapper $containerStyle={containerStyle} $disabled={disabled}>
+      {label && <label htmlFor={inputId}>{label}</label>}
+      <InputContent>
+        {inputElement}
+        {showError && <ErrorText>{errorMessage}</ErrorText>}
+      </InputContent>
+    </InputWrapper>
+  );
+}
+
+const InputWrapper = styled.div<{
+  $containerStyle?: CSSProp;
+  $disabled?: boolean;
+}>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-size: 0.75rem;
+  width: 100%;
+
+  ${({ $disabled }) => $disabled && `cursor: not-allowed; opacity: 0.5;`}
+  ${({ $containerStyle }) => $containerStyle}
+`;
+
+const InputContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  font-size: 12px;
+`;
+
+const ErrorText = styled.span`
+  color: #dc2626;
+  font-size: 0.75rem;
+`;
+
+const SignatureWrapper = styled.div`
+  position: relative;
+`;
+
+const SignatureCanvas = styled.canvas<{ $canvasStyle?: CSSProp }>`
+  width: 100%;
+  height: 200px;
+  position: relative;
+  border: 1px solid #d1d5db;
+  border-radius: 2px;
+
+  ${({ $canvasStyle }) => $canvasStyle}
+`;
+
+const SignatureClearable = styled(RiCloseLine)`
+  position: absolute;
+  font-size: 12px;
+  top: 14px;
+  right: 14px;
+  cursor: pointer;
+  padding: 2px;
+
+  &&:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+export { Signbox };
