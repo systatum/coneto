f48d37f333d2f9dd7ef06bcab0212217615d507e
Alimnfl <mnaufallim234@gmail.com>
2025-08-28 15:58:19 +0700

    Add for badge string id

--- components/badge.tsx
+++ components/badge.tsx
@@ -5,7 +5,7 @@ import { strToColor } from "./../lib/code-color";
 export type BadgeVariantProps = null | "neutral" | "green" | "yellow" | "red";
 
 export interface BadgeProps {
-  id?: number;
+  id?: string;
   variant?: BadgeVariantProps;
   withCircle?: boolean;
   caption?: string;
@@ -79,6 +79,7 @@ function Badge({
   circleColor,
   onClick,
   badgeStyle,
+  id,
 }: BadgeProps) {
   const { bg: backgroundColorVariant, color: colorVariant } =
     VARIANTS_BADGE[variant];
@@ -110,6 +111,7 @@ function Badge({
 
   return (
     <BadgeWrapper
+      id={String(id)}
       onClick={onClick}
       $backgroundColor={badgeBackgroundColor}
       $textColor={badgeTextColor}
