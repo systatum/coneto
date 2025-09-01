b29dd29b678a9d939d0af88dc64b06d0c3edc09e
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2025-09-01 12:22:16 +0700

    [Improvement] SYST-142: Add new fontSize textColor and hoverColor props to Crumb (#51)
    * Add some props (textColor, fontSize, etc).
    
    * Fix lit bit linter on another component.
    
    * Add story for case one data and customize coloring.
    
    * Add test for crumb new story.
    
    * Add calculate method when fontSize added.
    
    * Add test visit url without navigate and calculate icon.
    
    * Remove unnecessary on table.
    
    * Add label arrow icon on crumb
    
    * Add crumb test with all case.
    
    * Add label crumb item name.
    
    * Add style for html (not closed to corner).
    
    * Update for typo numbering value.
    
    * Update for crumb test.
    
    * Remove unnecessary name on describe.
    
    * Rename crumb context and it.
    
    * Update default crumb test.

--- components/crumb.stories.tsx
+++ components/crumb.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Crumb } from "./crumb";
-import { css } from "styled-components";
 
 const meta: Meta<typeof Crumb> = {
   title: "Controls/Crumb",
@@ -23,12 +22,60 @@ export const Default: Story = {
     ];
 
     return (
+      <Crumb fontSize={14} maxShown={3}>
+        {CRUMB_ITEMS.map((data, index) => (
+          <Crumb.Item path={data.path} key={index}>
+            {data.label}
+          </Crumb.Item>
+        ))}
+      </Crumb>
+    );
+  },
+};
+
+export const Custom: Story = {
+  render: () => {
+    const CRUMB_ITEMS = [
+      { label: "Item 1", path: "/" },
+      { label: "Item 2", path: "/item2" },
+      { label: "Item 3", path: "/item3" },
+      { label: "Item 4", path: "/item4" },
+      { label: "Item 5", path: "/item5" },
+      { label: "Item 6", path: "/item6" },
+    ];
+
+    return (
       <Crumb
-        style={css`
-          font-size: 14px;
-        `}
-        maxShown={3}
+        fontSize={16}
+        maxShown={4}
+        arrowColor="#A78BFA"
+        hoverColor="#EC4899"
+        textColor="#6B7280"
+        lastTextColor="#1E293B"
       >
+        {CRUMB_ITEMS.map((data, index) => (
+          <Crumb.Item path={data.path} key={index}>
+            {data.label}
+          </Crumb.Item>
+        ))}
+      </Crumb>
+    );
+  },
+};
+
+export const OneData: Story = {
+  render: () => {
+    const CRUMB_ITEMS = [
+      { label: "Item 1", path: "/" },
+      { label: "Item 2", path: "/item2" },
+      { label: "Item 3", path: "/item3" },
+      { label: "Item 4", path: "/item4" },
+      { label: "Item 5", path: "/item5" },
+      { label: "Item 6", path: "/item6" },
+    ];
+
+    return (
+      <Crumb fontSize={14} maxShown={1}>
         {CRUMB_ITEMS.map((data, index) => (
           <Crumb.Item path={data.path} key={index}>
             {data.label}
