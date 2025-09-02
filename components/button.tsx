9d8e4fd3acefc7b7eec8d688d1e4d63b33d9c4e0
Alimnfl <mnaufallim234@gmail.com>
2025-09-02 11:30:11 +0700

    Update button component effect similiar style.

--- components/button.tsx
+++ components/button.tsx
@@ -265,6 +265,25 @@ const BaseButton = styled.button<{
       background-color: ${bg};
       color: ${color};
 
+      ${$isOpen &&
+      css`
+        position: relative;
+
+        &::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-shadow:
+            inset 0 0.5px 4px rgba(0, 0, 0, 0.2),
+            inset 0 -0.5px 0.5px ${getActiveColor($variant)};
+          border-radius: inherit;
+          pointer-events: none;
+        }
+      `}
+
       ${underline
         ? css`
             text-decoration: underline;
