41c8f0543c2c81ac40f169950342bf1d53e6ea2a
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2025-09-10 06:12:03 +0700

    [Improvement] SYST-166: Add collapsible to TreeListContentProps (#64)
    * Add collapsible prop.
    
    * Add tree list story using collapsible prop.
    
    * Add test for collapsible e2e.
    
    * Move to left for arrow placement.
    
    * Remove separate collapsible story and test.
    
    * Add treelist test component.
    
    * Update for semantic content list.
    
    * Update for tree list
    
    * Update for treelist using with aria-expanded instead of $isOpen.
    
    * Disable user selection
    
    * Add padding-bottom to list
    
    * Pass in collapsible to set visibility
    
    * Update animate on items wrapper.
    
    ---------
    
    Co-authored-by: Adam <adb819@proton.me>

--- test/e2e/treelist.cy.ts
+++ test/e2e/treelist.cy.ts
@@ -1,8 +1,8 @@
 import { getIdContent } from "test/support/commands";
 
-context("Treelist Component", () => {
-  describe("Default", () => {
-    it("Should highlight selected item", () => {
+describe("Treelist", () => {
+  context("default", () => {
+    it("should highlight selected item", () => {
       cy.visit(getIdContent("content-treelist--default"));
 
       cy.contains("Adam Noto Hakarsa")
@@ -13,10 +13,38 @@ context("Treelist Component", () => {
         .parent()
         .should("have.css", "border-left-color", "rgb(59, 130, 246)");
     });
+
+    it("should toggle collapse and expand items", () => {
+      cy.visit(getIdContent("content-treelist--default"));
+
+      cy.contains("Adam Noto Hakarsa").should("exist");
+
+      cy.contains("Member of Technical Staff").click();
+      cy.contains("Adam Noto Hakarsa").should("not.exist");
+
+      cy.contains("Member of Technical Staff").click();
+      cy.contains("Adam Noto Hakarsa").should("exist");
+    });
+
+    it("should still allow selecting an item when expanded", () => {
+      cy.visit(getIdContent("content-treelist--default"));
+
+      cy.contains("Member of Technical Staff").click();
+      cy.contains("Member of Technical Staff").click();
+
+      cy.contains("Adam Noto Hakarsa")
+        .parent()
+        .should("have.css", "border-left-color", "rgba(0, 0, 0, 0)");
+
+      cy.contains("Adam Noto Hakarsa")
+        .click()
+        .parent()
+        .should("have.css", "border-left-color", "rgb(59, 130, 246)");
+    });
   });
 
-  describe("With Actions", () => {
-    it("Should click item and action button exists", () => {
+  context("with actions", () => {
+    it("should click item and action button exists", () => {
       cy.visit(getIdContent("content-treelist--with-actions"));
 
       cy.findByText("Discover").should("exist").click();
@@ -31,8 +59,8 @@ context("Treelist Component", () => {
     });
   });
 
-  describe("Without Header", () => {
-    it("Should click item and action button exists", () => {
+  context("without header", () => {
+    it("should click item and action button exists", () => {
       cy.visit(getIdContent("content-treelist--with-actions"));
 
       cy.findByText("Discover").should("exist").click();
