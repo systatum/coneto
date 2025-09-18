e89c15ca63ad1b99cc179e97eab0ebedf25f75fe
Alimnfl <mnaufallim234@gmail.com>
2025-09-18 12:47:13 +0700

    Add story with ref rich editor.

--- components/rich-editor.stories.tsx
+++ components/rich-editor.stories.tsx
@@ -1,7 +1,15 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { RichEditor } from "./rich-editor";
-import { useState } from "react";
-import { RiPrinterFill } from "@remixicon/react";
+import { RichEditor, RichEditorRef } from "./rich-editor";
+import { useRef, useState } from "react";
+import {
+  RiDeleteBinLine,
+  RiFileCopyLine,
+  RiPrinterFill,
+} from "@remixicon/react";
+import { Button } from "./button";
+import { css } from "styled-components";
+import { Boxbar } from "./boxbar";
+import { Badge } from "./badge";
 
 const meta: Meta<typeof RichEditor> = {
   title: "Input Elements/RichEditor",
@@ -58,6 +66,111 @@ export const Default: Story = {
             style={{
               padding: 28,
               background: "#D3D3D3",
+            }}
+          >
+            {printValue}
+          </pre>
+        )}
+      </div>
+    );
+  },
+};
+
+export const WithRef: Story = {
+  render: () => {
+    const [value, setValue] = useState("");
+    const [printValue, setPrintValue] = useState("");
+
+    const ref = useRef<RichEditorRef>(null);
+
+    const TIP_MENU_EMAIL = [
+      {
+        caption: "Duplicate",
+        icon: RiFileCopyLine,
+        iconColor: "",
+        onClick: () => console.log("Phishing reported"),
+      },
+      {
+        caption: "Report Junk",
+        icon: RiDeleteBinLine,
+        isDangerous: true,
+        onClick: () => console.log("Junk reported"),
+      },
+    ];
+
+    const BADGE_ITEMS = ["Company Name", "Sender Name", "Sender Email"];
+
+    const TOOLBAR_RIGHT_PANEL_ACTIONS = (
+      <RichEditor.ToolbarButton
+        icon={RiPrinterFill}
+        onClick={() => {
+          setPrintValue(value);
+          console.log(value);
+        }}
+      >
+        Print
+      </RichEditor.ToolbarButton>
+    );
+
+    return (
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <RichEditor
+          ref={ref}
+          onChange={(e) => setValue(e)}
+          value={value}
+          toolbarRightPanel={TOOLBAR_RIGHT_PANEL_ACTIONS}
+        />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            gap: "8px",
+            flexDirection: "row",
+          }}
+        >
+          <Boxbar
+            containerStyle={css`
+              width: 100%;
+            `}
+          >
+            {BADGE_ITEMS.map((data, index) => (
+              <Badge
+                onClick={async () => {
+                  await ref.current?.insertContent(data);
+                }}
+                key={index}
+                withCircle
+                caption={data}
+              />
+            ))}
+          </Boxbar>
+          <div>
+            <Button
+              dropdownStyle={css`
+                min-width: 200px;
+                margin-top: 10px;
+              `}
+              buttonStyle={css`
+                min-height: 40px;
+              `}
+              toggleStyle={css`
+                min-height: 40px;
+              `}
+              subMenuList={TIP_MENU_EMAIL}
+              className="w-fit"
+              tipMenu
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+        {printValue !== "" && (
+          <pre
+            style={{
+              padding: 28,
+              background: "#D3D3D3",
+              wordBreak: "break-word",
+              whiteSpace: "pre-wrap",
             }}
           >
             {printValue}
