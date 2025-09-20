75a54877f277822f716d1f0c01f120521539d251
Alimnfl <mnaufallim234@gmail.com>
2025-09-20 16:57:50 +0700

    Add all test case on story (ranged, multiple no weekend).

--- test/e2e/datebox.cy.ts
+++ test/e2e/datebox.cy.ts
@@ -6,21 +6,25 @@ describe("Datebox", () => {
       cy.visit(getIdContent("input-elements-datebox--default"));
     });
 
-    it("should render click on all day", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("next-month").click();
-      cy.findByLabelText("previous-month").click();
-      cy.findByText("13").click();
+    context("when click next, previous and date", () => {
+      it("should show render date", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("next-month").click();
+        cy.findByLabelText("previous-month").click();
+        cy.findByText("13").click();
+      });
     });
 
-    it("should select date", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("calendar-select-date").click();
-      cy.findByLabelText("combobox-month").click();
-      cy.findByText("JAN").click();
-      cy.findByLabelText("combobox-year").click();
-      cy.findByText("2024").click();
-      cy.findByText("3").click();
+    context("when click month and year", () => {
+      it("should select date", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("JAN").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2024").click();
+        cy.findByText("3").click();
+      });
     });
   });
 
@@ -29,16 +33,18 @@ describe("Datebox", () => {
       cy.visit(getIdContent("input-elements-datebox--no-weekends"));
     });
 
-    it("value doesn't change when clicking a weekend date (weekends are not clickable).", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("next-month").click();
-      cy.findByLabelText("previous-month").click();
-      cy.findByLabelText("calendar-select-date").click();
-      cy.findByLabelText("combobox-month").click();
-      cy.findByText("JUN").click();
-      cy.findByLabelText("combobox-year").click();
-      cy.findByText("2025").click();
-      cy.findByText("22").click();
+    context("when click on the weekend", () => {
+      it("value doesn't change ", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("next-month").click();
+        cy.findByLabelText("previous-month").click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("JUN").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        cy.findByText("22").click();
+      });
     });
   });
 
@@ -49,52 +55,124 @@ describe("Datebox", () => {
 
     const contentClick = ["1", "3", "5", "8"];
 
-    it("should click multiple date", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("calendar-select-date").click();
-      cy.findByLabelText("combobox-month").click();
-      cy.findByText("SEP").click();
-      cy.findByLabelText("combobox-year").click();
-      cy.findByText("2025").click();
-      contentClick.map((data) => {
-        cy.findByText(data).click();
-        cy.findByText(data).should(
-          "have.css",
-          "background-color",
-          "rgb(97, 169, 249)"
-        );
+    context("when click", () => {
+      it("should show multiple date", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        contentClick.map((data) => {
+          cy.findByText(data).click();
+          cy.findByText(data).should(
+            "have.css",
+            "background-color",
+            "rgb(97, 169, 249)"
+          );
+        });
+
+        const expectedContent = "09/01/2025,09/03/2025,09/05/2025,09/08/2025";
+
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContent);
       });
-
-      const expectedContent = "09/01/2025,09/03/2025,09/05/2025,09/08/2025";
-
-      cy.findByPlaceholderText("mm/dd/yyyy").as("input");
-      cy.get("@input").should("have.value", expectedContent);
     });
 
-    it("should select a range using shift+click", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("calendar-select-date").click();
-      cy.findByLabelText("combobox-month").click();
-      cy.findByText("SEP").click();
-      cy.findByLabelText("combobox-year").click();
-      cy.findByText("2025").click();
-      cy.findByText("5").click();
-      cy.findByText("10").click({ shiftKey: true });
-      cy.findByText("1").click({ shiftKey: true });
+    context("when keydown shift and click", () => {
+      it("should show selected date", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        cy.findByText("5").click();
+        cy.findByText("10").click({ shiftKey: true });
+        cy.findByText("1").click({ shiftKey: true });
 
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((day) => {
-        cy.findByText(String(day)).should(
-          "have.css",
-          "background-color",
-          "rgb(97, 169, 249)"
-        );
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((day) => {
+          cy.findByText(String(day)).should(
+            "have.css",
+            "background-color",
+            "rgb(97, 169, 249)"
+          );
+        });
+
+        const expectedContent =
+          "09/01/2025,09/02/2025,09/03/2025,09/04/2025,09/05/2025,09/06/2025,09/07/2025,09/08/2025,09/09/2025,09/10/2025";
+
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContent);
       });
+    });
+  });
 
-      const expectedContent =
-        "09/01/2025,09/02/2025,09/03/2025,09/04/2025,09/05/2025,09/06/2025,09/07/2025,09/08/2025,09/09/2025,09/10/2025";
+  context("multiple no weekends", () => {
+    beforeEach(() => {
+      cy.visit(getIdContent("input-elements-datebox--multiple-no-weekends"));
+    });
 
-      cy.findByPlaceholderText("mm/dd/yyyy").as("input");
-      cy.get("@input").should("have.value", expectedContent);
+    const contentClick = ["1", "3", "5", "8"];
+    const contentClickWeekend = ["7"];
+
+    context("when click", () => {
+      it("should show multiple date but weekend not selected", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        contentClick.map((data) => {
+          cy.findByText(data).click();
+          cy.findByText(data).should(
+            "have.css",
+            "background-color",
+            "rgb(97, 169, 249)"
+          );
+        });
+        contentClickWeekend.map((data) => {
+          cy.findByText("7").click();
+          cy.findByText(data).should(
+            "have.css",
+            "background-color",
+            "rgb(255, 255, 255)"
+          );
+        });
+        const expectedContent = "09/01/2025,09/03/2025,09/05/2025,09/08/2025";
+
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContent);
+      });
+    });
+
+    context("when keydown shift and click", () => {
+      it("should show selected date without weekend", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        cy.findByText("5").click();
+        cy.findByText("10").click({ shiftKey: true });
+        cy.findByText("1").click({ shiftKey: true });
+
+        [1, 2, 3, 4, 5, 8, 9, 10].forEach((day) => {
+          cy.findByText(String(day)).should(
+            "have.css",
+            "background-color",
+            "rgb(97, 169, 249)"
+          );
+        });
+
+        const expectedContent =
+          "09/01/2025,09/02/2025,09/03/2025,09/04/2025,09/05/2025,09/08/2025,09/09/2025,09/10/2025";
+
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContent);
+      });
     });
   });
 
@@ -103,31 +181,69 @@ describe("Datebox", () => {
       cy.visit(getIdContent("input-elements-datebox--ranged"));
     });
 
-    it("should click range date", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.findByLabelText("calendar-select-date").click();
-      cy.findByLabelText("combobox-month").click();
-      cy.findByText("SEP").click();
-      cy.findByLabelText("combobox-year").click();
-      cy.findByText("2025").click();
-      cy.findByText("1").click();
-      cy.findByText("30").click();
-      cy.findByText("1").should(
-        "have.css",
-        "background-color",
-        "rgb(97, 169, 249)"
-      );
+    context("when clicked", () => {
+      it("should show range date", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        cy.findByText("1").click();
+        cy.findByText("30").click();
+        cy.findByText("1").should(
+          "have.css",
+          "background-color",
+          "rgb(97, 169, 249)"
+        );
 
-      cy.findByText("30").should(
-        "have.css",
-        "background-color",
-        "rgb(97, 169, 249)"
-      );
+        cy.findByText("30").should(
+          "have.css",
+          "background-color",
+          "rgb(97, 169, 249)"
+        );
 
-      const expectedContent = "09/01/2025-09/30/2025";
+        const expectedContent = "09/01/2025-09/30/2025";
 
-      cy.findByPlaceholderText("mm/dd/yyyy").as("input");
-      cy.get("@input").should("have.value", expectedContent);
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContent);
+      });
+    });
+  });
+
+  context("ranged no weekends", () => {
+    beforeEach(() => {
+      cy.visit(getIdContent("input-elements-datebox--ranged-no-weekends"));
+    });
+
+    context("when clicked", () => {
+      it("should show range date, without weekend", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.findByLabelText("calendar-select-date").click();
+        cy.findByLabelText("combobox-month").click();
+        cy.findByText("SEP").click();
+        cy.findByLabelText("combobox-year").click();
+        cy.findByText("2025").click();
+        cy.findByText("1").click();
+        cy.findByText("19").click();
+        cy.findByText("1").should(
+          "have.css",
+          "background-color",
+          "rgb(97, 169, 249)"
+        );
+
+        cy.findByText("19").should(
+          "have.css",
+          "background-color",
+          "rgb(97, 169, 249)"
+        );
+
+        const expectedContentNoWeekend =
+          "09/01/2025,09/02/2025,09/03/2025,09/04/2025,09/05/2025,09/08/2025,09/09/2025,09/10/2025,09/11/2025,09/12/2025,09/15/2025,09/16/2025,09/17/2025,09/18/2025,09/19/2025";
+
+        cy.findByPlaceholderText("mm/dd/yyyy").as("input");
+        cy.get("@input").should("have.value", expectedContentNoWeekend);
+      });
     });
   });
 
@@ -136,13 +252,15 @@ describe("Datebox", () => {
       cy.visit(getIdContent("input-elements-datebox--with-footer"));
     });
 
-    it("should show footer", () => {
-      cy.get('[data-type="selectbox"]').click();
-      cy.contains("span", "No weekends");
-      cy.contains(
-        "span",
-        "Unfortunately, due to a driver shortage, please choose delivery dates other than weekends. Thank you."
-      );
+    context("when given", () => {
+      it("should show footer", () => {
+        cy.get('[data-type="selectbox"]').click();
+        cy.contains("span", "No weekends");
+        cy.contains(
+          "span",
+          "Unfortunately, due to a driver shortage, please choose delivery dates other than weekends. Thank you."
+        );
+      });
     });
   });
 });
