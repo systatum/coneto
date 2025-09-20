3cc3b16d9b8a0708cd28b5fe2cdd1d80302edd14
Alimnfl <mnaufallim234@gmail.com>
2025-09-20 16:57:32 +0700

    Adds tory on datebox and calendar.

--- components/datebox.stories.tsx
+++ components/datebox.stories.tsx
@@ -166,7 +166,108 @@ export const Multiple: Story = {
   },
 };
 
+export const MultipleNoWeekends: Story = {
+  render: () => {
+    const DAY_NAMES = [
+      { text: "Su", value: 1 },
+      { text: "Mo", value: 2 },
+      { text: "Tu", value: 3 },
+      { text: "We", value: 4 },
+      { text: "Th", value: 5 },
+      { text: "Fr", value: 6 },
+      { text: "Sa", value: 7 },
+    ];
+
+    const MONTH_NAMES = [
+      { text: "JAN", value: 1 },
+      { text: "FEB", value: 2 },
+      { text: "MAR", value: 3 },
+      { text: "APR", value: 4 },
+      { text: "MAY", value: 5 },
+      { text: "JUN", value: 6 },
+      { text: "JUL", value: 7 },
+      { text: "AUG", value: 8 },
+      { text: "SEP", value: 9 },
+      { text: "OCT", value: 10 },
+      { text: "NOV", value: 11 },
+      { text: "DEC", value: 12 },
+    ];
+
+    const [, setValue] = useState<OptionsProps>({
+      text: "",
+      value: 0,
+    });
+
+    return (
+      <div
+        style={{
+          width: "295px",
+        }}
+      >
+        <Datebox
+          options={DAY_NAMES}
+          setInputValue={setValue}
+          dayNames={DAY_NAMES}
+          monthNames={MONTH_NAMES}
+          selectabilityMode="multiple"
+          disableWeekend
+        />
+      </div>
+    );
+  },
+};
+
 export const Ranged: Story = {
+  render: () => {
+    const DAY_NAMES = [
+      { text: "Su", value: 1 },
+      { text: "Mo", value: 2 },
+      { text: "Tu", value: 3 },
+      { text: "We", value: 4 },
+      { text: "Th", value: 5 },
+      { text: "Fr", value: 6 },
+      { text: "Sa", value: 7 },
+    ];
+
+    const MONTH_NAMES = [
+      { text: "JAN", value: 1 },
+      { text: "FEB", value: 2 },
+      { text: "MAR", value: 3 },
+      { text: "APR", value: 4 },
+      { text: "MAY", value: 5 },
+      { text: "JUN", value: 6 },
+      { text: "JUL", value: 7 },
+      { text: "AUG", value: 8 },
+      { text: "SEP", value: 9 },
+      { text: "OCT", value: 10 },
+      { text: "NOV", value: 11 },
+      { text: "DEC", value: 12 },
+    ];
+
+    const [, setValue] = useState<OptionsProps>({
+      text: "",
+      value: 0,
+    });
+
+    return (
+      <div
+        style={{
+          width: "295px",
+        }}
+      >
+        <Datebox
+          options={DAY_NAMES}
+          setInputValue={setValue}
+          dayNames={DAY_NAMES}
+          monthNames={MONTH_NAMES}
+          selectabilityMode="ranged"
+        />
+      </div>
+    );
+  },
+};
+
+export const RangedNoWeekends: Story = {
   render: () => {
     const DAY_NAMES = [
       { text: "Su", value: 1 },
