8f01cb3a5670c375ef02df1fcde7e35941685eaf
Alimnfl <mnaufallim234@gmail.com>
2025-10-01 12:00:42 +0700

    Add test for stories draggable.

--- test/e2e/table.cy.ts
+++ test/e2e/table.cy.ts
@@ -1,6 +1,70 @@
 import { getIdContent } from "test/support/commands";
 
 describe("Table Component", () => {
+  context("Draggable", () => {
+    beforeEach(() => {
+      cy.visit(getIdContent("content-table--draggable"));
+    });
+
+    context("when dragging and dropped content", () => {
+      it("should move to the last dropped", () => {
+        cy.findAllByLabelText("table-row")
+          .eq(0)
+          .should("contain.text", "Load Balancer 1");
+        cy.findAllByLabelText("table-row")
+          .eq(2)
+          .should("contain.text", "Load Balancer 3");
+
+        const dataTransfer = new DataTransfer();
+
+        cy.findAllByLabelText("draggable-request")
+          .eq(2)
+          .trigger("dragstart", { dataTransfer });
+        cy.findAllByLabelText("draggable-request")
+          .eq(0)
+          .trigger("dragover", { dataTransfer })
+          .trigger("drop", { dataTransfer });
+
+        cy.findAllByLabelText("table-row")
+          .eq(0)
+          .should("contain.text", "Load Balancer 3");
+        cy.findAllByLabelText("table-row")
+          .eq(2)
+          .should("contain.text", "Load Balancer 2");
+      });
+    });
+
+    context("With Row Group", () => {
+      context("when dragging and dropped content", () => {
+        it("should move to the last dropped", () => {
+          cy.findAllByLabelText("table-row")
+            .eq(40)
+            .should("contain.text", "Understanding React 18");
+          cy.findAllByLabelText("table-row")
+            .eq(43)
+            .should("contain.text", "React & Redux Bootcamp");
+
+          const dataTransfer = new DataTransfer();
+
+          cy.findAllByLabelText("draggable-request")
+            .eq(40)
+            .trigger("dragstart", { dataTransfer });
+          cy.findAllByLabelText("draggable-request")
+            .eq(43)
+            .trigger("dragover", { dataTransfer })
+            .trigger("drop", { dataTransfer });
+
+          cy.findAllByLabelText("table-row")
+            .eq(40)
+            .should("contain.text", "TypeScript Deep Dive");
+          cy.findAllByLabelText("table-row")
+            .eq(42)
+            .should("contain.text", "Understanding React 18");
+        });
+      });
+    });
+  });
+
   context("Appendable", () => {
     beforeEach(() => {
       cy.visit(getIdContent("content-table--appendable"));
@@ -8,18 +72,22 @@ describe("Table Component", () => {
 
     const checkbox = () => cy.get("input[type=checkbox]").first();
 
-    it("should select and unselect a row", () => {
-      checkbox().click().should("be.checked");
-      checkbox().click().should("not.be.checked");
+    context("when double click checkbox", () => {
+      it("render selected and unselected checkbox", () => {
+        checkbox().click().should("be.checked");
+        checkbox().click().should("not.be.checked");
+      });
     });
 
-    it("should scroll to bottom, select and count", () => {
-      cy.findByLabelText("table-scroll-container").scrollTo("bottom", {
-        duration: 1000,
-      });
+    context("when scroll to bottom and select", () => {
+      it("renders content and have selected item", () => {
+        cy.findByLabelText("table-scroll-container").scrollTo("bottom", {
+          duration: 1000,
+        });
 
-      checkbox().click().should("be.checked");
-      cy.get('input[type="checkbox"]:checked').should("have.length", 41);
+        checkbox().click().should("be.checked");
+        cy.get('input[type="checkbox"]:checked').should("have.length", 41);
+      });
     });
   });
 
@@ -28,23 +96,29 @@ describe("Table Component", () => {
       cy.visit(getIdContent("content-table--sortable-with-pagination"));
     });
 
-    it("should go to next and previous page", () => {
-      cy.findByLabelText("next-button-pagination").click();
-      cy.contains("Pg. 2").should("exist");
+    context("when click next and previous button", () => {
+      it("should move to another page", () => {
+        cy.findByLabelText("next-button-pagination").click();
+        cy.contains("Pg. 2").should("exist");
 
-      cy.findByLabelText("previous-button-pagination").click();
-      cy.contains("Pg. 1").should("exist");
+        cy.findByLabelText("previous-button-pagination").click();
+        cy.contains("Pg. 1").should("exist");
+      });
     });
 
-    it("should select a row", () => {
-      cy.get("input[type=checkbox]").first().click().should("be.checked");
+    context("when click checkbox", () => {
+      it("should select a row", () => {
+        cy.get("input[type=checkbox]").first().click().should("be.checked");
+      });
     });
 
-    it("should can check if we clicked on Table Row", () => {
-      cy.findByText("Load Balancer 1").click();
-      cy.get("input[type=checkbox]").eq(1).should("be.checked");
-      cy.findAllByText("HTTPS").eq(0).click();
-      cy.get("input[type=checkbox]").eq(2).should("be.checked");
+    context("when click content", () => {
+      it("should render checked", () => {
+        cy.findByText("Load Balancer 1").click();
+        cy.get("input[type=checkbox]").eq(1).should("be.checked");
+        cy.findAllByText("HTTPS").eq(0).click();
+        cy.get("input[type=checkbox]").eq(2).should("be.checked");
+      });
     });
   });
 
@@ -53,10 +127,12 @@ describe("Table Component", () => {
       cy.visit(getIdContent("content-table--with-empty-slate"));
     });
 
-    it("should render empty slate content", () => {
-      cy.contains("Manage your inventory transfers").should("exist");
-      cy.contains("Add Item").should("exist");
-      cy.contains("Learn More").should("exist");
+    context("when empty content", () => {
+      it("should render empty slate content", () => {
+        cy.contains("Manage your inventory transfers").should("exist");
+        cy.contains("Add Item").should("exist");
+        cy.contains("Learn More").should("exist");
+      });
     });
   });
 
@@ -65,23 +141,34 @@ describe("Table Component", () => {
       cy.visit(getIdContent("content-table--with-row-group"));
     });
 
-    it("should render table with grouped rows", () => {
-      cy.contains("Tech Articles").should("exist");
-      cy.get("input[type=checkbox]").first().click().should("be.checked");
-    });
-
-    it("should click top action buttons", () => {
-      cy.contains("Delete").should("be.disabled");
-      cy.contains("Copy").click();
-    });
-
-    it("should display expected actions on tip menu and allow clicking them", () => {
-      cy.findByLabelText("button-toggle").click();
-      const COPY_ACTIONS = ["Copy to parent", "Copy to link"];
-      COPY_ACTIONS.forEach((text) => {
-        cy.contains(text).should("be.visible");
+    context("when click checkbox", () => {
+      it("should render table with grouped rows", () => {
+        cy.contains("Tech Articles").should("exist");
+        cy.get("input[type=checkbox]").first().click().should("be.checked");
       });
-      cy.contains(COPY_ACTIONS[0]).click();
+    });
+
+    context("when click action", () => {
+      it("should clicked button", () => {
+        cy.window().then((win) => {
+          cy.spy(win.console, "log").as("consoleLog");
+        });
+
+        cy.contains("Delete").should("be.disabled");
+        cy.contains("Copy").click();
+        cy.get("@consoleLog").should("have.been.calledWith", "Copy clicked");
+      });
+    });
+
+    context("when click tip menu", () => {
+      it("should display actions on tip and allow to clicking", () => {
+        cy.findByLabelText("button-toggle").click();
+        const COPY_ACTIONS = ["Copy to parent", "Copy to link"];
+        COPY_ACTIONS.forEach((text) => {
+          cy.contains(text).should("be.visible");
+        });
+        cy.contains(COPY_ACTIONS[0]).click();
+      });
     });
   });
 });
