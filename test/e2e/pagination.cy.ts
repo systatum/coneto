8c2bac016b1060a25799faee041894a9e6448d07
Alimnfl <mnaufallim234@gmail.com>
2025-10-03 15:28:20 +0700

    Update test on

--- test/e2e/pagination.cy.ts
+++ test/e2e/pagination.cy.ts
@@ -1,31 +1,118 @@
 import { getIdContent } from "test/support/commands";
 
-context("Pagination Component", () => {
-  describe("Default", () => {
+describe("Pagination", () => {
+  context("default", () => {
     beforeEach(() => {
       cy.visit(getIdContent("controls-pagination--default"));
     });
-    it("Should be click for move page", () => {
-      cy.findByLabelText("next-page").should("be.visible").click();
-      cy.wait(400);
-      cy.findByLabelText("previous-page").should("be.visible").click();
+
+    context("arrow button", () => {
+      context("when pressing arrow right", () => {
+        it("should be move to the next page", () => {
+          cy.findAllByLabelText("pagination-button")
+            .eq(6)
+            .should("be.visible")
+            .click();
+          cy.findAllByLabelText("pagination-button")
+            .eq(1)
+            .should("have.css", "border", "1px solid rgb(243, 244, 246)");
+          cy.findAllByLabelText("pagination-button")
+            .eq(2)
+            .should("have.css", "border", "1px solid rgb(97, 169, 249)");
+        });
+      });
+
+      context("when disabled", () => {
+        it("shouldn't pressing ", () => {
+          cy.findAllByLabelText("pagination-button")
+            .eq(0)
+            .should("be.visible")
+            .click({ force: true });
+          cy.findAllByLabelText("pagination-button")
+            .eq(0)
+            .should("have.css", "border", "1px solid rgb(243, 244, 246)")
+            .and("have.css", "opacity", "0.3");
+          cy.findAllByLabelText("pagination-button")
+            .eq(1)
+            .should("have.css", "border", "1px solid rgb(97, 169, 249)");
+        });
+      });
+    });
+
+    context("when active", () => {
+      it("should have blue border color", () => {
+        cy.findAllByLabelText("pagination-button")
+          .eq(1)
+          .should("have.css", "border", "1px solid rgb(97, 169, 249)");
+        cy.findAllByLabelText("pagination-button")
+          .eq(2)
+          .should("have.css", "border", "1px solid rgb(243, 244, 246)");
+
+        cy.findAllByLabelText("pagination-button")
+          .eq(6)
+          .should("be.visible")
+          .click();
+
+        cy.findAllByLabelText("pagination-button")
+          .eq(1)
+          .should("have.css", "border", "1px solid rgb(243, 244, 246)");
+        cy.findAllByLabelText("pagination-button")
+          .eq(2)
+          .should("have.css", "border", "1px solid rgb(97, 169, 249)");
+      });
     });
   });
 
-  describe("Over Five Pages", () => {
+  context("over five pages", () => {
     beforeEach(() => {
       cy.visit(getIdContent("controls-pagination--over-five-page"));
     });
-    it("Should be select page for ", () => {
-      cy.findByLabelText("next-page").should("be.visible").click();
-      cy.wait(400);
-      cy.findByLabelText("previous-page").should("be.visible").click();
-
-      cy.findByPlaceholderText("1").click();
-      cy.findByRole("listbox").scrollTo("bottom", {
-        duration: 1000,
+    context("combobox", () => {
+      context("when selected", () => {
+        it("should be select page", () => {
+          cy.findByPlaceholderText("1").click();
+          cy.findByRole("listbox").scrollTo("bottom", {
+            duration: 1000,
+          });
+          cy.findByText("46").click();
+          cy.findByPlaceholderText("1").should(
+            "have.css",
+            "border",
+            "1px solid rgb(97, 169, 249)"
+          );
+        });
       });
-      cy.findByText("46").click();
+
+      context("when pressing not include number", () => {
+        it("shouldn't have active style", () => {
+          cy.findByPlaceholderText("1").click();
+          cy.findByRole("listbox").scrollTo("bottom", {
+            duration: 1000,
+          });
+          cy.findByText("46").click();
+          cy.findByPlaceholderText("1").should(
+            "have.css",
+            "border",
+            "1px solid rgb(97, 169, 249)"
+          );
+          cy.findByText("49").click();
+          cy.findByPlaceholderText("1").should(
+            "have.css",
+            "border",
+            "1px solid rgb(243, 244, 246)"
+          );
+        });
+      });
+    });
+
+    context("when pressing button", () => {
+      it("should be select page", () => {
+        cy.findByText("49").click();
+        cy.findByText("50").click();
+        cy.findAllByLabelText("pagination-button")
+          .eq(3)
+          .should("have.css", "border", "1px solid rgb(97, 169, 249)");
+      });
     });
   });
 });
