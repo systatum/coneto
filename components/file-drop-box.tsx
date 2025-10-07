e276de0704922227e0e50c03e0d4c054121c560e
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2025-10-07 10:27:48 +0700

    [Improvement] SYST-230: Remove bolder font-weight on FileDropBox (#97)
    * Update not too bold on FileDropBox
    
    * Remove unnecessary font-weight style

--- components/file-drop-box.tsx
+++ components/file-drop-box.tsx
@@ -340,7 +340,6 @@ const IconsRow = styled.div`
 `;
 
 const PlaceholderText = styled.span<{ $isDragging: boolean }>`
-  font-weight: 600;
   font-size: 1.25rem;
   color: ${(props) => (props.$isDragging ? "#3b82f6" : "#000")};
 `;
