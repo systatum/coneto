198563245c20791ee37cd74af8352fd15024a425
Alimnfl <mnaufallim234@gmail.com>
2025-10-15 10:54:45 +0700

    Rename argument on Imagebox callback.

--- components/imagebox.tsx
+++ components/imagebox.tsx
@@ -6,7 +6,7 @@ export interface ImageboxProps {
   containerStyle?: CSSProp;
   labelStyle?: CSSProp;
   style?: CSSProp;
-  onFileSelected?: (files: File) => void;
+  onFileSelected?: (file: File) => void;
   size?: "xs" | "sm" | "md" | "lg";
   label?: string;
   showError?: boolean;
