31dd9fde34978c7648f5194c63d1ac4960502e17
Alimnfl <mnaufallim234@gmail.com>
2025-10-25 17:56:42 +0700

    Ensure the component test on button using subMenuList.

--- test/component/button.cy.tsx
+++ test/component/button.cy.tsx
@@ -1,3 +1,4 @@
+import styled, { css } from "styled-components";
 import { Button } from "./../../components/button";
 import {
   RiSpam2Line,
@@ -72,67 +73,246 @@ describe("Button", () => {
   ];
 
   context("tipMenu", () => {
-    context("when click", () => {
-      it("renders background color active by variant", () => {
-        cy.mount(
-          <Button
-            variant="default"
-            dropdownStyle={{
-              minWidth: "240px",
-            }}
-            subMenuList={TIP_MENU_ITEMS}
-          >
-            Test
-          </Button>
-        );
-        cy.findByLabelText("button-toggle")
-          .click()
-          .then(($button) => {
-            cy.wrap($button).should(
-              "have.css",
-              "background-color",
-              "rgb(207, 207, 207)"
+    context("when given content", () => {
+      context("when using list", () => {
+        context("when clicked", () => {
+          it("renders the menu list", () => {
+            cy.viewport(800, 700);
+            cy.mount(
+              <Button
+                variant="default"
+                dropdownStyle={{
+                  minWidth: "240px",
+                }}
+                subMenu={({ list }) => list(TIP_MENU_ITEMS)}
+              >
+                Test
+              </Button>
             );
-          });
-      });
-      it("renders content menu", () => {
-        cy.viewport(800, 700);
-        cy.mount(
-          <Button
-            variant="default"
-            dropdownStyle={{
-              minWidth: "240px",
-            }}
-            subMenuList={TIP_MENU_ITEMS}
-          >
-            Test
-          </Button>
-        );
 
-        cy.findByLabelText("button-toggle").click();
-        TIP_MENU_ITEMS.forEach((item) => {
-          cy.contains(item.caption).should("exist");
+            cy.findByLabelText("button-toggle").click();
+            TIP_MENU_ITEMS.forEach((item) => {
+              cy.contains(item.caption).should("exist");
+            });
+          });
+        });
+
+        context("when hovered", () => {
+          it("renders background color based on the variant", () => {
+            cy.mount(
+              <Button
+                variant="default"
+                dropdownStyle={{
+                  minWidth: "240px",
+                }}
+                subMenu={({ list }) => list(TIP_MENU_ITEMS)}
+              >
+                Test
+              </Button>
+            );
+            cy.findByLabelText("button-toggle")
+              .realHover()
+              .wait(300)
+              .should("have.css", "background-color", "rgb(226, 226, 226)");
+          });
+        });
+      });
+
+      context("when using show", () => {
+        it("renders content inside the tip menu container", () => {
+          cy.mount(
+            <Button
+              variant="default"
+              dropdownStyle={{
+                minWidth: "240px",
+              }}
+              subMenu={({ show }) =>
+                show(
+                  <div
+                    style={{
+                      padding: "10px",
+                    }}
+                  >
+                    This button using function show
+                  </div>
+                )
+              }
+            >
+              Test
+            </Button>
+          );
+          cy.findByLabelText("button-toggle")
+            .click()
+            .then(($button) => {
+              cy.wrap($button).should(
+                "have.css",
+                "background-color",
+                "rgb(207, 207, 207)"
+              );
+            });
+          cy.findByText("This button using function show").should("exist");
+          cy.findByLabelText("button-tip-menu-container").should("exist");
+        });
+      });
+
+      context("when using render", () => {
+        it("renders custom content as designed", () => {
+          cy.mount(
+            <Button
+              variant="default"
+              dropdownStyle={{
+                minWidth: "240px",
+              }}
+              subMenu={({ render }) =>
+                render(
+                  <Button.TipMenuContainer
+                    style={css`
+                      padding: 10px;
+                      display: flex;
+                      flex-direction: column;
+                      gap: 4px;
+                    `}
+                  >
+                    <MenuContainer>
+                      <MenuTitle>Information</MenuTitle>
+                      <MenuDescription>
+                        This button uses a render function to show custom
+                        content inside TipMenu.
+                      </MenuDescription>
+                    </MenuContainer>
+                    <Button variant="primary">Got it</Button>
+                  </Button.TipMenuContainer>
+                )
+              }
+            >
+              Test
+            </Button>
+          );
+          cy.findByLabelText("button-toggle")
+            .click()
+            .then(($button) => {
+              cy.wrap($button).should(
+                "have.css",
+                "background-color",
+                "rgb(207, 207, 207)"
+              );
+            });
+          cy.findByText("Information").should("exist");
+          cy.findByText(
+            "This button uses a render function to show custom content inside TipMenu."
+          ).should("exist");
+          cy.findByText("Got it").should("exist");
+          cy.findByLabelText("button-tip-menu-container").should("exist");
         });
       });
     });
-    context("when hover", () => {
-      it("renders background color hover by variant", () => {
-        cy.mount(
-          <Button
-            variant="default"
-            dropdownStyle={{
-              minWidth: "240px",
-            }}
-            subMenuList={TIP_MENU_ITEMS}
-          >
-            Test
-          </Button>
-        );
-        cy.findByLabelText("button-toggle")
-          .realHover()
-          .wait(300)
-          .should("have.css", "background-color", "rgb(226, 226, 226)");
+
+    context("with showSubMenuOn", () => {
+      context("when set to caret", () => {
+        it("renders toggle button", () => {
+          cy.mount(
+            <Button
+              variant="default"
+              dropdownStyle={{
+                minWidth: "240px",
+              }}
+              subMenu={({ render }) =>
+                render(
+                  <Button.TipMenuContainer
+                    style={css`
+                      padding: 10px;
+                      display: flex;
+                      flex-direction: column;
+                      gap: 4px;
+                    `}
+                  >
+                    <MenuContainer>
+                      <MenuTitle>Information</MenuTitle>
+                      <MenuDescription>
+                        This button uses a render function to show custom
+                        content inside TipMenu.
+                      </MenuDescription>
+                    </MenuContainer>
+                    <Button variant="primary">Got it</Button>
+                  </Button.TipMenuContainer>
+                )
+              }
+            >
+              With Caret
+            </Button>
+          );
+          cy.findAllByRole("button").eq(0).should("contain.text", "With Caret");
+          cy.findByLabelText("button-toggle").should("exist").click();
+          cy.findByText("Information").should("exist");
+          cy.findByText(
+            "This button uses a render function to show custom content inside TipMenu."
+          ).should("exist");
+          cy.findByText("Got it").should("exist");
+          cy.findByLabelText("button-tip-menu-container").should("exist");
+        });
+      });
+
+      context("when set to self", () => {
+        it("renders just only a button", () => {
+          cy.mount(
+            <Button
+              variant="default"
+              dropdownStyle={{
+                minWidth: "240px",
+              }}
+              showSubMenuOn="self"
+              subMenu={({ render }) =>
+                render(
+                  <Button.TipMenuContainer
+                    style={css`
+                      padding: 10px;
+                      display: flex;
+                      flex-direction: column;
+                      gap: 4px;
+                    `}
+                  >
+                    <MenuContainer>
+                      <MenuTitle>Information</MenuTitle>
+                      <MenuDescription>
+                        This button uses a render function to show custom
+                        content inside TipMenu.
+                      </MenuDescription>
+                    </MenuContainer>
+                    <Button variant="primary">Got it</Button>
+                  </Button.TipMenuContainer>
+                )
+              }
+            >
+              With Self
+            </Button>
+          );
+          cy.findByRole("button").should("contain.text", "With Self").click();
+          cy.findByText("Information").should("exist");
+          cy.findByText(
+            "This button uses a render function to show custom content inside TipMenu."
+          ).should("exist");
+          cy.findByText("Got it").should("exist");
+
+          cy.findByLabelText("button-toggle").should("not.exist");
+        });
       });
     });
   });
 });
+
+const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+const MenuTitle = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+  color: #111827;
+`;
+
+const MenuDescription = styled.div`
+  font-size: 13px;
+  color: #6b7280;
+`;
