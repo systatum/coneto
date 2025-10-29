358dfa13d8ca8642c0323c893a715b7b430834ff
Alimnfl <mnaufallim234@gmail.com>
2025-10-30 01:40:43 +0700

    Update story with icon.

--- components/capsule.stories.tsx
+++ components/capsule.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Capsule, CapsuleContentProps } from "./capsule";
 import { useArgs } from "@storybook/preview-api";
+import { RiFile2Line, RiNewspaperLine } from "@remixicon/react";
 
 const meta: Meta<typeof Capsule> = {
   title: "Input Elements/Capsule",
@@ -29,6 +30,37 @@ const VIEW_MODES: CapsuleContentProps[] = [
 export const Default: Story = {
   args: {
     tabs: VIEW_MODES,
+    activeTab: "new",
+  },
+  render: (args) => {
+    const [{ activeTab }, setUpdateArgs] = useArgs();
+
+    return (
+      <Capsule
+        {...args}
+        activeTab={activeTab}
+        setActiveTab={(prev: string) => setUpdateArgs({ activeTab: prev })}
+      />
+    );
+  },
+};
+
+const VIEW_WITH_ICON_MODES: CapsuleContentProps[] = [
+  {
+    id: "new",
+    title: "New",
+    icon: RiFile2Line,
+  },
+  {
+    id: "list",
+    title: "List",
+    icon: RiNewspaperLine,
+  },
+];
+
+export const WithIcon: Story = {
+  args: {
+    tabs: VIEW_WITH_ICON_MODES,
     activeTab: "new",
   },
   render: (args) => {
