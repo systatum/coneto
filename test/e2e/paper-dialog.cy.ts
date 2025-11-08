b9134219786ab2b77f83f1e02a8abf36a3da1640
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2025-11-08 15:29:58 +0700

    [Improvement] SYST-274: Support multiple paper dialog in one page (#132)
    * Refactor the card content.
    
    * Update the story using the newest args on the Card component.
    
    * Update the card default and test stories.
    
    * Update story card using the newest default header.
    
    * Update the args was added.
    
    * Remove unnecessary style.
    
    * Update the react mouse event on tip menu.
    
    * Update the card style.
    
    * Add the newest story on the card (with full width).
    
    * Add the action on the story card
    
    * Update the card when hovered color.
    
    * Update the paper dialog style on close.
    
    * Update the paper dialog for multiple.
    
    * Update the story for multiple paper dialog.
    
    * Update the test on paper dialog.
    
    * Update the dialog test and the story.

--- test/e2e/paper-dialog.cy.ts
+++ test/e2e/paper-dialog.cy.ts
@@ -1,8 +1,8 @@
 import { getIdContent } from "test/support/commands";
 
-context("PaperDialog Component", () => {
-  describe("Default", () => {
-    it("Should open the dialog and show the form", () => {
+describe("PaperDialog", () => {
+  context("default", () => {
+    it("should open the dialog and show the form", () => {
       cy.visit(getIdContent("stage-paperdialog--default"));
 
       cy.findByRole("button", { name: /open/i }).click();
@@ -11,7 +11,7 @@ context("PaperDialog Component", () => {
       cy.findByRole("button", { name: /save/i }).should("exist");
     });
 
-    it("Should minimize the dialog", () => {
+    it("should minimize the dialog", () => {
       cy.visit(getIdContent("stage-paperdialog--default"));
 
       cy.findByRole("button", { name: /open/i }).click();
@@ -19,8 +19,8 @@ context("PaperDialog Component", () => {
     });
   });
 
-  describe("Closable", () => {
-    it("Should open and close the dialog using the close icon", () => {
+  context("with closable", () => {
+    it("should open and close the dialog using the close icon", () => {
       cy.visit(getIdContent("stage-paperdialog--closable"));
 
       cy.findByRole("button", { name: /trigger/i }).click();
@@ -32,8 +32,8 @@ context("PaperDialog Component", () => {
     });
   });
 
-  describe("Fixed Left", () => {
-    it("Should open and close the left-positioned dialog", () => {
+  context("with left position", () => {
+    it("should open and close the left-positioned dialog", () => {
       cy.visit(getIdContent("stage-paperdialog--fixed-left"));
 
       cy.findByRole("button", { name: /trigger/i }).click();
@@ -42,6 +42,41 @@ context("PaperDialog Component", () => {
 
       cy.findByLabelText("button-close").click();
       cy.findByText("Add New Employee").should("not.exist");
+    });
+  });
+
+  context("with left position", () => {
+    it("should open and close the left-positioned dialog", () => {
+      cy.visit(getIdContent("stage-paperdialog--fixed-left"));
+
+      cy.findByRole("button", { name: /trigger/i }).click();
+
+      cy.findByText("Add New Employee").should("exist");
+
+      cy.findByLabelText("button-close").click();
+      cy.findByText("Add New Employee").should("not.exist");
+    });
+  });
+
+  context("when nested", () => {
+    it("should render nested dialog", () => {
+      cy.visit(getIdContent("stage-paperdialog--nested"));
+
+      cy.findAllByLabelText("table-row")
+        .eq(0)
+        .trigger("mouseover")
+        .then(() => {
+          cy.wait(100);
+          cy.findAllByRole("button").eq(0).click();
+        });
+
+      cy.wait(100);
+      cy.findByText("Family Registry").should("exist");
+      cy.findByText(
+        "Detailed view of employees and their family registry records"
+      ).should("exist");
+      cy.findByText("Add Family").should("exist").click({ force: true });
+      cy.findByText("Nested").should("exist");
     });
   });
 });
