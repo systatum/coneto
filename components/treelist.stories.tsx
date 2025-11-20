38d8ac236b51e4ad1b0a6d809b3bf72bcdad5577
Alimnfl <mnaufallim234@gmail.com>
2025-11-20 14:09:06 +0700

    Update story using actions item.

--- components/treelist.stories.tsx
+++ components/treelist.stories.tsx
@@ -1,14 +1,25 @@
 import { Meta, StoryObj } from "@storybook/react";
 import {
+  SubMenuTreeList,
   TreeList,
   TreeListActionsProps,
   TreeListContentProps,
   TreeListItemsProps,
 } from "./treelist";
-import { RiAtLine, RiSearchLine } from "@remixicon/react";
+import {
+  RiAtLine,
+  RiDeleteBin2Line,
+  RiEdit2Line,
+  RiFileCopyLine,
+  RiSearchLine,
+  RiShareForwardLine,
+} from "@remixicon/react";
 import { EmptySlate } from "./empty-slate";
 import { Button } from "./button";
 import { css } from "styled-components";
+import { useMemo, useState } from "react";
+import { Combobox } from "./combobox";
+import { OptionsProps } from "./selectbox";
 
 const meta: Meta<typeof TreeList> = {
   title: "Content/TreeList",
@@ -67,20 +78,95 @@ export const WithActions: Story = {
       console.log("Clicked person:", item.caption);
     };
 
+    const ITEM_ACTION = (rowId: string): SubMenuTreeList[] => {
+      return [
+        {
+          caption: "Edit",
+          icon: RiEdit2Line,
+          iconColor: "gray",
+          onClick: () => {
+            console.log(`${rowId} was edited`);
+          },
+        },
+        {
+          caption: "Delete",
+          icon: RiDeleteBin2Line,
+          iconColor: "gray",
+          onClick: () => {
+            console.log(`${rowId} was deleted`);
+          },
+        },
+        {
+          caption: "Copy",
+          icon: RiFileCopyLine,
+          iconColor: "gray",
+          onClick: () => {
+            console.log(`${rowId} was copied`);
+          },
+        },
+        {
+          caption: "Share",
+          icon: RiShareForwardLine,
+          iconColor: "gray",
+          onClick: () => {
+            console.log(`${rowId} was shared`);
+          },
+        },
+      ];
+    };
+
+    const [showItem, setShowItem] = useState({
+      actions: ITEM_ACTION,
+      length: ["1"],
+    });
+
+    const filteredActionsShow = useMemo(() => {
+      return (id: string) => {
+        const itemActions = showItem.actions(id);
+        const itemLength = Number(showItem.length[0]);
+        return itemActions.slice(0, itemLength);
+      };
+    }, [showItem]);
+
     const TREE_LIST_DATA: TreeListContentProps[] = [
       {
         caption: "Member of Technical Staff",
         items: [
-          { id: "mts-1", caption: "Adam Noto Hakarsa", onClick: setPerson },
-          { id: "mts-2", caption: "Mohamad Naufal Alim", onClick: setPerson },
+          {
+            id: "mts-1",
+            caption: "Adam Noto Hakarsa",
+            onClick: setPerson,
+            actions: filteredActionsShow,
+          },
+          {
+            id: "mts-2",
+            caption: "Mohamad Naufal Alim",
+            onClick: setPerson,
+            actions: filteredActionsShow,
+          },
         ],
       },
       {
         caption: "Product Management Team",
         items: [
-          { id: "pmt-1", caption: "Samantha Lee", onClick: setPerson },
-          { id: "pmt-2", caption: "Jason Kim", onClick: setPerson },
-          { id: "pmt-3", caption: "Rina Patel", onClick: setPerson },
+          {
+            id: "pmt-1",
+            caption: "Samantha Lee",
+            onClick: setPerson,
+            actions: filteredActionsShow,
+          },
+          {
+            id: "pmt-2",
+            caption: "Jason Kim",
+            onClick: setPerson,
+            actions: filteredActionsShow,
+          },
+          {
+            id: "pmt-3",
+            caption: "Rina Patel",
+            onClick: setPerson,
+            actions: filteredActionsShow,
+          },
         ],
       },
     ];
@@ -102,16 +188,46 @@ export const WithActions: Story = {
       },
     ];
 
+    const SHOW_OPTIONS: OptionsProps[] = [
+      { text: "1", value: "1" },
+      { text: "2", value: "2" },
+      { text: "3", value: "3" },
+      { text: "4", value: "4" },
+    ];
+
     return (
       <div
         style={{
           display: "flex",
           flexDirection: "column",
           gap: "0.25rem",
-          maxWidth: "250px",
+          maxWidth: "300px",
         }}
       >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "4px",
+            paddingLeft: "20px",
+          }}
+        >
+          Subitem actions:
+          <Combobox
+            options={SHOW_OPTIONS}
+            selectedOptions={showItem.length}
+            setSelectedOptions={(e: string[]) =>
+              setShowItem((prev) => ({ ...prev, length: e }))
+            }
+            containerStyle={css`
+              width: 30%;
+            `}
+          />
+        </div>
         <TreeList
+          key={Number(showItem.length[0])}
           content={TREE_LIST_DATA}
           actions={TREE_LIST_ACTIONS}
           emptySlate={<p>Not found.</p>}
