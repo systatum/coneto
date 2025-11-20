e82e20e9ec95ba2c1da45d965706ebb75c446850
Alimnfl <mnaufallim234@gmail.com>
2025-11-20 14:08:46 +0700

    Update treelist for actions.

--- components/treelist.tsx
+++ components/treelist.tsx
@@ -2,6 +2,8 @@ import styled, { css, CSSProp } from "styled-components";
 import { ReactNode, useState } from "react";
 import { RemixiconComponentType, RiArrowRightSLine } from "@remixicon/react";
 import { motion, AnimatePresence } from "framer-motion";
+import { TipMenuItemProps } from "./tip-menu";
+import ContextMenu from "./context-menu";
 
 export interface TreeListProps {
   content: TreeListContentProps[];
@@ -31,7 +33,10 @@ export interface TreeListItemsProps {
   id: string;
   caption: string;
   onClick?: (item?: TreeListItemsProps) => void;
+  actions?: (columnCaption: string) => SubMenuTreeList[];
 }
+
+export type SubMenuTreeList = TipMenuItemProps;
 
 function TreeList({
   content,
@@ -145,6 +150,8 @@ function TreeListItem<T extends TreeListItemsProps>({
   onChange?: (item?: string) => void;
   isSelected?: string;
 }) {
+  const [isHovered, setIsHovered] = useState<null | string>(null);
+
   const escapedTerm = escapeRegExp(searchTerm.trim());
   const regex = new RegExp(`(${escapedTerm})`, "gi");
   const parts = escapedTerm ? item.caption.split(regex) : [item.caption];
@@ -158,6 +165,14 @@ function TreeListItem<T extends TreeListItemsProps>({
         item.onClick(item);
         onChange(item.id);
       }}
+      $style={css`
+        ${isHovered === item.id &&
+        css`
+          background-color: #f3f4f6;
+        `}
+      `}
+      onMouseLeave={() => setIsHovered(null)}
+      onMouseEnter={() => setIsHovered(item.id)}
     >
       {parts.map((part, index) =>
         part.toLowerCase() === searchTerm.toLowerCase() ? (
@@ -166,6 +181,48 @@ function TreeListItem<T extends TreeListItemsProps>({
           <span key={index}>{part}</span>
         )
       )}
+
+      {item.id === isHovered &&
+        item.actions &&
+        (() => {
+          const list = item.actions(`${item.id}`);
+          const actionsWithIcons = list.map((prop) => ({
+            ...prop,
+            icon: prop.icon ?? RiArrowRightSLine,
+            onClick: (e?: React.MouseEvent) => {
+              e?.stopPropagation();
+              prop.onClick?.(e);
+              if (list.length > 2) {
+                setIsHovered(null);
+              }
+            },
+          }));
+
+          return (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <ContextMenu
+                maxInlineAction={2}
+                iconSize={16}
+                focusBackgroundColor="#d4d4d4"
+                hoverBackgroundColor="#d4d4d4"
+                activeBackgroundColor="#d4d4d4"
+                actions={actionsWithIcons}
+                buttonStyle={css`
+                  width: 20px;
+                  height: 20px;
+                  padding: 0;
+                `}
+              />
+            </div>
+          );
+        })()}
     </TreeListItemWrapper>
   );
 }
@@ -269,17 +326,27 @@ const ItemsWrapper = styled(motion.ul)<{ $collapsed?: boolean }>`
     `};
 `;
 
-const TreeListItemWrapper = styled.li<{ $isSelected: boolean }>`
+const TreeListItemWrapper = styled.li<{
+  $isSelected: boolean;
+  $style?: CSSProp;
+}>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
   cursor: pointer;
+  justify-content: space-between;
+  min-height: 36px;
+
   border-left: 3px solid
     ${(props) => (props.$isSelected ? "#3b82f6" : "transparent")};
   background-color: ${(props) =>
     props.$isSelected ? "#f3f4f6" : "transparent"};
   padding: 0.25rem 1.2rem;
-  &:hover {
-    background-color: #f3f4f6;
-  }
+  padding-right: 8px;
   list-style: none;
+
+  ${(props) => props.$style}
 `;
 
 const HighlightedText = styled.span`
