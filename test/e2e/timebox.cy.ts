4a1ef698a0c1a189b7e3d881df89ca32c8d3e8bf
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2025-11-21 12:38:56 +0700

    [Improvement] SYST-302: Better timebox time input experience (#151)
    * Update the newest behavior on timebox.
    
    * Ensure the test on time box when pressing colon, backspace, and number.
    
    * Ensure the test e2e with properly context on the timebox.

--- test/e2e/timebox.cy.ts
+++ test/e2e/timebox.cy.ts
@@ -1,68 +1,118 @@
 import { getIdContent } from "test/support/commands";
 
-context("Timebox Component", () => {
-  describe("Default", () => {
+describe("Timebox", () => {
+  context("when on HH:MM", () => {
     beforeEach(() => {
       cy.visit(getIdContent("input-elements-timebox--default"));
     });
 
-    it("Should allow entering hours and minutes", () => {
-      cy.findByPlaceholderText("HH")
-        .clear()
-        .type("13")
-        .should("have.value", "13");
-      cy.findByPlaceholderText("MM")
-        .clear()
-        .type("45")
-        .should("have.value", "45");
+    context("when typing", () => {
+      it("renders the input with value and move automaticaly", () => {
+        cy.findByPlaceholderText("HH").clear().type("1430");
+        cy.findByPlaceholderText("HH").should("have.value", "14");
+        cy.findByPlaceholderText("MM").should("have.value", "30");
+      });
+
+      context("when more than maximum allowed time", () => {
+        it("should move to the next field and transfer the value", () => {
+          cy.findByPlaceholderText("HH").clear().type("25");
+          cy.findByPlaceholderText("HH").should("have.value", "2");
+          cy.findByPlaceholderText("MM").should("have.value", "5");
+        });
+      });
+
+      context("when pressing backspace", () => {
+        it("should move to the previous input", () => {
+          cy.findByPlaceholderText("HH")
+            .clear()
+            .type("25{backspace}{backspace}");
+          cy.findByPlaceholderText("HH")
+            .should("have.value", "2")
+            .and("be.focused");
+        });
+      });
+
+      context("when pressing colon", () => {
+        it("should move to the next input", () => {
+          cy.findByPlaceholderText("HH").clear().type("4:");
+          cy.findByPlaceholderText("HH")
+            .should("have.value", "4")
+            .and("not.be.focused");
+          cy.findByPlaceholderText("MM")
+            .should("have.value", "")
+            .and("be.focused");
+        });
+      });
     });
   });
 
-  describe("With Seconds", () => {
+  context("when on HH:MM:SS", () => {
     beforeEach(() => {
       cy.visit(getIdContent("input-elements-timebox--with-seconds"));
     });
 
-    it("Should allow entering hours, minutes, and seconds", () => {
-      cy.findByPlaceholderText("HH")
-        .clear()
-        .type("13")
-        .should("have.value", "13");
-      cy.findByPlaceholderText("MM")
-        .clear()
-        .type("45")
-        .should("have.value", "45");
-      cy.findByPlaceholderText("SS")
-        .clear()
-        .type("30")
-        .should("have.value", "30");
-    });
+    context("when typing", () => {
+      it("renders the input with value and move automaticaly", () => {
+        cy.findByPlaceholderText("HH").clear().type("143000");
+        cy.findByPlaceholderText("HH").should("have.value", "14");
+        cy.findByPlaceholderText("MM").should("have.value", "30");
+        cy.findByPlaceholderText("SS").should("have.value", "00");
+      });
 
-    it("Should input time with new behaviour arrow and enter", () => {
-      cy.findByPlaceholderText("HH")
-        .clear()
-        .type("14{rightarrow}30{rightarrow}00{enter}");
+      context("when more than maximum allowed time", () => {
+        it("should move to the next field and transfer the value", () => {
+          cy.findByPlaceholderText("HH").clear().type("25");
+          cy.findByPlaceholderText("HH").should("have.value", "2");
+          cy.findByPlaceholderText("MM").should("have.value", "5");
+        });
+      });
+
+      context("when pressing backspace", () => {
+        it("should move to the previous input", () => {
+          cy.findByPlaceholderText("HH")
+            .clear()
+            .type("25{backspace}{backspace}");
+          cy.findByPlaceholderText("HH")
+            .should("have.value", "2")
+            .and("be.focused");
+        });
+      });
+
+      context("when pressing colon", () => {
+        it("should move to the next input", () => {
+          cy.findByPlaceholderText("HH").clear().type("25:");
+          cy.findByPlaceholderText("HH")
+            .should("have.value", "2")
+            .and("not.be.focused");
+          cy.findByPlaceholderText("MM")
+            .should("have.value", "5")
+            .and("not.be.focused");
+          cy.findByPlaceholderText("SS")
+            .should("have.value", "")
+            .and("be.focused");
+        });
+      });
     });
   });
 
-  describe("With Live Time", () => {
+  context("with live time", () => {
     beforeEach(() => {
       cy.visit(getIdContent("input-elements-timebox--with-live-time"));
     });
 
-    it("Should render a non-editable timebox with current time", () => {
+    it("should render a non-editable timebox with current time", () => {
       cy.findByPlaceholderText("HH").should("have.attr", "disabled");
       cy.findByPlaceholderText("MM").should("have.attr", "disabled");
       cy.findByPlaceholderText("SS").should("have.attr", "disabled");
     });
   });
 
-  describe("With Error", () => {
+  context("with error", () => {
     beforeEach(() => {
       cy.visit(getIdContent("input-elements-timebox--with-error"));
     });
 
-    it("Should show error initially and validate on input", () => {
+    it("should show error initially and validate on input", () => {
       cy.contains("This field is required").should("exist");
 
       cy.findByPlaceholderText("HH").clear().type("13");
