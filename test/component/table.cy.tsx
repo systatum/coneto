2c4207eaffddb722d975db38ba61eeb418c96d25
Alimnfl <mnaufallim234@gmail.com>
2025-12-10 11:50:50 +0700

    Ensure the test when initialize selectedItems on table.

--- test/component/table.cy.tsx
+++ test/component/table.cy.tsx
@@ -438,6 +438,60 @@ describe("Table", () => {
     });
   });
 
+  context("with selectable", () => {
+    context("when initialize", () => {
+      it("renders content with checked value", () => {
+        const columns: ColumnTableProps[] = [
+          {
+            id: "name",
+            caption: "Name",
+            sortable: true,
+          },
+          {
+            id: "type",
+            caption: "Type",
+            sortable: true,
+          },
+        ];
+        const selectedItems = [
+          "Load Balancer 1-HTTP",
+          "Load Balancer 3-TCP",
+          "Load Balancer 4-UDP",
+        ];
+
+        const TYPES_DATA = ["HTTP", "HTTPS", "TCP", "UDP", "QUIC"];
+
+        const rawRows = Array.from({ length: 20 }, (_, i) => ({
+          name: `Load Balancer ${i + 1}`,
+          type: TYPES_DATA[i % TYPES_DATA.length],
+        }));
+
+        cy.mount(
+          <Table selectable selectedItems={selectedItems} columns={columns}>
+            {rawRows?.map((row, index) => (
+              <Table.Row key={index} rowId={`${row.name}-${row.type}`}>
+                {[row.name, row.type].map((rowCell, i) => (
+                  <Table.Row.Cell
+                    key={`${row.name}-${row.type}-${rowCell}`}
+                    width={columns[i].width}
+                  >
+                    {rowCell}
+                  </Table.Row.Cell>
+                ))}
+              </Table.Row>
+            ))}
+          </Table>
+        );
+
+        cy.get('input[type="checkbox"]').eq(0).should("not.be.checked");
+        cy.get('input[type="checkbox"]').eq(1).should("be.checked");
+        cy.get('input[type="checkbox"]').eq(2).should("not.be.checked");
+        cy.get('input[type="checkbox"]').eq(3).should("be.checked");
+        cy.get('input[type="checkbox"]').eq(4).should("be.checked");
+      });
+    });
+  });
+
   context("with top actions", () => {
     context("when given default", () => {
       const DEFAULT_TOP_ACTIONS: TableActionsProps[] = [
