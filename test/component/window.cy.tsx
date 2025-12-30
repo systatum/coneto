c3283ec716e3ad4ef958a095ee49bc23f7ddf41c
Alimnfl <mnaufallim234@gmail.com>
2025-12-30 10:23:06 +0700

    Ensure test when given initial size ratio and not given.

--- test/component/window.cy.tsx
+++ test/component/window.cy.tsx
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 import { Window } from "./../../components/window";
 
 describe("Window", () => {
-  context("initialSizes", () => {
+  context("initialSizeRatio", () => {
     it("renders with initial size", () => {
       cy.mount(
         <Window
@@ -10,7 +10,7 @@ describe("Window", () => {
           style={css`
             height: 500px;
           `}
-          initialSizes={[90, 10]}
+          initialSizeRatio={[90, 10]}
         >
           <Window.Cell
             style={css`
@@ -37,6 +37,43 @@ describe("Window", () => {
         .eq(1)
         .invoke("height")
         .should("be.closeTo", 50, 1);
+    });
+
+    context("when not given", () => {
+      it("renders automatically reference from children", () => {
+        cy.mount(
+          <Window
+            orientation="horizontal"
+            style={css`
+              height: 500px;
+            `}
+          >
+            <Window.Cell
+              style={css`
+                background-color: #fee2e2;
+              `}
+            >
+              Up
+            </Window.Cell>
+            <Window.Cell
+              style={css`
+                background-color: #dcfce7;
+              `}
+            >
+              Down
+            </Window.Cell>
+          </Window>
+        );
+        cy.findByLabelText("window").should("have.css", "height", "500px");
+        cy.findAllByLabelText("window-cell")
+          .eq(0)
+          .invoke("height")
+          .should("be.closeTo", 250, 1);
+        cy.findAllByLabelText("window-cell")
+          .eq(1)
+          .invoke("height")
+          .should("be.closeTo", 250, 1);
+      });
     });
   });
 });
