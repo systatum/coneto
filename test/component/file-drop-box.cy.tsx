044e9330424fdfed614f79584d0a992666943f21
Alim Naufal <122043760+Alimnfl@users.noreply.github.com>
2026-01-15 20:49:23 +0700

    [Bug] SYST-406: Do not automatically trigger file selection when clicking on table part of `FileDropBox` (#211)
    * Update the content wrapper on the `children` to have stop propagate to not distracted.
    
    * Update for cursor default on the children.
    
    * Ensure the test to now open file picker on the `FileDropBox`.

--- test/component/file-drop-box.cy.tsx
+++ test/component/file-drop-box.cy.tsx
@@ -4,8 +4,139 @@ import {
   OnCompleteFunctionProps,
   OnFileDroppedFunctionProps,
 } from "./../../components/file-drop-box";
+import { Table } from "./../../components/table";
+import { useState } from "react";
+import { RiDeleteBin2Fill } from "@remixicon/react";
 
 describe("FileDropBox", () => {
+  context("children", () => {
+    context("when click on the children area", () => {
+      function FileDropBoxContent() {
+        const [files, setFiles] = useState<File[]>([]);
+
+        const onFileDropped = async ({
+          error,
+          files,
+          setProgressLabel,
+          succeed,
+        }: OnFileDroppedFunctionProps) => {
+          const file = files[0];
+          setFiles((prev) => [...prev, file]);
+          setProgressLabel(`Uploading ${file.name}`);
+
+          return new Promise<void>((resolve) => {
+            let progress = 0;
+            const interval = setInterval(() => {
+              progress += 20;
+
+              if (progress >= 100) {
+                clearInterval(interval);
+                if (file === null) {
+                  error(file, `file ${file.name} is not uploaded`);
+                } else {
+                  succeed(file);
+                }
+                setProgressLabel(`Uploaded ${files[0].name}`);
+                resolve();
+              }
+            }, 300);
+          });
+        };
+
+        const onComplete = async ({
+          failedFiles,
+          setProgressLabel,
+          succeedFiles,
+          hideProgressLabel,
+          showUploaderForm,
+        }: OnCompleteFunctionProps) => {
+          console.log(succeedFiles, "This is succeedFiles");
+          console.log(failedFiles, "This is failedFiles");
+          await setProgressLabel(
+            `Upload complete! Success: ${succeedFiles.length}, Failed: ${failedFiles.length}`
+          );
+          await hideProgressLabel();
+          await showUploaderForm();
+        };
+
+        return (
+          <FileDropBox onFileDropped={onFileDropped} onComplete={onComplete}>
+            <Table
+              styles={{
+                containerStyle: css`
+                  ${files.length === 0 &&
+                  css`
+                    display: none;
+                  `}
+                `,
+              }}
+              columns={[
+                {
+                  id: "file_name",
+                  caption: "File Name",
+                },
+                {
+                  id: "date",
+                  caption: "Date",
+                },
+              ]}
+            >
+              {files.map((props) => (
+                <Table.Row
+                  actions={(id) => [
+                    {
+                      caption: "Delete",
+                      icon: RiDeleteBin2Fill,
+                      onClick: () => {
+                        if (id) {
+                          setFiles((prev) =>
+                            prev.filter((val) => val.name !== id)
+                          );
+                        }
+                      },
+                    },
+                  ]}
+                  rowId={props.name}
+                  content={[
+                    props.name,
+                    new Date(props.lastModified).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }),
+                  ]}
+                />
+              ))}
+            </Table>
+          </FileDropBox>
+        );
+      }
+
+      it("should not open file picker", () => {
+        cy.mount(<FileDropBoxContent />);
+        cy.findByLabelText("filedropbox").selectFile(
+          [
+            "test/fixtures/test-images/sample-1.jpg",
+            "test/fixtures/test-images/sample-2.jpg",
+          ],
+          {
+            action: "drag-drop",
+            force: true,
+          }
+        );
+        cy.wait(1000);
+
+        cy.findByLabelText("filedropbox").then(($input) => {
+          cy.spy($input[0], "click").as("fileClick");
+        });
+        cy.findByText("sample-1.jpg").should("be.visible").click();
+        cy.findByText("sample-2.jpg").should("be.visible").click();
+
+        cy.get("@fileClick").should("not.have.been.called");
+      });
+    });
+  });
+
   context("onComplete", () => {
     context("when hide progress", () => {
       const onFileDropped = async ({
