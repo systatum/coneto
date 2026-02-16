5aea64051b5ffa073cf84ccb665f840171f2cd5e
Alimnfl <mnaufallim234@gmail.com>
2026-02-16 13:35:12 +0700

    Add colorbox withDropdowns story.

--- components/colorbox.stories.tsx
+++ components/colorbox.stories.tsx
@@ -1,8 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Colorbox } from "./colorbox";
 import { useArgs } from "@storybook/preview-api";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { css } from "styled-components";
+import { Calendar } from "./calendar";
+import { DropdownOptionProps } from "./field-lane";
+import * as RemixIcons from "@remixicon/react";
 
 const meta: Meta<typeof Colorbox> = {
   title: "Input Elements/Colorbox",
@@ -88,6 +91,108 @@ export const Default: Story = {
         value={args.color}
         name="color"
         onChange={onChangeValue}
+      />
+    );
+  },
+};
+
+export const WithDropdowns: Story = {
+  render: () => {
+    const [inputValue, setInputValue] = useState({
+      selectedText1: "11/12/2025",
+      selectedOption1: "11/12/2025",
+      selectedText2: "WFH",
+      selectedOption2: "2",
+      value: "",
+    });
+
+    const ATTENDANCE_OPTIONS: DropdownOptionProps[] = [
+      { text: "On-site", value: "1", icon: RemixIcons.RiHome2Line },
+      { text: "WFH", value: "2", icon: RemixIcons.RiUser2Line },
+      {
+        text: "Sick leave",
+        value: "3",
+        icon: RemixIcons.RiSettings2Line,
+      },
+      {
+        text: "Annual leave",
+        value: "4",
+        icon: RemixIcons.RiLogoutBoxLine,
+      },
+    ];
+
+    const MONTH_NAMES = [
+      { text: "JAN", value: "1" },
+      { text: "FEB", value: "2" },
+      { text: "MAR", value: "3" },
+      { text: "APR", value: "4" },
+      { text: "MAY", value: "5" },
+      { text: "JUN", value: "6" },
+      { text: "JUL", value: "7" },
+      { text: "AUG", value: "8" },
+      { text: "SEP", value: "9" },
+      { text: "OCT", value: "10" },
+      { text: "NOV", value: "11" },
+      { text: "DEC", value: "12" },
+    ];
+
+    return (
+      <Colorbox
+        styles={{
+          containerStyle: css`
+            max-width: 700px;
+          `,
+        }}
+        placeholder="Add color"
+        dropdowns={[
+          {
+            width: "100px",
+            caption: inputValue.selectedText1,
+            render: ({ render }) =>
+              render(
+                <Calendar
+                  selectedDates={[inputValue.selectedOption1]}
+                  monthNames={MONTH_NAMES}
+                  setSelectedDates={(date: string[]) =>
+                    setInputValue((prev) => ({
+                      ...prev,
+                      selectedText1: date[0],
+                      selectedOption1: date[0],
+                    }))
+                  }
+                />
+              ),
+          },
+          {
+            width: "120px",
+            styles: {
+              drawerStyle: css`
+                width: 300px;
+              `,
+            },
+            caption: inputValue.selectedText2,
+            options: ATTENDANCE_OPTIONS,
+            onChange: (id) => {
+              const selected = ATTENDANCE_OPTIONS.find(
+                (item) => item.value === id
+              );
+              if (selected) {
+                setInputValue((prev) => ({
+                  ...prev,
+                  selectedOption2: id,
+                  selectedText2: selected.text,
+                }));
+              }
+            },
+            withFilter: true,
+          },
+        ]}
+        label="Color"
+        value={inputValue.value}
+        name="color"
+        onChange={(e) =>
+          setInputValue((prev) => ({ ...prev, value: e.target.value }))
+        }
       />
     );
   },
