11dbb9f86f10bedecdd5ceefff21b1d78fec306c
Alimnfl <mnaufallim234@gmail.com>
2026-02-18 11:15:53 +0700

    Ensure the window cell after resize textarea.

--- test/component/window.cy.tsx
+++ test/component/window.cy.tsx
@@ -1,7 +1,83 @@
 import { css } from "styled-components";
 import { Window } from "./../../components/window";
+import { Textarea } from "./../../components/textarea";
+import { useRef, useState } from "react";
 
 describe("Window", () => {
+  context("ref in window.cell level", () => {
+    context("when resize", () => {
+      it("renders similar height as a reference", () => {
+        function WindowWithTextbox() {
+          const [textareaHeight, setTextareaHeight] = useState<number | null>(
+            null
+          );
+          const secondCellRef = useRef<HTMLDivElement | null>(null);
+
+          return (
+            <Window
+              styles={{
+                self: css`
+                  height: 500px;
+                `,
+              }}
+              orientation="horizontal"
+              onResizeComplete={() => {
+                if (secondCellRef.current) {
+                  const height = secondCellRef.current.clientHeight;
+                  setTextareaHeight(height);
+                }
+              }}
+            >
+              <Window.Cell>Test</Window.Cell>
+              <Window.Cell
+                ref={secondCellRef}
+                styles={{
+                  self: css`
+                    z-index: 10;
+                    background-color: white;
+                  `,
+                }}
+              >
+                <Textarea
+                  aria-label="textarea-with-window"
+                  styles={{
+                    self: css`
+                      height: ${textareaHeight
+                        ? `${textareaHeight}px`
+                        : "250px"};
+                      border-color: white;
+                    `,
+                  }}
+                  onChange={(e) => {
+                    console.log(e);
+                  }}
+                />
+              </Window.Cell>
+            </Window>
+          );
+        }
+        cy.mount(<WindowWithTextbox />);
+
+        cy.findAllByLabelText("window-divider")
+          .trigger("mousedown", {
+            clientY: 250,
+          })
+          .trigger("mousemove", { clientY: 150 })
+          .trigger("mouseup");
+
+        cy.findAllByLabelText("window-cell")
+          .eq(1)
+          .should("have.css", "height", "350px");
+
+        cy.findAllByLabelText("textarea-with-window")
+          .eq(0)
+          .parent()
+          .invoke("height")
+          .should("be.closeTo", 350, 4);
+      });
+    });
+  });
+
   context("initialSizeRatio", () => {
     it("renders with initial size", () => {
       cy.mount(
