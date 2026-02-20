c2c393f093f63970e4260135f2b119b4654a4de0
Alimnfl <mnaufallim234@gmail.com>
2026-02-20 14:40:06 +0700

    Update the story for long description data.

--- components/keynote.stories.tsx
+++ components/keynote.stories.tsx
@@ -78,7 +78,7 @@ export const CustomRendering: Story = {
       requestCreatedBy: "alim@systatum.com",
       lastSynced: "2025-06-20",
       createdOn: "2025-06-19",
-      desc: "Backup unit installed on site",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     };
 
     return (
