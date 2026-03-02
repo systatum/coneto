76e3a0b2aa4cc348318a22f27a43e18f98645da4
Alimnfl <mnaufallim234@gmail.com>
2026-03-02 13:03:11 +0700

    Add aria label on the `NavTab` component.

--- components/nav-tab.tsx
+++ components/nav-tab.tsx
@@ -151,7 +151,11 @@ function NavTab({
   return (
     <NavTabWrapper $containerStyle={styles?.containerStyle}>
       <NavTabRowWrapper $style={styles?.containerRowStyle}>
-        <NavTabHeader $style={styles?.containerBoxStyle} ref={containerRef}>
+        <NavTabHeader
+          aria-label="nav-tab-wrapper"
+          $style={styles?.containerBoxStyle}
+          ref={containerRef}
+        >
           <NavTabList
             aria-label="nav-tab-list"
             $activeColor={activeColor}
@@ -344,6 +348,7 @@ function NavTab({
 
         {actions && (
           <NavTabHeader
+            aria-label="nav-tab-actions-wrapper"
             $actions={!!actions}
             $style={css`
               gap: 6px;
