b3cf5dd88018ebe2c417ed00faca47340d1b7910
Alim Naufal <122043760+alimnfl@users.noreply.github.com>
2026-03-02 15:22:17 +0700

    [Bug] SYST-485: FIx icon not shown on searchbox (#256)
    * Fix story using new template.
    
    * Ensure the test not have error when given img.

--- test/component/searchbox.cy.tsx
+++ test/component/searchbox.cy.tsx
@@ -23,39 +23,39 @@ describe("Searchbox", () => {
       const PEOPLE_MENU: SearchboxResultMenuItemProps[] = [
         {
           caption: "Adam Noto Hakarsa",
-          icon: RiUserSmileLine,
+          icon: { image: RiUserSmileLine },
         },
         {
           caption: "Alim Naufal",
-          icon: RiUserStarLine,
+          icon: { image: RiUserStarLine },
         },
         {
           caption: "Michael Chen",
-          icon: RiUserLine,
+          icon: { image: RiUserLine },
         },
         {
           caption: "Ayu Pratama",
-          icon: RiUserHeartLine,
+          icon: { image: RiUserHeartLine },
         },
         {
           caption: "Daniel Rodriguez",
-          icon: RiUserVoiceLine,
+          icon: { image: RiUserVoiceLine },
         },
         {
           caption: "Rina Sari",
-          icon: RiUserSettingsLine,
+          icon: { image: RiUserSettingsLine },
         },
         {
           caption: "Tom Williams",
-          icon: RiUserSharedLine,
+          icon: { image: RiUserSharedLine },
         },
         {
           caption: "Nabila Zahra",
-          icon: RiUserSearchLine,
+          icon: { image: RiUserSearchLine },
         },
         {
           caption: "Kevin Park",
-          icon: RiUserAddLine,
+          icon: { image: RiUserAddLine },
         },
       ];
 
