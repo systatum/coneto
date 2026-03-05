d1b3826bf08bd02c89410c10da7358facb97a812
Alimnfl <mnaufallim234@gmail.com>
2026-03-05 11:47:33 +0700

    Updates the `Stateful` component to use `Figure['image]`

--- components/selectbox.tsx
+++ components/selectbox.tsx
@@ -24,7 +24,6 @@ import {
   useInteractions,
 } from "@floating-ui/react";
 import {
-  RemixiconComponentType,
   RiArrowDownSLine,
   RiArrowUpSLine,
   RiCloseLine,
@@ -32,6 +31,7 @@ import {
 import styled, { css, CSSProp } from "styled-components";
 import { isValidDateString } from "../lib/date";
 import { FieldLane, FieldLaneProps, FieldLaneStylesProps } from "./field-lane";
+import { FigureProps } from "./figure";
 
 export type SelectboxSelectedOptions = number | string | number[] | string[];
 
@@ -41,8 +41,8 @@ interface BaseSelectboxProps
   selectedOptions?: SelectboxSelectedOptions;
   onChange?: (selectedOptions: SelectboxSelectedOptions) => void;
   placeholder?: string;
-  iconOpened?: RemixiconComponentType;
-  iconClosed?: RemixiconComponentType;
+  iconOpened?: FigureProps["image"];
+  iconClosed?: FigureProps["image"];
   type?: "calendar" | "default";
   clearable?: boolean;
   highlightOnMatch?: boolean;
