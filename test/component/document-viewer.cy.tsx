aecb243270b57ab14c5e61a4d42aaaa338348e53
Alimnfl <mnaufallim234@gmail.com>
2026-03-09 16:39:23 +0700

    Ensure the test with file, image, pdf, and encodedString

--- test/component/document-viewer.cy.tsx
+++ test/component/document-viewer.cy.tsx
@@ -1,16 +1,115 @@
+import { useEffect, useState } from "react";
 import { DocumentViewer } from "./../../components/document-viewer";
+import { urlToBase64 } from "./../../lib/base64";
 
 describe("Document Viewer", () => {
+  context("source", () => {
+    context("when given with pdf()", () => {
+      it("renders the pdf", () => {
+        cy.mount(
+          <DocumentViewer
+            source={({ pdf }) => pdf("sample.pdf")}
+            title="Team Collaboration Notes"
+          />
+        );
+
+        cy.findByText("Team Collaboration Notes").should("exist");
+        cy.findByLabelText("container-content")
+          .should("exist")
+          .within(() => {
+            cy.get("canvas", { timeout: 10000 }).should("exist");
+          });
+      });
+    });
+
+    context("when given with image()", () => {
+      it("renders the image", () => {
+        cy.mount(
+          <DocumentViewer
+            title="Document Viewer With Image"
+            source={({ image }) =>
+              image(
+                "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
+              )
+            }
+          />
+        );
+
+        cy.findByText("Document Viewer With Image").should("exist");
+        cy.findByLabelText("container-content")
+          .should("exist")
+          .within(() => {
+            cy.get("canvas", { timeout: 10000 }).should("exist");
+          });
+      });
+    });
+
+    context("when given with encodedString()", () => {
+      function DocumentViewerWithBase64() {
+        const [base64, setBase64] = useState<string | null>(null);
+
+        useEffect(() => {
+          urlToBase64(
+            "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
+          ).then(setBase64);
+        }, []);
+
+        return (
+          <DocumentViewer
+            title="Document Viewer with Encode String"
+            source={({ encodedString }) => encodedString(base64, "png")}
+          />
+        );
+      }
+      it("renders the image from base 64", () => {
+        cy.mount(<DocumentViewerWithBase64 />);
+
+        cy.findByText("Document Viewer with Encode String").should("exist");
+        cy.findByLabelText("container-content")
+          .should("exist")
+          .within(() => {
+            cy.get("canvas", { timeout: 10000 }).should("exist");
+          });
+      });
+    });
+
+    context("when given with file()", () => {
+      it("renders the image from local file", () => {
+        cy.fixture("test-images/sample-1.jpg", "base64").then((base64) => {
+          const blob = Cypress.Blob.base64StringToBlob(base64, "image/jpeg");
+
+          const file = new File([blob], "sample-1.jpg", {
+            type: "image/jpeg",
+          });
+
+          cy.mount(
+            <DocumentViewer
+              title="Document Viewer With File"
+              source={({ file: fileBuilder }) => fileBuilder(file)}
+            />
+          );
+        });
+
+        cy.findByText("Document Viewer With File").should("exist");
+
+        cy.findByLabelText("container-content")
+          .should("exist")
+          .within(() => {
+            cy.get("canvas", { timeout: 10000 }).should("exist");
+          });
+      });
+    });
+  });
+
   context("scroll", () => {
     context("when scroll to the bottom", () => {
       it("on the latest page", () => {
         cy.mount(
           <DocumentViewer
-            source="sample.pdf"
+            source={({ pdf }) => pdf("sample.pdf")}
             title="Team Collaboration Notes"
           />
         );
-        cy.wait(400);
 
         cy.findByText("Team Collaboration Notes").should("exist");
         cy.findByLabelText("container-content")
@@ -25,11 +124,10 @@ describe("Document Viewer", () => {
       it("renders viewport change scale to 0.75 or 75%", () => {
         cy.mount(
           <DocumentViewer
-            source="sample.pdf"
+            source={({ pdf }) => pdf("sample.pdf")}
             title="Team Collaboration Notes"
           />
         );
-        cy.wait(400);
 
         cy.findByPlaceholderText("zoom your pdf...").as("input");
 
@@ -42,7 +140,7 @@ describe("Document Viewer", () => {
         cy.mount(
           <DocumentViewer
             initialZoom={75}
-            source="sample.pdf"
+            source={({ pdf }) => pdf("sample.pdf")}
             title="Team Collaboration Notes"
           />
         );
@@ -71,12 +169,11 @@ describe("Document Viewer", () => {
         ];
         cy.mount(
           <DocumentViewer
-            source="sample.pdf"
+            source={({ pdf }) => pdf("sample.pdf")}
             boundingBoxes={boundingBoxes}
             title="Team Collaboration Notes"
           />
         );
-        cy.wait(400);
 
         cy.findAllByLabelText("selection-box").eq(0).trigger("mouseover");
         cy.findAllByLabelText("selection-content-hovered")
@@ -90,24 +187,21 @@ describe("Document Viewer", () => {
       it("renders selected rectangle", () => {
         cy.mount(
           <DocumentViewer
-            source="sample.pdf"
+            source={({ pdf }) => pdf("sample.pdf")}
             title="Team Collaboration Notes"
           />
         );
-        cy.wait(400);
 
         cy.findAllByLabelText("container-content")
           .eq(0)
           .trigger("mousemove", { clientX: 100, clientY: 100, force: true });
 
-        cy.findAllByLabelText("container-content")
-          .eq(0)
-          .trigger("mousedown", {
-            clientX: 100,
-            clientY: 100,
-            button: 0,
-            force: true,
-          });
+        cy.findAllByLabelText("container-content").eq(0).trigger("mousedown", {
+          clientX: 100,
+          clientY: 100,
+          button: 0,
+          force: true,
+        });
 
         cy.findAllByLabelText("container-content")
           .eq(0)
