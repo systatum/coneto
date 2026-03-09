faeac56fc70c9ba0c5443aec7ea9da73e79d2407
Alimnfl <mnaufallim234@gmail.com>
2026-03-09 17:51:23 +0700

    Add onClick for button props to be work.

--- components/stateful-form.tsx
+++ components/stateful-form.tsx
@@ -520,7 +520,12 @@ function FormFields<T extends FieldValues>({
                       ${field.buttonProps?.styles?.containerStyle};
                     `,
                   }}
-                  onClick={field.onClick}
+                  onClick={(e) => {
+                    field.onClick(e);
+                    if (field?.buttonProps?.onClick) {
+                      field?.buttonProps?.onClick(e);
+                    }
+                  }}
                   disabled={field.disabled}
                 >
                   {field.icon && (
