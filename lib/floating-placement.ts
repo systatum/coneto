7e04d2632a4c321e63000ef77ed41f936f36ec9e
Alimnfl <mnaufallim234@gmail.com>
2026-03-12 00:26:33 +0700

    Add placement for `left` and `right`.

--- lib/floating-placement.ts
+++ lib/floating-placement.ts
@@ -4,18 +4,34 @@ export type DialogPlacement =
   | "bottom-left"
   | "bottom-right"
   | "top-left"
-  | "top-right";
+  | "top-right"
+  | "left-top"
+  | "left-bottom"
+  | "right-top"
+  | "right-bottom";
 
-export function getFloatingPlacement(position?: DialogPlacement): Placement {
-  switch (position) {
+export function getFloatingPlacement(placement: DialogPlacement): Placement {
+  switch (placement) {
     case "bottom-left":
       return "bottom-start";
     case "bottom-right":
       return "bottom-end";
+
     case "top-left":
       return "top-start";
     case "top-right":
       return "top-end";
+
+    case "left-top":
+      return "left-start";
+    case "left-bottom":
+      return "left-end";
+
+    case "right-top":
+      return "right-start";
+    case "right-bottom":
+      return "right-end";
+
     default:
       return "bottom-start";
   }
