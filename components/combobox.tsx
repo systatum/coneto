18d26ffde7472f39b76fb0159ecf545715f4af79
Alimnfl <mnaufallim234@gmail.com>
2026-03-13 15:10:55 +0700

    Ensures the test when given falsy for `combobox.action`.

--- components/combobox.tsx
+++ components/combobox.tsx
@@ -22,6 +22,7 @@ import { List, ListItemStylesProps } from "./list";
 import { FieldLaneProps } from "./field-lane";
 import { Figure, FigureProps } from "./figure";
 import { StatefulForm } from "./stateful-form";
+import { FalsyOr } from "./../lib/falsy";
 
 interface BaseComboboxProps {
   selectedOptions?: SelectboxSelectedOptions;
@@ -50,7 +51,9 @@ export interface ComboboxStylesProps
   labelStyle?: CSSProp;
 }
 
-export interface ComboboxActionProps {
+export type ComboboxActionProps = FalsyOr<ComboboxInternalActionProps>;
+
+export interface ComboboxInternalActionProps {
   onClick?: () => void;
   icon?: FigureProps;
   title: string;
@@ -289,6 +292,13 @@ function ComboboxDrawer({
     }
   }, [highlightedIndex, multiple]);
 
+  const filteredActions =
+    actions?.filter((action): action is ComboboxInternalActionProps =>
+      Boolean(action)
+    ) ?? [];
+
+  const hasActions = filteredActions.length > 0;
+
   return (
     <DrawerWrapper
       {...getFloatingProps()}
@@ -345,8 +355,8 @@ function ComboboxDrawer({
             });
           }}
         >
-          {actions &&
-            actions.map((action, index) => {
+          {hasActions &&
+            filteredActions.map((action, index) => {
               const shouldHighlight = highlightedIndex === index;
               const isLast = index === actions.length - 1;
 
