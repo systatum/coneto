ee20c9e4f5e9d983feaa823929a6d1951fb0a6cf
Alimnfl <mnaufallim234@gmail.com>
2026-03-17 17:51:16 +0700

    Update the status bar component.

---
+++ components/statusbar.tsx
@@ -0,0 +1,283 @@
+import styled, { css, CSSProp } from "styled-components";
+import { Figure, FigureProps } from "./figure";
+import { ReactNode } from "react";
+import { Button, ButtonProps } from "./button";
+import { FalsyOr } from "@/lib/falsy";
+
+export interface StatusbarProps {
+  styles?: StatusbarStylesProps;
+  content?: StatusbarContentProps;
+  paddingTop?: string;
+  activeBackgroundColor?: string;
+  hoverBackgroundColor?: string;
+  transparent?: boolean;
+  size?: number;
+}
+
+export interface StatusbarContentProps {
+  left?: StatusbarItemProps[];
+  right?: StatusbarItemProps[];
+}
+
+export interface StatusbarStylesProps {
+  self?: CSSProp;
+  itemStyle?: CSSProp;
+  leftWrapperStyle?: CSSProp;
+  rightWrapperStyle?: CSSProp;
+}
+
+function Statusbar({
+  styles,
+  content,
+  paddingTop,
+  activeBackgroundColor = "#ececec",
+  hoverBackgroundColor,
+  transparent,
+  size = 8,
+}: StatusbarProps) {
+  return (
+    <StatusbarWrapper
+      $transparent={transparent}
+      $style={css`
+        ${paddingTop &&
+        css`
+          margin-top: ${paddingTop};
+        `}
+
+        ${styles?.self}
+      `}
+    >
+      {renderSection(
+        content?.left,
+        styles?.leftWrapperStyle,
+        activeBackgroundColor,
+        hoverBackgroundColor,
+        size,
+        transparent
+      )}
+      {renderSection(
+        content?.right,
+        styles?.rightWrapperStyle,
+        activeBackgroundColor,
+        hoverBackgroundColor,
+        size,
+        transparent
+      )}
+    </StatusbarWrapper>
+  );
+}
+
+const renderSection = (
+  items?: StatusbarItemProps[],
+  style?: CSSProp,
+  activeBackgroundColor?: string,
+  hoverBackgroundColor?: string,
+  size?: number,
+  transparent?: boolean
+) => {
+  if (!items) return null;
+
+  return (
+    <ActionWrapper $style={style}>
+      {items.filter(isValidStatusbarItem).map((component, index) => (
+        <StatusbarItem
+          key={index}
+          {...component}
+          size={size}
+          transparent={transparent}
+          activeBackgroundColor={activeBackgroundColor}
+          hoverBackgroundColor={hoverBackgroundColor}
+        />
+      ))}
+    </ActionWrapper>
+  );
+};
+
+const isValidStatusbarItem = (
+  component?: StatusbarItemInternalProps
+): component is StatusbarItemInternalProps => Boolean(component);
+
+type StatusbarItemProps = FalsyOr<StatusbarItemInternalProps>;
+
+interface StatusbarItemInternalProps {
+  text?: string;
+  icon?: FigureProps;
+  render?: ReactNode;
+  button?: ButtonProps;
+  styles?: StatusbarItemStylesProps;
+  width?: string;
+}
+
+interface StatusbarItemStylesProps {
+  self?: CSSProp;
+}
+
+function StatusbarItem({
+  button,
+  icon,
+  text,
+  render,
+  styles,
+  activeBackgroundColor,
+  hoverBackgroundColor,
+  size,
+  width,
+  transparent,
+}: StatusbarItemInternalProps & {
+  activeBackgroundColor?: string;
+  hoverBackgroundColor?: string;
+  size?: number;
+  transparent?: boolean;
+}) {
+  if (button) {
+    return (
+      <Button
+        {...button}
+        variant={transparent ? "transparent" : "default"}
+        activeBackgroundColor={activeBackgroundColor}
+        hoverBackgroundColor={hoverBackgroundColor}
+        styles={{
+          ...button?.styles,
+          self: css`
+            width: fit-content;
+            min-height: 21px;
+            padding-left: 10px;
+            padding-right: 10px;
+            height: 100%;
+            border-radius: 0px;
+            font-size: ${`${size}px`};
+
+            ${button?.styles?.self ? button?.styles?.self : styles?.self};
+          `,
+          toggleStyle: css`
+            width: fit-content;
+            min-height: 21px;
+            padding-left: 10px;
+            padding-right: 10px;
+            height: 100%;
+            border-radius: 0px;
+            font-size: ${`${size}px`};
+            ${button?.styles?.self ? button?.styles?.self : styles?.self}
+          `,
+        }}
+      >
+        {button?.children}
+      </Button>
+    );
+  }
+
+  return (
+    <TextWrapper $width={width} $style={styles?.self}>
+      {icon && (
+        <Figure
+          {...icon}
+          size={size}
+          styles={{
+            self: css`
+              height: 100%;
+              justify-content: center;
+              align-items: center;
+              display: flex;
+              transition: background-color 0.2s ease;
+
+              ${icon?.styles?.self ? icon?.styles?.self : styles?.self}
+            `,
+          }}
+        />
+      )}
+      {render ? render : <Label aria-label="statusbar-label">{text}</Label>}
+    </TextWrapper>
+  );
+}
+
+const StatusbarWrapper = styled.div<{
+  $style?: CSSProp;
+  $transparent?: boolean;
+}>`
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  font-size: 14px;
+  flex-direction: row;
+  justify-content: space-between;
+  position: absolute;
+  border-top: 1px solid #ececec;
+  ${({ $transparent }) =>
+    $transparent &&
+    css`
+      border-width: 0px;
+    `};
+
+  ${({ $style }) => $style};
+`;
+
+const Label = styled.span`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  min-width: 0;
+  flex: 1;
+`;
+
+const ActionWrapper = styled.div<{
+  $style?: CSSProp;
+}>`
+  display: flex;
+  flex-direction: row;
+
+  ${({ $style }) => $style}
+`;
+
+const TextWrapper = styled.div<{
+  $style?: CSSProp;
+  $width?: string;
+}>`
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  display: flex;
+  font-size: 8px;
+  gap: 4px;
+  ${({ $width }) =>
+    $width &&
+    css`
+      width: ${$width};
+    `};
+
+  ${({ $style }) => $style}
+`;
+
+export interface StatusbarSpacerProps {
+  mobileWidth?: string;
+  desktopWidth?: string;
+}
+
+function StatusbarSpacer({ desktopWidth, mobileWidth }: StatusbarSpacerProps) {
+  return (
+    <StyledStatusbarSpacer
+      $desktopWidth={desktopWidth}
+      $mobileWidth={mobileWidth}
+    />
+  );
+}
+
+const StyledStatusbarSpacer = styled.div<{
+  $mobileWidth?: string;
+  $desktopWidth?: string;
+}>`
+  display: flex;
+  background-color: transparent;
+  height: ${({ $mobileWidth }) => ($mobileWidth ? `${$mobileWidth}` : "21px")};
+
+  @media (min-width: 768px) {
+    display: flex;
+    height: ${({ $desktopWidth }) =>
+      $desktopWidth ? `${$desktopWidth}` : "21px"};
+  }
+`;
+
+Statusbar.Spacer = StatusbarSpacer;
+
+export { Statusbar };
