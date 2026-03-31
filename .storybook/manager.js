58616a7d9a13ea4bd3d28c9afe403b7893533b48
Alimnfl <mnaufallim234@gmail.com>
2026-03-31 11:00:57 +0700

    CHange naming in manager.js

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,12 +2,12 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
 
 addons.setConfig({
-  title: "Systatum Coneto",
+  title: "Systatum - Coneto React UI",
   theme: create({
     base: "dark",
     brandImage: "/systatum.png",
-    brandTitle: "Systatum Coneto",
+    brandTitle: "Systatum - Coneto React UI",
   }),
 });
 
-document.title = "Systatum Coneto";
+document.title = "Systatum - Coneto React UI";
