f29668a6bbda50ce4f4bb5f89edfef3b0752efb3
Alim Naufal <122043760+alimnfl@users.noreply.github.com>
2026-03-31 18:10:31 +0700

    [Improvement] SYST-548: Add favicon to Coneto (#329)
    * Change the icon with the newest icon.
    
    * Change the `favicon.ico` in the coneto.
    
    * Change the favicon to be 16x16.
    
    * Add icon only coneto logo.
    
    * Add only systatum logo and systatum with coneto.
    
    * CHange naming in manager.js
    
    * Add configure with logo
    
    * Remove unnecessary icon.
    
    * Fix manager icon.
    
    * Add icon without title.
    
    * Wrap those logos with flexDirection.
    
    * Change the alt configure.
    
    * Fix the style with for the `systatum` and `coneto`.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,12 +2,12 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
 
 addons.setConfig({
-  title: "Systatum Coneto",
+  title: "Systatum Coneto React UI Library",
   theme: create({
     base: "dark",
-    brandImage: "/systatum.png",
-    brandTitle: "Systatum Coneto",
+    brandImage: "/systatum-with-title.png",
+    brandTitle: "Systatum Coneto React UI Library",
   }),
 });
 
-document.title = "Systatum Coneto";
+document.title = "Systatum Coneto React UI Library";
