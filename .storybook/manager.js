65f39265481b424eac5ed60a2c6296359c352c4c
Alimnfl <mnaufallim234@gmail.com>
2026-03-31 16:00:55 +0700

    Fix manager icon.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,12 +2,12 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
 
 addons.setConfig({
-  title: "Systatum - Coneto React UI",
+  title: "Systatum Coneto React UI Library",
   theme: create({
     base: "dark",
-    brandImage: "/systatum.png",
-    brandTitle: "Systatum - Coneto React UI",
+    brandImage: "/systatum-with-title.png",
+    brandTitle: "Systatum Coneto React UI Library",
   }),
 });
 
-document.title = "Systatum - Coneto React UI";
+document.title = "Systatum Coneto React UI Library";
