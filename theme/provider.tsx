b10f720e04336604b49e1cadcf9121ea793e49c8
Alimnfl <mnaufallim234@gmail.com>
2026-04-01 19:24:41 +0700

    Update `themes` in provider can customize from outer component.

--- theme/provider.tsx
+++ theme/provider.tsx
@@ -1,10 +1,11 @@
 import React, { createContext, useContext } from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { AppTheme, ThemeMode } from "./index";
 import { themes } from "./themes";
-import { ThemeMode } from "./index";
 
 interface ThemeContextValue {
   mode: ThemeMode;
+  themes?: Record<string, AppTheme>;
 }
 
 const ThemeContext = createContext<ThemeContextValue>({
@@ -18,21 +19,23 @@ export function useThemeMode() {
 export function ThemeProvider({
   mode,
   children,
+  themes: themesContent = themes,
 }: {
   mode: ThemeMode;
   children: React.ReactNode;
+  themes?: Record<string, AppTheme>;
 }) {
-  const theme = themes[mode];
+  const theme = themesContent[mode];
 
   return (
-    <ThemeContext.Provider value={{ mode }}>
+    <ThemeContext.Provider value={{ mode, themes: themesContent }}>
       <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
     </ThemeContext.Provider>
   );
 }
 
 export function useTheme() {
-  const { mode } = useThemeMode();
+  const { mode, themes } = useThemeMode();
   const currentTheme = themes[mode];
 
   return {
