09761b4fa24a060dcfdcd94060c2fec229ea3eba
Alimnfl <mnaufallim234@gmail.com>
2026-04-01 13:14:07 +0700

    Add several interfaces using for `themes`.

--- theme/index.ts
+++ theme/index.ts
@@ -1,10 +1,52 @@
+import { ButtonVariants } from "./../components/button";
+
 export type ThemeMode = "light" | "dark";
 
 export interface BodyThemeConfiguration {
-  backgroundColor: string;
-  textColor: string;
+  backgroundColor?: string;
+  textColor?: string;
+}
+
+// Avatar.tsx
+export interface AvatarThemeConfiguration {
+  borderColor?: string;
+  textColor?: string;
+  overlayBackground?: string;
+  overlayIconColor?: string;
+}
+
+// Badge.tsx
+export interface BadgeThemeConfiguration extends BodyThemeConfiguration {
+  borderColor?: string;
+  circleColor?: string;
+  action?: {
+    hoverBackgroundColor?: string;
+    activeBackgroundColor?: string;
+    focusRingColor?: string;
+    disabledOpacity?: number;
+  };
+}
+
+// Boxbar.tsx
+export interface BoxbarThemeConfiguration extends BodyThemeConfiguration {
+  borderColor?: string;
+  toggleButtonColor?: string;
+  toggleButtonHoverColor?: string;
+}
+
+// Button.tsx
+export interface ButtonThemeConfiguration extends BodyThemeConfiguration {
+  hoverBackgroundColor?: string;
+  activeBackgroundColor?: string;
+  borderColor?: string;
+  textDecoration?: string;
+  focusBackgroundColor?: string;
 }
 
 export interface AppTheme {
   body: BodyThemeConfiguration;
+  avatar: AvatarThemeConfiguration;
+  badge: BadgeThemeConfiguration;
+  button: Record<ButtonVariants["variant"], ButtonThemeConfiguration>;
+  boxbar: BoxbarThemeConfiguration;
 }
