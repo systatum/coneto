762669246044e7a1b90bd7bb0af3d9c78f8508b0
Alimnfl <mnaufallim234@gmail.com>
2026-04-01 19:26:33 +0700

    Introduces `theming` for the `ChoiceGroup` component.

--- components/choice-group.tsx
+++ components/choice-group.tsx
@@ -7,8 +7,9 @@ import {
   ReactNode,
 } from "react";
 import styled, { css, CSSProp } from "styled-components";
-import { RadioProps } from "./radio";
-import { CheckboxProps } from "./checkbox";
+import { Radio, RadioProps } from "./radio";
+import { Checkbox, CheckboxProps } from "./checkbox";
+import { useTheme } from "./../theme/provider";
 
 export interface ChoiceGroupProps {
   children: ReactNode;
@@ -21,21 +22,21 @@ export interface ChoiceGroupStylesProps {
 }
 
 function ChoiceGroup({ children, styles }: ChoiceGroupProps) {
+  const { currentTheme } = useTheme();
+  const choiceGroupTheme = currentTheme.choiceGroup;
+
   const childArray = Children.toArray(children).filter(isValidElement);
   const isRadioButton = childArray.some((child) => {
     if (!isValidElement(child)) return false;
-
-    if (isRadioElement(child)) {
-      return child.props.mode === "button";
-    }
-
+    if (isRadioElement(child)) return child.props.mode === "button";
     return false;
   });
 
   return (
     <ChoiceGroupWrapper
       $isRowDirection={isRadioButton}
-      $containerStyle={styles?.containerStyle}
+      $style={styles?.containerStyle}
+      $borderColor={choiceGroupTheme.borderColor}
     >
       {childArray.map((child, index) => {
         const isLast = index === childArray.length - 1;
@@ -43,32 +44,79 @@ function ChoiceGroup({ children, styles }: ChoiceGroupProps) {
           RadioProps | CheckboxProps
         >;
 
+        const isRadio = componentChild.type === Radio;
+        const isCheckbox = componentChild.type === Checkbox;
+
+        const radioProps = isRadio
+          ? (componentChild.props as RadioProps)
+          : undefined;
+        const checkboxProps = isCheckbox
+          ? (componentChild.props as CheckboxProps)
+          : undefined;
+
+        const radioMode = radioProps?.mode ?? "radio";
+        const isRadioButton = radioMode === "button";
+        const isRadioNormal = radioMode === "radio";
+
         const modifiedChild = cloneElement(componentChild, {
           highlightOnChecked: true,
-          ...(isRadioButton
-            ? {
-                styles: {
-                  containerStyle: css`
-                    border: 0.5px solid rgba(229, 231, 235, 0.6);
-                    border-radius: 4px;
-                    overflow: hidden;
-
-                    ${componentChild.props.styles?.containerStyle}
-                  `,
-                },
-              }
-            : {
-                styles: {
-                  containerStyle: css`
-                    width: 100%;
-                    ${componentChild.props.styles?.containerStyle}
-                  `,
-                  labelStyle: css`
-                    font-size: 14px;
-                    ${componentChild.props.styles?.labelStyle}
-                  `,
-                },
-              }),
+          ...(isRadioButton && {
+            styles: {
+              ...radioProps?.styles,
+              containerStyle: css`
+                border: 0.5px solid ${choiceGroupTheme.dividerColor};
+                border-radius: 4px;
+                overflow: hidden;
+                ${radioProps?.styles?.containerStyle}
+              `,
+              textWrapperStyle: css`
+                background-color: ${choiceGroupTheme.backgroundColor};
+                ${radioProps?.styles?.textWrapperStyle}
+              `,
+              labelStyle: css`
+                color: ${choiceGroupTheme.labelColor};
+                ${radioProps?.styles?.labelStyle}
+              `,
+              descriptionStyle: css`
+                color: ${choiceGroupTheme.descriptionColor};
+                ${radioProps?.styles?.descriptionStyle}
+              `,
+            },
+          }),
+          ...(isRadioNormal && {
+            styles: {
+              ...radioProps?.styles,
+              textWrapperStyle: css`
+                ${radioProps?.styles?.textWrapperStyle}
+              `,
+              labelStyle: css`
+                color: ${choiceGroupTheme.labelColor};
+                ${radioProps?.styles?.labelStyle}
+              `,
+              descriptionStyle: css`
+                ${radioProps?.styles?.descriptionStyle}
+              `,
+            },
+          }),
+          ...(isCheckbox && {
+            styles: {
+              ...checkboxProps?.styles,
+              containerStyle: css`
+                width: 100%;
+                background-color: ${choiceGroupTheme.backgroundColor};
+                ${checkboxProps?.styles?.containerStyle}
+              `,
+              labelStyle: css`
+                font-size: 14px;
+                color: ${choiceGroupTheme.labelColor};
+                ${checkboxProps?.styles?.labelStyle}
+              `,
+              descriptionStyle: css`
+                color: ${choiceGroupTheme.descriptionColor};
+                ${checkboxProps?.styles?.descriptionStyle}
+              `,
+            },
+          }),
         });
 
         return (
@@ -77,7 +125,8 @@ function ChoiceGroup({ children, styles }: ChoiceGroupProps) {
             {!isLast && !isRadioButton && (
               <ChoiceGroupDivider
                 aria-label="divider for choice group"
-                $dividerStyle={styles?.dividerStyle}
+                $style={styles?.dividerStyle}
+                $dividerColor={choiceGroupTheme.dividerColor}
               />
             )}
           </Fragment>
@@ -88,12 +137,13 @@ function ChoiceGroup({ children, styles }: ChoiceGroupProps) {
 }
 
 const ChoiceGroupWrapper = styled.div<{
-  $containerStyle?: CSSProp;
+  $style?: CSSProp;
   $isRowDirection?: boolean;
+  $borderColor: string;
 }>`
   display: flex;
   position: relative;
-  border: 1px solid #e5e7eb;
+  border: 1px solid ${({ $borderColor }) => $borderColor};
   border-radius: 4px;
   overflow: hidden;
   width: 100%;
@@ -109,21 +159,22 @@ const ChoiceGroupWrapper = styled.div<{
         `
       : css`
           flex-direction: column;
-        `}
-  ${({ $containerStyle }) => $containerStyle}
+        `};
+
+  ${({ $style }) => $style}
 `;
 
 const ChoiceGroupDivider = styled.div<{
-  $dividerStyle?: CSSProp;
-  $isRowDirection?: boolean;
+  $style?: CSSProp;
+  $dividerColor: string;
 }>`
-  background-color: #e5e7eb;
+  background-color: ${({ $dividerColor }) => $dividerColor};
   flex-shrink: 0;
 
   width: 100%;
   height: 1px;
 
-  ${({ $dividerStyle }) => $dividerStyle}
+  ${({ $style }) => $style}
 `;
 
 function isRadioElement(
