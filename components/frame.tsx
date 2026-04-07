2c7c9516695ba854e0ef14187ebacfd91bf9e8fa
Alimnfl <mnaufallim234@gmail.com>
2026-04-07 17:55:45 +0700

    Introduces `theming` for the `frame` component.

--- components/frame.tsx
+++ components/frame.tsx
@@ -1,5 +1,7 @@
 import React, { HTMLAttributes } from "react";
 import styled, { CSSProp } from "styled-components";
+import { useTheme } from "./../theme/provider";
+import { FrameThemeConfiguration } from "./../theme";
 
 export interface FrameProps
   extends Omit<HTMLAttributes<HTMLDivElement>, "title" | "style"> {
@@ -14,16 +16,20 @@ export interface FrameStyles {
 }
 
 function Frame({ title, children, styles, ...props }: FrameProps) {
+  const { currentTheme } = useTheme();
+  const frameTheme = currentTheme?.frame;
+
   return (
     <FrameContainer
       aria-label="frame"
       {...props}
       $style={styles?.containerStyle}
+      $theme={frameTheme}
     >
       {title && (
-        <TitleContainer $style={styles?.titleStyle}>
-          <TitleOverlay aria-hidden="true" />
-          <TitleText>{title}</TitleText>
+        <TitleContainer $theme={frameTheme} $style={styles?.titleStyle}>
+          <TitleOverlay aria-hidden="true" $theme={frameTheme} />
+          <TitleText $theme={frameTheme}>{title}</TitleText>
         </TitleContainer>
       )}
       {children}
@@ -31,41 +37,51 @@ function Frame({ title, children, styles, ...props }: FrameProps) {
   );
 }
 
-const FrameContainer = styled.div<{ $style?: CSSProp }>`
+const FrameContainer = styled.div<{
+  $style?: CSSProp;
+  $theme?: FrameThemeConfiguration;
+}>`
   position: relative;
   width: 100%;
   min-width: 500px;
   padding: 30px 16px;
   border-radius: var(--radius-xs);
-  border: 1px solid #d1d5db;
-  box-shadow: var(--shadow-xs);
-  background-color: white;
+  border: 1px solid ${({ $theme }) => $theme?.borderColor || "#d1d5db"};
+  box-shadow: ${({ $theme }) => $theme?.boxShadow || "var(--shadow-xs)"};
+  background-color: ${({ $theme }) => $theme?.backgroundColor || "white"};
   ${({ $style }) => $style}
 `;
 
-const TitleContainer = styled.span<{ $style?: CSSProp }>`
+const TitleContainer = styled.span<{
+  $style?: CSSProp;
+  $theme?: FrameThemeConfiguration;
+}>`
   position: absolute;
   top: -12px;
   left: 0.75rem;
   font-size: 0.95em;
-  color: #999b9d;
   padding: 0px 6px;
-  background-color: white;
+
+  color: ${({ $theme }) => $theme?.titleColor || "#999b9d"};
+  background-color: ${({ $theme }) => $theme?.titleBackgroundColor || "white"};
+
   ${({ $style }) => $style}
 `;
 
-const TitleOverlay = styled.span`
+const TitleOverlay = styled.span<{ $theme?: FrameThemeConfiguration }>`
   position: absolute;
   top: -2px;
   left: 0;
   width: 100%;
   height: 66.666667%;
-  background-color: white;
+  background-color: ${({ $theme }) =>
+    $theme?.overlayBackgroundColor || "white"};
   pointer-events: none;
 `;
 
-const TitleText = styled.span`
+const TitleText = styled.span<{ $theme: FrameThemeConfiguration }>`
   position: relative;
+  color: ${({ $theme }) => $theme?.titleColor || "#999b9d"};
 `;
 
 export { Frame };
