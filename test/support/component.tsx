d7ef035293392c98675e396dd52c43e220406a05
Alimnfl <mnaufallim234@gmail.com>
2026-04-10 15:39:40 +0700

    Add function to provide `theme` in test component.

--- test/support/component.tsx
+++ test/support/component.tsx
@@ -1,15 +1,39 @@
 import "./../../shared.css";
-import { mount } from "cypress/react";
+import { mount, MountOptions } from "cypress/react";
 import "./commands";
 import "@testing-library/cypress/add-commands";
 import "cypress-real-events";
+import { ThemeProvider } from "./../../theme/provider";
+import { themes } from "./../../theme/mode";
+import { ReactNode } from "react";
 
-Cypress.Commands.add("mount", mount);
+type CustomMountOptions = MountOptions & {
+  mode?: "light" | "dark";
+};
+
+const mountWithTheme = (
+  component: React.ReactNode,
+  options: CustomMountOptions = {}
+) => {
+  const { mode = "light" } = options;
+
+  return mount(
+    <ThemeProvider themes={themes} mode={mode}>
+      <div data-theme={mode}>{component}</div>
+    </ThemeProvider>,
+    options
+  );
+};
+
+Cypress.Commands.add("mount", mountWithTheme);
 
 declare global {
   namespace Cypress {
     interface Chainable {
-      mount: typeof mount;
+      mount(
+        component: ReactNode,
+        options?: CustomMountOptions
+      ): Cypress.Chainable;
     }
   }
 }
