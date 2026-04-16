7c5eb477307d9f385b39c57f6c0128cd5d6352da
Alimnfl <mnaufallim234@gmail.com>
2026-04-16 15:04:49 +0700

    Update to readable in validator rss.

--- scripts/generate-rss.cjs
+++ scripts/generate-rss.cjs
@@ -376,7 +376,8 @@ async function main() {
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
   xmlns:atom="http://www.w3.org/2005/Atom"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/">
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/elements/1.1/">
   <channel>
     <title>${escapeXml(channelTitle)}</title>
     <link>${SITE_URL}</link>
