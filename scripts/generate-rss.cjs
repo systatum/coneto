b4e37dc82c7e6e13d49053843245146e14cfa386
Alimnfl <mnaufallim234@gmail.com>
2026-04-16 15:58:15 +0700

    Add `atom:link` in another PR.

--- scripts/generate-rss.cjs
+++ scripts/generate-rss.cjs
@@ -375,8 +375,6 @@ async function main() {
    */
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
-  xmlns:dc="http://purl.org/dc/elements/1.1/"
-  xmlns:atom="http://www.w3.org/2005/Atom"
   xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>${escapeXml(channelTitle)}</title>
@@ -384,7 +382,6 @@ async function main() {
     <description><![CDATA[${makeSummary(channelDescription)}]]></description>
     <language>en</language>
     <generator>storybook-rss v${version}</generator>
-    <atom:link href="${SITE_URL}/feed.xml" rel="self" type="application/rss+xml" />
     <lastBuildDate>${lastModified.toUTCString()}</lastBuildDate>
     ${items.join("\n")}
   </channel>
