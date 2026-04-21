fa2abc5b36d140ab5a8730a395f264ad3e789b83
Alimnfl <mnaufallim234@gmail.com>
2026-04-21 15:23:30 +0700

    Add story for `code-editor`, `markdown-editor`, and update the `only-view` result.

--- components/rich-editor.stories.tsx
+++ components/rich-editor.stories.tsx
@@ -177,6 +177,13 @@ This is ordered list
 This is unordered list
 * test
 * test
+
+This is with code-block
+\`\`\`tsx
+import { Button } from "@systatum/coneto/button"
+
+return <Button variant="primary">Your caption</Button>
+\`\`\`
 `,
       },
       { title: "Sender Name", content: "Sender Name" },
@@ -311,7 +318,14 @@ This is ordered list
 
 This is unordered list
 * test
-* test  
+* test
+
+This is with code-block
+\`\`\`tsx
+import { Button } from "@systatum/coneto/button"
+
+return <Button variant="primary">Your caption</Button>
+\`\`\`
 `,
       },
       { title: "Sender Name", content: "Sender Name" },
@@ -493,6 +507,159 @@ export const PageEditor: Story = {
   },
 };
 
+export const MarkdownEditor: Story = {
+  render: () => {
+    const [value, setValue] = useState("");
+    const [printValue, setPrintValue] = useState("");
+
+    const ref = useRef<RichEditorRef>(null);
+
+    const TIP_MENU_EMAIL: TipMenuItemProps[] = [
+      {
+        caption: "Duplicate",
+        icon: {
+          image: RiFileCopyLine,
+        },
+        onClick: () => console.log("Phishing reported"),
+      },
+      {
+        caption: "Report Junk",
+        icon: {
+          image: RiDeleteBinLine,
+        },
+        variant: "danger",
+        onClick: () => console.log("Junk reported"),
+      },
+    ];
+
+    const sentences = useMemo(
+      () => generateSentence({ minLen: 30, maxLen: 40, seed: 12345 }),
+      [generateSentence]
+    );
+    const BADGE_ITEMS = [
+      {
+        title: "Markdown Example",
+        content: `### Hello there!
+${sentences}
+
+This is ordered list
+1. [ ] test
+2. [x] test
+
+This is unordered list
+* test
+* test
+`,
+      },
+      { title: "Sender Name", content: "Sender Name" },
+      { title: "Sender Email", content: "Sender Email" },
+    ];
+
+    const TOOLBAR_RIGHT_PANEL_ACTIONS = (
+      <RichEditor.ToolbarButton
+        icon={{ image: RiPrinterFill }}
+        onClick={() => {
+          setPrintValue(value);
+          console.log(value);
+        }}
+      >
+        Print
+      </RichEditor.ToolbarButton>
+    );
+
+    return (
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <RichEditor
+          ref={ref}
+          mode="markdown-editor"
+          onChange={(e) => setValue(e)}
+          value={value}
+          toolbarRightPanel={TOOLBAR_RIGHT_PANEL_ACTIONS}
+        />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            gap: "8px",
+            flexDirection: "row",
+          }}
+        >
+          <Boxbar
+            styles={{
+              self: css`
+                width: 100%;
+              `,
+            }}
+          >
+            {BADGE_ITEMS.map((data, index) => (
+              <Badge
+                onClick={async () => {
+                  if (data.title === "Markdown Example") {
+                    await ref.current?.insertMarkdownContent(data.content);
+                  } else {
+                    await ref.current?.insertPlainText(data.content);
+                  }
+                }}
+                key={index}
+                withCircle
+                caption={data.title}
+              />
+            ))}
+          </Boxbar>
+          <div>
+            <Button
+              styles={{
+                dropdownStyle: css`
+                  min-width: 200px;
+                `,
+                self: css`
+                  min-height: 40px;
+                `,
+                toggleStyle: css`
+                  min-height: 40px;
+                `,
+              }}
+              subMenu={({ list }) => list(TIP_MENU_EMAIL)}
+              className="w-fit"
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+        {printValue !== "" && (
+          <pre
+            style={{
+              padding: 28,
+              background: "#D3D3D3",
+              wordBreak: "break-word",
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {printValue}
+          </pre>
+        )}
+      </div>
+    );
+  },
+};
+
+export const CodeEditor: Story = {
+  render: () => {
+    const [value, setValue] = useState("");
+
+    const ref = useRef<RichEditorRef>(null);
+
+    return (
+      <RichEditor
+        ref={ref}
+        mode="code-editor"
+        onChange={(e) => setValue(e)}
+        value={value}
+      />
+    );
+  },
+};
+
 export const ViewOnly: Story = {
   render: () => {
     const { currentTheme } = useTheme();
@@ -513,6 +680,13 @@ This is ordered list
 This is unordered list
 * test
 * test
+
+This is with code-block
+\`\`\`tsx
+import { Button } from "@systatum/coneto/button"
+
+return <Button variant="primary">Your caption</Button>
+\`\`\`
 `
     );
 
