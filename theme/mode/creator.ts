f3a5723f6a437dca5e914db4df6dcc2cb2de3722
Alimnfl <mnaufallim234@gmail.com>
2026-04-21 14:10:04 +0700

    Add `scrollThumbColor` to make good color for scrolling in `Combobox`.

--- theme/mode/creator.ts
+++ theme/mode/creator.ts
@@ -578,6 +578,8 @@ export function createComboboxTheme(
       "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
 
     groupBackgroundColor: "rgb(249, 250, 251)",
+
+    scrollThumbColor: "#9ca3af",
   };
 
   return { ...defaultTheme, ...custom };
