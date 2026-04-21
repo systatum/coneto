9fef3fbce695e51af37e5072640859767b5b5736
Alimnfl <mnaufallim234@gmail.com>
2026-04-21 14:12:23 +0700

    Add `drawerStyle` and `scrollThumb` inside `combobox`.

--- components/combobox.tsx
+++ components/combobox.tsx
@@ -82,6 +82,7 @@ export interface ComboboxStyles extends Omit<SelectboxStyles, "self"> {
   containerStyle?: CSSProp;
   selectboxStyle?: CSSProp;
   labelStyle?: CSSProp;
+  drawerStyle?: CSSProp;
 }
 
 export interface ComboboxAction {
@@ -317,6 +318,7 @@ function ComboboxDrawer({
   setConfirmedValue,
   openedCategoryGroup,
   setOpenedCategoryGroup,
+  styles,
 }: ComboboxDrawerProps) {
   const { currentTheme } = useTheme();
   const comboboxTheme = currentTheme?.combobox;
@@ -553,6 +555,7 @@ function ComboboxDrawer({
       role="listbox"
       $width={refs.reference.current?.getBoundingClientRect().width}
       style={{ ...floatingStyles }}
+      $style={styles?.drawerStyle}
     >
       {(finalOptions || actions) && (
         <List
@@ -791,6 +794,7 @@ const listItemTitleWithRender = css`
 const DrawerWrapper = styled.ul<{
   $width?: number;
   $theme: ComboboxThemeConfig;
+  $style?: CSSProp;
 }>`
   position: absolute;
   z-index: 9992999;
@@ -801,6 +805,17 @@ const DrawerWrapper = styled.ul<{
   background-color: ${({ $theme }) => $theme?.backgroundColor};
   box-shadow: ${({ $theme }) => $theme?.boxShadow};
   width: ${({ $width }) => ($width ? `${$width}px` : "100%")};
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ $theme }) => $theme?.scrollThumbColor || "#3f3f46"};
+    border-radius: 999px;
+  }
+
+  ${({ $style }) => $style}
 `;
 
 const Divider = styled.div<{ $theme: ComboboxThemeConfig }>`
