200dbdf9b40e8a640c615d79ec0f368bcd1d024a
Alimnfl <mnaufallim234@gmail.com>
2026-04-22 20:26:37 +0700

    Test the deploying with change `main.ts`.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,7 +12,28 @@ const config: StorybookConfig = {
     options: {},
   },
   viteFinal: async (config) => {
-    return config;
+    return {
+      ...config,
+      optimizeDeps: {
+        ...config.optimizeDeps,
+        exclude: [...(config.optimizeDeps?.exclude ?? []), "@systatum/coneto"],
+      },
+      build: {
+        ...config.build,
+        chunkSizeWarningLimit: 10000,
+        rollupOptions: {
+          ...config.build?.rollupOptions,
+          output: {
+            ...((config.build?.rollupOptions?.output as object) ?? {}),
+            manualChunks(id: string) {
+              if (id.includes("monaco-editor")) return "monaco";
+              if (id.includes("node_modules")) return "vendor";
+            },
+          },
+        },
+      },
+    };
   },
 };
+
 export default config;
