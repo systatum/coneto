2f22b9a934c80f6df40bd5678026dd429fe34863
Alimnfl <mnaufallim234@gmail.com>
2026-04-27 17:26:06 +0700

    Update `FieldLane` actions with `BaseAction` type.

--- components/field-lane.tsx
+++ components/field-lane.tsx
@@ -7,6 +7,7 @@ import { Tooltip } from "./tooltip";
 import { Figure, FigureProps } from "./figure";
 import { useTheme } from "./../theme/provider";
 import { FieldLaneThemeConfig } from "./../theme";
+import { BaseAction } from "../constants/action";
 
 export const FieldLaneErrorIconPosition = {
   Absolute: "absolute",
@@ -51,14 +52,8 @@ export interface FieldLaneStyles {
   bodyStyle?: CSSProp;
 }
 
-export interface FieldLaneAction {
-  title?: string;
-  icon?: FigureProps;
-  iconColor?: string;
-  onClick?: (e: React.MouseEvent) => void;
-  disabled?: boolean;
+export interface FieldLaneAction extends BaseAction {
   titleShowDelay?: number;
-  hidden?: boolean;
 }
 
 export interface FieldLaneDropdown {
@@ -137,7 +132,11 @@ function FieldLane({
                   const dropdownData = dropdown.options.map((prop) => ({
                     caption: prop.text,
                     icon: prop.icon,
-                    onClick: () => dropdown.onChange(prop.value),
+                    onClick: (e: React.MouseEvent) => {
+                      e.preventDefault();
+                      dropdown.onChange(prop.value);
+                    },
+                    id: prop.value,
                   }));
 
                   return list(dropdownData, {
@@ -183,6 +182,7 @@ function FieldLane({
                     &:hover {
                       color: ${fieldLaneTheme?.buttonTextColor};
                     }
+                    min-height: 32px;
 
                     ${!children &&
                     css`
@@ -220,11 +220,11 @@ function FieldLane({
       {children}
 
       {hasActions &&
-        filteredActions.map((props, index) => {
-          const { icon, titleShowDelay = 1250 } = props;
+        filteredActions.map((action, index) => {
+          const { icon, titleShowDelay = 1250 } = action;
           const offsetBase = 8;
           const offsetEach = 22;
-          const reverseIndex = actions.length - 1 - index;
+          const reverseIndex = filteredActions.length - 1 - index;
           const offset =
             offsetBase +
             reverseIndex * offsetEach +
@@ -234,16 +234,17 @@ function FieldLane({
           return (
             <Button
               key={index}
+              id={action.id}
               labelMode="flex"
               aria-label="action-icon"
               onMouseDown={(e) => e.preventDefault()}
               onClick={(e) => {
                 e.stopPropagation();
-                if ((!disabled || !props?.disabled) && props.onClick) {
-                  props.onClick(e);
+                if ((!disabled || !action?.disabled) && action.onClick) {
+                  action.onClick(e);
                 }
               }}
-              disabled={disabled ? disabled : props.disabled}
+              disabled={disabled ? disabled : action.disabled}
               styles={{
                 containerStyle: css`
                   position: absolute;
@@ -264,16 +265,12 @@ function FieldLane({
 
                   color: ${showError
                     ? fieldLaneTheme?.errorBackground
-                    : props.iconColor
-                      ? props.iconColor
-                      : fieldLaneTheme?.actionColor};
+                    : fieldLaneTheme?.actionColor};
 
                   &:hover {
                     color: ${showError
                       ? fieldLaneTheme?.errorForeground
-                      : props.iconColor
-                        ? props.iconColor
-                        : fieldLaneTheme?.actionHoverColor};
+                      : fieldLaneTheme?.actionHoverColor};
                   }
                 `,
               }}
@@ -283,8 +280,8 @@ function FieldLane({
                 onClick={(e) => {
                   e.preventDefault();
                   e.stopPropagation();
-                  if ((!disabled || !props?.disabled) && props.onClick) {
-                    props.onClick(e);
+                  if ((!disabled || !action?.disabled) && action.onClick) {
+                    action.onClick(e);
                   }
                 }}
                 styles={{
@@ -316,7 +313,7 @@ function FieldLane({
                   `,
                 }}
                 showDelayPeriod={titleShowDelay}
-                dialog={props.title}
+                dialog={action?.caption}
               >
                 {icon && (
                   <Figure
