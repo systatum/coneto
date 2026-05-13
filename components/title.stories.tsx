15296fce3fa931170ee0e05fd3b4d4a63607e3a6
Alimnfl <mnaufallim234@gmail.com>
2026-05-13 12:49:49 +0700

    Add a sample for title.

--- components/title.stories.tsx
+++ components/title.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Title } from "./title";
+import { Title, TitleSection } from "./title";
+import { Ri24HoursFill, RiCloseLine } from "@remixicon/react";
+import { css } from "styled-components";
 
 const meta: Meta<typeof Title> = {
   title: "Content/Title",
@@ -15,8 +17,56 @@ export default meta;
 
 type Story = StoryObj<typeof Title>;
 
-export const Default: Story = {
+export const WithACustomAction: Story = {
   render: () => {
-    return <Title pretitle="pretitle" text="title" subtitle="subtitle" />;
+    const rightSection: TitleSection[] = [
+      {
+        styles: {
+          toggleActionStyle: css`
+            padding: 2px;
+            width: 20px;
+            height: 20px;
+          `,
+        },
+        actions: [
+          {
+            icon: { image: RiCloseLine },
+            onClick: () => {},
+          },
+        ],
+      },
+    ];
+
+    return (
+      <>
+        <Title
+          size="sm"
+          text="Default Modal"
+          subtitle="Here is the content of the modal dialog."
+          icon={{
+            image: Ri24HoursFill,
+          }}
+          rightSection={rightSection}
+        />
+        <Title
+          size="md"
+          text="Default Modal"
+          subtitle="Here is the content of the modal dialog."
+          icon={{
+            image: Ri24HoursFill,
+          }}
+          rightSection={rightSection}
+        />
+        <Title
+          size="lg"
+          text="Default Modal"
+          subtitle="Here is the content of the modal dialog."
+          icon={{
+            image: Ri24HoursFill,
+          }}
+          rightSection={rightSection}
+        />
+      </>
+    );
   },
 };
