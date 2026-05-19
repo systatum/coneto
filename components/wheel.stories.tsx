89dd9c1e725442e66735354afbebc282e27eda4d
Alimnfl <mnaufallim234@gmail.com>
2026-05-19 09:40:46 +0700

    Add `mobile` stories.

--- components/wheel.stories.tsx
+++ components/wheel.stories.tsx
@@ -68,3 +68,52 @@ export const Default: Story = {
     );
   },
 };
+
+export const Mobile: Story = {
+  render: () => {
+    const [values, setValues] = useState({
+      hour: "10",
+      minute: "30",
+      second: "30",
+      ampm: "am",
+    });
+
+    const hours = Array.from({ length: 12 }, (_, i) => {
+      const h = i + 1;
+      return { value: h.toString(), text: h.toString() };
+    });
+
+    const minutes = Array.from({ length: 60 }, (_, i) => ({
+      value: i.toString(),
+      text: i.toString().padStart(2, "0"),
+    }));
+
+    const seconds = minutes;
+
+    const ampm = [
+      { value: "am", text: "AM" },
+      { value: "pm", text: "PM" },
+    ];
+
+    return (
+      <Wheel
+        onChange={(values) => {
+          setValues({
+            ampm: values.ampm,
+            hour: values.hour,
+            minute: values.minute,
+            second: values.second,
+          });
+        }}
+        parts={[
+          { id: "hour", values: hours, width: "64px" },
+          { id: "minute", values: minutes, width: "72px" },
+          { id: "second", values: seconds, width: "72px" },
+          { id: "ampm", values: ampm, width: "64px" },
+        ]}
+        values={values}
+        mobile
+      />
+    );
+  },
+};
