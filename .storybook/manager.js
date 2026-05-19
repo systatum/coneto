c0ffa40e27f0193602e810d52aaa36cd9d13c1c0
Alimnfl <mnaufallim234@gmail.com>
2026-05-19 11:34:20 +0700

    Update another method to get brand image.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,11 +1,12 @@
 import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
+import logo from "./../public/systatum-with-title.png";
 
 addons.setConfig({
   title: "Systatum Coneto React UI Library",
   theme: create({
     base: "dark",
-    brandImage: "./systatum-with-title.png",
+    brandImage: logo,
     brandTitle: "Systatum Coneto React UI Library",
   }),
 });
