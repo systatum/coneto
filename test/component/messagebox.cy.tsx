dcda4dddee7d5aaccade64bf3ed67781e36199e7
Alimnfl <mnaufallim234@gmail.com>
2026-06-11 15:56:26 +0700

    Ensures the `height` with `fit-content`.

---
+++ test/component/messagebox.cy.tsx
@@ -0,0 +1,41 @@
+import { css } from "styled-components";
+import { Messagebox, MessageboxProps } from "./../../components/messagebox";
+
+describe("Messagebox", () => {
+  function ProductMessagebox(props?: Partial<MessageboxProps>) {
+    return (
+      <Messagebox title="This is title" {...props}>
+        This is content
+      </Messagebox>
+    );
+  }
+  context("height", () => {
+    it("renders with fit-content (by default)", () => {
+      cy.mount(<ProductMessagebox />);
+      cy.findByLabelText("messagebox-container").should(
+        "have.css",
+        "height",
+        "78.59375px"
+      );
+    });
+
+    context("when given height 400px", () => {
+      it("renders height 400px", () => {
+        cy.mount(
+          <ProductMessagebox
+            styles={{
+              containerStyle: css`
+                height: 400px;
+              `,
+            }}
+          />
+        );
+        cy.findByLabelText("messagebox-container").should(
+          "have.css",
+          "height",
+          "400px"
+        );
+      });
+    });
+  });
+});
