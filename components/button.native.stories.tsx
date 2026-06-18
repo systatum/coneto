29b144121b27db8e57223344ee771d63137b723f
Alimnfl <mnaufallim234@gmail.com>
2026-06-18 16:54:23 +0700

    Add storybook on `mobile` category.

---
+++ components/button.native.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button.native";
+import { css } from "styled-components";
+
+const meta = {
+  title: "Mobile/Button",
+  component: Button,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: `
+test react native
+        `,
+      },
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <>
+        <Button
+          styles={{
+            containerStyle: css`
+              padding: 20px;
+            `,
+          }}
+        />
+      </>
+    );
+  },
+};
