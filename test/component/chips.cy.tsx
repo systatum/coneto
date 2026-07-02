9ca4cde5304b8161c5dcc4430eb9edda55fc20e7
Alimnfl <mnaufallim234@gmail.com>
2026-07-02 18:31:11 +0700

    Ensures the `chips`  works correctly for `actions` clicking and `options` coloring.

--- test/component/chips.cy.tsx
+++ test/component/chips.cy.tsx
@@ -2,51 +2,56 @@ import { Chips, ChipsProps } from "./../../components/chips";
 import { BadgeProps } from "./../../components/badge";
 import { ChangeEvent, useState } from "react";
 import { css } from "styled-components";
+import { RiCloseLine } from "@remixicon/react";
 
 describe("Chips", () => {
-  function ProductChips(props: ChipsProps) {
-    const BADGE_OPTIONS: BadgeProps[] = [
-      {
-        id: "1",
-        caption: "Anime",
-      },
-      {
-        id: "2",
-        caption: "Manga",
-      },
-      {
-        id: "3",
-        caption: "Comics",
-      },
-      {
-        id: "4",
-        caption: "Movies",
-      },
-      {
-        id: "5",
-        caption: "Podcasts",
-      },
-      {
-        id: "6",
-        caption: "TV Shows",
-      },
-      {
-        id: "7",
-        caption: "Novels",
-      },
-      {
-        id: "8",
-        caption: "Music",
-      },
-      {
-        id: "9",
-        caption: "Games",
-      },
-      {
-        id: "10",
-        caption: "Webtoons",
-      },
-    ];
+  function ProductChips({
+    onActionClick,
+    ...props
+  }: ChipsProps & {
+    onActionClick?: (props?: {
+      badge?: BadgeProps;
+      event?: React.MouseEvent;
+    }) => void;
+  }) {
+    const BADGE_OPTIONS: BadgeProps[] = Array.from({ length: 10 }, (_, i) => ({
+      id: String(i + 1),
+      caption: (() => {
+        switch (i + 1) {
+          case 1:
+            return "Anime";
+          case 2:
+            return "Manga";
+          case 3:
+            return "Comics";
+          case 4:
+            return "Movies";
+          case 5:
+            return "Podcasts";
+          case 6:
+            return "TV Shows";
+          case 7:
+            return "Novels";
+          case 8:
+            return "Music";
+          case 9:
+            return "Games";
+          case 10:
+            return "Webtoons";
+          default:
+            return "";
+        }
+      })(),
+      actions: [
+        {
+          image: RiCloseLine,
+          onClick: ({ badge, event }) => {
+            onActionClick?.({ badge, event });
+          },
+          size: 16,
+        },
+      ],
+    }));
 
     interface InputValueProps {
       search: string;
@@ -84,15 +89,6 @@ describe("Chips", () => {
       <Chips
         inputValue={inputValue.search}
         setInputValue={onChangeValue}
-        styles={{
-          chipOptionStyle: css`
-            width: 100%;
-            gap: 8px;
-          `,
-          chipOptionWrapperStyle: css`
-            gap: 4px;
-          `,
-        }}
         onChange={(selectedOptions: string[]) =>
           setInputValue((prev) => ({
             ...prev,
@@ -140,6 +136,90 @@ describe("Chips", () => {
             "height",
             "400px"
           );
+        });
+      });
+    });
+  });
+
+  context("actions", () => {
+    context("when clicking", () => {
+      it("should stop propagate to the selection behavior", () => {
+        cy.mount(<ProductChips />);
+
+        cy.findByRole("button").click();
+
+        cy.findAllByLabelText("badge-action").eq(0).click();
+        cy.findAllByLabelText("chips-selected").should("not.exist");
+      });
+
+      it("should call the callback", () => {
+        const onClick = cy.stub().as("onClick");
+
+        cy.mount(<ProductChips onActionClick={onClick} />);
+
+        cy.findByRole("button").click();
+
+        cy.findAllByLabelText("badge-action").eq(0).click();
+
+        cy.get("@onClick").should("have.been.calledOnce");
+      });
+
+      context("when console the event", () => {
+        it("should shows the event mouse", () => {
+          cy.window().then((win) => {
+            cy.spy(win.console, "log").as("consoleLog");
+          });
+
+          cy.mount(
+            <ProductChips onActionClick={({ event }) => console.log(event)} />
+          );
+
+          cy.findByRole("button").click();
+
+          cy.findAllByLabelText("badge-action").eq(0).click();
+
+          cy.get("@consoleLog").then((spy: any) => {
+            expect(spy).to.have.been.calledOnce;
+
+            const event = spy.firstCall.args[0];
+
+            expect(event).to.exist;
+            expect(event.type).to.equal("mousedown");
+          });
+        });
+      });
+
+      context("when console the badge", () => {
+        it("should shows the badge clicked", () => {
+          cy.window().then((win) => {
+            cy.spy(win.console, "log").as("consoleLog");
+          });
+
+          cy.mount(
+            <ProductChips
+              onActionClick={({ badge }) => console.log(JSON.stringify(badge))}
+            />
+          );
+
+          cy.findByRole("button").click();
+
+          cy.findAllByLabelText("badge-action").eq(0).click();
+
+          cy.get("@consoleLog").then((spy: any) => {
+            expect(spy).to.have.been.calledOnce;
+
+            const logged = spy.firstCall.args[0];
+
+            expect(logged).to.equal(
+              JSON.stringify({
+                id: "1",
+                caption: "Anime",
+                variant: null,
+                backgroundColor: "transparent",
+                withCircle: true,
+              })
+            );
+          });
         });
       });
     });
