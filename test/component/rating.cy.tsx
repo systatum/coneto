b9fd495fb0644d767c7b3e0ec8069cd93904408b
Alimnfl <mnaufallim234@gmail.com>
2026-07-31 11:12:27 +0700

    Add the case of `renderLabel`

--- test/component/rating.cy.tsx
+++ test/component/rating.cy.tsx
@@ -11,11 +11,59 @@ describe("Rating", () => {
         editable
         rating={value}
         onChange={(e) => setValue(e.target.value)}
-        withLabel
+        renderLabel
         {...props}
       />
     );
   }
+
+  context("renderLabel", () => {
+    context("when given true", () => {
+      it("render the label", () => {
+        cy.mount(<ProductRating renderLabel />);
+
+        cy.findByLabelText("rating-label").should("have.text", "4.5 / 5");
+      });
+    });
+
+    context("when given false", () => {
+      it("not render the label", () => {
+        cy.mount(<ProductRating renderLabel={false} />);
+
+        cy.findByLabelText("rating-label").should("not.exist");
+      });
+    });
+
+    context("when given string function", () => {
+      it("render the label", () => {
+        cy.mount(
+          <ProductRating
+            renderLabel={({ value, maxValue }) => `${value} out of ${maxValue}`}
+          />
+        );
+
+        cy.findByLabelText("rating-label").should("have.text", "4.5 out of 5");
+      });
+    });
+
+    context("when given reactnode function", () => {
+      it("render the react element", () => {
+        cy.mount(
+          <ProductRating
+            renderLabel={({ value, maxValue }) => (
+              <span aria-label="test-label">
+                {value} out of {maxValue}
+              </span>
+            )}
+          />
+        );
+
+        cy.findByLabelText("test-label")
+          .should("exist")
+          .and("have.text", "4.5 out of 5");
+      });
+    });
+  });
 
   context("styles", () => {
     context("ratingWrapperStyle", () => {
