8b6fe7aaa72e45cf736665792b66fb53edf61279
Alimnfl <mnaufallim234@gmail.com>
2026-07-31 14:29:58 +0700

    Ensures the direction for position and the `scoreLabel` value.

--- test/component/rating.cy.tsx
+++ test/component/rating.cy.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { Rating, RatingProps } from "./../../components/rating";
+import {
+  Rating,
+  RatingProps,
+  RatingScoreLabelPosition,
+} from "./../../components/rating";
 import { css } from "styled-components";
 
 describe("Rating", () => {
@@ -10,58 +14,164 @@ describe("Rating", () => {
       <Rating
         rating={value}
         onChange={(e) => setValue(e.target.value)}
-        iconSideLabel
+        scoreLabel={{
+          text: "4.5",
+        }}
         {...props}
       />
     );
   }
 
-  context("iconSideLabel", () => {
-    context("when given true", () => {
-      it("render the label", () => {
-        cy.mount(<ProductRating iconSideLabel />);
-
-        cy.findByLabelText("rating-label").should("have.text", "4.5 / 5");
-      });
-    });
-
-    context("when given false", () => {
+  context("scoreLabel", () => {
+    context("with given empty object", () => {
       it("not render the label", () => {
-        cy.mount(<ProductRating iconSideLabel={false} />);
+        cy.mount(<ProductRating scoreLabel={{}} />);
 
         cy.findByLabelText("rating-label").should("not.exist");
       });
     });
 
-    context("when given string function", () => {
-      it("render the label", () => {
-        cy.mount(
-          <ProductRating
-            iconSideLabel={({ value, maxValue }) =>
-              `${value} out of ${maxValue}`
-            }
-          />
-        );
+    context("text", () => {
+      context("with empty string", () => {
+        it("not render label", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "",
+              }}
+            />
+          );
 
-        cy.findByLabelText("rating-label").should("have.text", "4.5 out of 5");
+          cy.findByLabelText("rating-label").should("not.exist");
+        });
+      });
+
+      context("with string", () => {
+        it("render the label", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "4.5 / 5",
+              }}
+            />
+          );
+
+          cy.findByLabelText("rating-label").should("have.text", "4.5 / 5");
+        });
+      });
+
+      context("with reactnode", () => {
+        it("render the label with element", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: <span aria-label="test-label">4.5 / 5</span>,
+              }}
+            />
+          );
+
+          cy.findByLabelText("test-label")
+            .should("exist")
+            .should("have.text", "4.5 / 5");
+        });
+      });
+
+      context("with function reactnode", () => {
+        it("render the react element", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: ({ value, maxValue }) => (
+                  <span aria-label="test-label">
+                    {value} out of {maxValue}
+                  </span>
+                ),
+              }}
+            />
+          );
+
+          cy.findByLabelText("test-label")
+            .should("exist")
+            .and("have.text", "4.5 out of 5");
+        });
       });
     });
 
-    context("when given reactnode function", () => {
-      it("render the react element", () => {
-        cy.mount(
-          <ProductRating
-            iconSideLabel={({ value, maxValue }) => (
-              <span aria-label="test-label">
-                {value} out of {maxValue}
-              </span>
-            )}
-          />
-        );
+    context("position", () => {
+      context("when given bottom", () => {
+        it("render the label below the rating", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "4.5 / 5",
+                position: RatingScoreLabelPosition.Bottom,
+              }}
+            />
+          );
 
-        cy.findByLabelText("test-label")
-          .should("exist")
-          .and("have.text", "4.5 out of 5");
+          cy.findByLabelText("rating-wrapper").should(
+            "have.css",
+            "flex-direction",
+            "column"
+          );
+        });
+      });
+
+      context("when given top", () => {
+        it("render the label above the rating", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "4.5 / 5",
+                position: RatingScoreLabelPosition.Top,
+              }}
+            />
+          );
+
+          cy.findByLabelText("rating-wrapper").should(
+            "have.css",
+            "flex-direction",
+            "column-reverse"
+          );
+        });
+      });
+
+      context("when given left", () => {
+        it("render the label on the left of the rating", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "4.5 / 5",
+                position: RatingScoreLabelPosition.Left,
+              }}
+            />
+          );
+
+          cy.findByLabelText("rating-wrapper").should(
+            "have.css",
+            "flex-direction",
+            "row-reverse"
+          );
+        });
+      });
+
+      context("when given right", () => {
+        it("render the label on the right of the rating", () => {
+          cy.mount(
+            <ProductRating
+              scoreLabel={{
+                text: "4.5 / 5",
+                position: RatingScoreLabelPosition.Right,
+              }}
+            />
+          );
+
+          cy.findByLabelText("rating-wrapper").should(
+            "have.css",
+            "flex-direction",
+            "row"
+          );
+        });
       });
     });
   });
